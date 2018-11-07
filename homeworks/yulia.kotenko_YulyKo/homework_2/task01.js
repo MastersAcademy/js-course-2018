ce242d3c2f318e8496e0e7d35f920e57361572f6
y.kotenko <kyabi71@gmail.com>
2018-11-07 23:17:57 +0200

    Added a query to the height of the pyramid

--- homeworks/yulia.kotenko_YulyKo/homework_2/task01.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task01.js
@@ -1,6 +1,18 @@
-   
+// 
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('How much? ', (answer) => {
+    
     let spase = " ";
     let grille = "#";
-    for (let i = 0; i < 10; i++) {
-        console.log(spase.repeat(9-i) + grille.repeat(2+i) + grille.repeat(2+i) + spase.repeat(9-i));
+    for (let i = 0; i < answer; i++) {
+        console.log(spase.repeat(answer-i) + grille.repeat(2+i) + grille.repeat(2+i) + spase.repeat(answer-i));
     }
+  rl.close();
+});
