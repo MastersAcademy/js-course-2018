e7a9cd139402069e14065993aa4a9d8db1d93524
Yaroslav <yaroslav.belinskiy@qa-testlab.com>
2018-11-09 00:25:04 +0200

    almost added readline

--- homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
+++ homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
@@ -1,9 +1,30 @@
-var initialStroke = '##';
-var pyramidHeight = 15;
-var space = ' ';
-for(var i = 2; i <= pyramidHeight + 1; i++) {
-    var stroke = initialStroke.repeat(i);
-    var amountOfSpace = ((pyramidHeight * 2 - 2) - stroke.length) / 2
-    var spaceBefore = space.repeat(amountOfSpace); 
-    console.log(`${spaceBefore}${stroke}`)
-};
+const READLINE = require('readline');
+const rl = READLINE.createInterface(process.stdin, process.stdout);
+
+rl.question('Enter the height of pyramid: ', function(height) {
+    
+    let parsed = parseInt(height);
+    
+    if(typeof(parsed) === 'number' && height % 1 === 0) {
+        var initialStroke = '##';
+        var pyramidHeight = height;
+        var space = ' ';
+        for(var i = 2; i <= pyramidHeight + 1; i++) {
+            var stroke = initialStroke.repeat(i);
+            var amountOfSpace = ((pyramidHeight * 2 + 2) - stroke.length) / 2;
+            var spaceBefore = space.repeat(amountOfSpace); 
+            console.log(`${spaceBefore}${stroke}`);
+        };
+        console.log('Here you are!');
+        process.exit();
+    } else {
+        rl.setPrompt('Please enter only intengers: ');
+        rl.prompt();
+        rl.on('line', function() {
+
+        });
+    };
+
+});
+
+
--- homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/secondScenario.js
+++ homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/secondScenario.js
@@ -1,20 +1,34 @@
-let minValue = 500;
-let maxValue = 1000;
-let array = [];
-let sortedArray = [];
+const READLINE = require('readline');
+const rl = READLINE.createInterface(process.stdin, process.stdout);
 
-for(let i = minValue; i <= maxValue; i++) {
-    array.push(i);
-}
-array.forEach(e => {
-    var stringify = e.toString();
-    var splited = stringify.split('');
-    var tempArray = [];
-    splited.forEach(el => {
-        tempArray.unshift(el);
-        var specularArray = tempArray.join('');
-        if(specularArray === stringify) {
-            sortedArray.push(specularArray);
-        };
-    });
+rl.question('Enter Min and Max values of array: ', function(min, max) {
+
+    let parsedMin = parseInt(min);
+    let parsedMax = parseInt(max);
+    if((typeof(parsedMin) === 'number' && parsedMin % 1 === 0) && (typeof(parsedMax) === 'number' && parsedMax % 1 === 0)) {
+        let minValue = min;
+        let maxValue = max;
+        let array = [];
+        let sortedArray = [];
+        for(let i = minValue; i <= maxValue; i++) {
+            array.push(i);
+        }
+        array.forEach(e => {
+            var stringify = e.toString();
+            var splited = stringify.split('');
+            var tempArray = [];
+            splited.forEach(el => {
+                tempArray.unshift(el);
+                var specularArray = tempArray.join('');
+                if(specularArray === stringify) {
+                    sortedArray.push(specularArray);
+                };
+            });
+        });
+        console.log(sortedArray);
+        process.exit();
+    } else {
+        console.log('Please enter only intengers')
+    };   
+    
 });
