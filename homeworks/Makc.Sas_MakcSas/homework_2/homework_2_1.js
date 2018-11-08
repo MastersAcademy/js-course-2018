4ffd4eabe9e900fb3bb5b3ee6d6c6321ce8ef0f7
MaksSas <sasmaks123@gmail.com>
2018-11-08 21:04:26 +0200

    refacrot: homework_2

--- homeworks/Makc.Sas_MakcSas/homework_2/homework_2_1.js
+++ homeworks/Makc.Sas_MakcSas/homework_2/homework_2_1.js
@@ -1,13 +1,13 @@
 function Pyramid(hight) {
     for (let number = 1; number < hight; number++) {
         let str = ' ';
-      for (let indentLeft = 1; indentLeft < hight-number; indentLeft++) {
-        str += ' ';
-      }
-      for (let indentTop = 0; indentTop <= number; indentTop++) {
-        str += '##';
-      }
-      console.log(str);
+        for (let indentLeft = 1; indentLeft < hight-number; indentLeft++) {
+            str += ' ';
+        }
+        for (let indentTop = 0; indentTop <= number; indentTop++) {
+            str += '##';
+        }
+        console.log(str);
     }
-  }
-  Pyramid(15); 
+}
+Pyramid(15); 
