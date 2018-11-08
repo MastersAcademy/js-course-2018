b5086e84fba2ad3db6dd381738552d0fae60658f
Denis Zamosenchuk <denis.zamosenchuk@gmail.com>
2018-11-08 10:30:23 +0200

    Add readline based cli

--- homeworks/denis.zamosenchuk_DenisZm/homework_2/palindrome.js
+++ homeworks/denis.zamosenchuk_DenisZm/homework_2/palindrome.js
@@ -1,6 +1,6 @@
 for (let i = 500; i < 1000; i++) {
     let string = i.toString();
-    let reverse = string.split("").reverse().join("");
+    let reverse = string.split('').reverse().join('');
     if (string === reverse) {
         console.log(i);
     }
--- homeworks/denis.zamosenchuk_DenisZm/homework_2/pyramid.js
+++ homeworks/denis.zamosenchuk_DenisZm/homework_2/pyramid.js
@@ -1,3 +1,23 @@
-for (let i = 0; i < 15; i++) {
-    console.log(' '.repeat(14 - i) + '*'.repeat(4 + i * 2));
-}
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('What is the desired height of the pyramid? ', (answer) => {
+    var height = parseInt(answer, 10);
+    if (isNaN(height)) {
+        console.log('Incorrect input, try again');
+    } else {
+        drawPyramid(height);
+    }
+    rl.close();
+});
+rl.write('15');
+
+const drawPyramid = (height) => {
+    for (let i = 0; i < height; i++) {
+        console.log(' '.repeat(height - 1 - i) + '*'.repeat(4 + i * 2));
+    }
+};
