951ff55b4209fe7331d39770141e0456450c1a1a
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2018-11-08 18:31:32 +0200

    fix linter

--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_2/polyfillStar.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_2/polyfillStar.js
@@ -1,39 +1,39 @@
-const polyfill = (min, max) => {
-    for (let i =Math.min(min,max), total = Math.max(min,max); i <= total; i++) {
-        let curI = i.toString();
-        let curReverseI=curI.split("").reverse().join("");
-        if(curI===curReverseI) console.log(i);
-    }
-}
+const readline = require('readline');
 
-const readline = require('readline')
+const polyfill = (min, max) => {
+    for (let i = Math.min(min, max), total = Math.max(min, max); i <= total; i++) {
+        let curI = i.toString();
+        let curReverseI = curI.split('').reverse().join('');
+        if (curI === curReverseI) console.log(i);
+    }
+};
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+    input: process.stdin,
+    output: process.stdout
+});
 
 const question = (name, rl) => () => new Promise(resolve => {
-  rl.question(`Введите ${name}: `, resolve)
-})
+    rl.question(`Введите ${name}: `, resolve);
+});
 
 const answers = {
-  min:    '',
-  max:    ''
-}
+    min: '',
+    max: ''
+};
 
 const setAnswer = name => answer => {
-  answers[name] = parseInt(answer);
-}
+    answers[name] = parseInt(answer);
+};
 
-const askMin    = question('min', rl)
-const askMax    = question('max', rl)
+const askMin = question('min', rl);
+const askMax = question('max', rl);
 
 askMin()
-.then(setAnswer('min'))
-.then(askMax)
-.then(setAnswer('max'))
-.then(() => {
-  polyfill(answers.min,answers.max)  
-  rl.close()
-})
+    .then(setAnswer('min'))
+    .then(askMax)
+    .then(setAnswer('max'))
+    .then(() => {
+        polyfill(answers.min, answers.max);
+        rl.close();
+    });
--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_2/pyramidStar.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_2/pyramidStar.js
@@ -8,7 +8,7 @@ const rl = readline.createInterface({
 const drawPyramid = (height) => {
     for (let i = 0, total = height + 4, q = 0; i < total; i++) {
         if (i >= 4) {
-            console.log(" ".repeat(total - i - 1) + "#".repeat(i + q));
+            console.log(' '.repeat(total - i - 1) + '#'.repeat(i + q));
             q += 1;
         }
     }
@@ -16,7 +16,7 @@ const drawPyramid = (height) => {
 
 rl.question('PLEASE ENTER HEIGHT ', (answer) => {
     let height = parseInt(answer);
-    if (isNaN(height)) console.log("ERROR ", answer, " IS NOT A NUMBER !!!");
+    if (isNaN(height)) console.log('ERROR ', answer, ' IS NOT A NUMBER !!!');
     else drawPyramid(height);
     rl.close();
 });
