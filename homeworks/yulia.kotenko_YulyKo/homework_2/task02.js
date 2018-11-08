b7af1dfdb22445a1e1ccf1d554ac0f9e543055b2
y.kotenko <kyabi71@gmail.com>
2018-11-08 21:16:19 +0200

    hometask_2 task2: доданий запит верхньої границі

--- homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
@@ -1,10 +1,26 @@
+const readline = require('readline');
 
-for (let i = 500; i < 1000; i++) {
-    let nomber = i;
-    let newN = nomber.toString();
-    if (newN[0]===newN[1] && newN[1]===newN[2]) {
-        console.log(nomber);
-    }else if (newN[0]===newN[2] && newN[0]!==newN[1]) {
-        console.log(nomber);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('final nomber (0; 10000] ', (answer) => {
+     if(answer<=10000){
+        for (let i = answer2; i < answer; i++) {
+            let nomber = i;
+            let newN = nomber.toString();
+            if (newN[0]===newN[1] && newN[2]===undefined && newN[3]===undefined) {
+                console.log(nomber);
+            }else if (newN[0]===newN[2] && newN[0]!==newN[1] && newN[3]===undefined) {
+                console.log(nomber);
+            }else if(newN[0]===newN[2] && newN[2]===newN[1] && newN[3]===undefined){
+                console.log(nomber);
+            }else if (newN[0]===newN[1] && newN[1]===newN[2] && newN[2]===newN[3]) {
+                console.log(nomber);
+            }else if (newN[0]===newN[3] && newN[1]===newN[2] && newN[0]!==newN[1]) {
+                console.log(nomber);
+            }
+        }
     }
-}
+    rl.close();
+});
