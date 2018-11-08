b4addc4771ebadc894cbeb656017ee452f7d0626
Murayzes <murayzes@gmail.com>
2018-11-08 18:03:18 +0200

    homework_2_1

---
+++ homeworks/vladislav.reshetylo_Murayzes/homework_2/homework_2_1.js
@@ -0,0 +1,13 @@
+let height = 15; //height pyramid
+let space = " ";
+let hesh = "#";
+let amountSpace = height;
+let amountHesh = 2;
+
+for (let i = 0; i < height; i++){
+    amountHesh = amountHesh + 2;
+    amountSpace--;
+    let printHesh = hesh.repeat(amountHesh);
+    let printSpace = space.repeat(amountSpace);  
+    console.log(printSpace + printHesh);
+}
