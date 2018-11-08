79af578f50483b22cc5fb500d08427052a8b41dc
batteryfree <battery.free@gmail.com>
2018-11-08 22:37:52 +0200

    add space

--- homeworks/roman.vlasov_batteryfree/homework_2/palindromicnumbers.js
+++ homeworks/roman.vlasov_batteryfree/homework_2/palindromicnumbers.js
@@ -1,29 +1,29 @@
 const readline = require('readline');
 const readconsole = readline.createInterface(process.stdin, process.stdout);
 readconsole.question('Enter the number #1(>=500):',function(answer){
-  const num1 = +answer;
-  readconsole.question('Enter the number #2(<number #1):',function(answer){
-    const num2 = +answer;
-    if (typeof(num1)==='number' && typeof(num2)==='number' && num1<num2 && num1>=500){
-      palnum(num1,num2);
-    }else{
-      console.log('error data ;(');
-    }
-    readconsole.close();
-  });
+    const num1 = +answer;
+    readconsole.question('Enter the number #2(<number #1):',function(answer){
+        const num2 = +answer;
+        if (typeof(num1)==='number' && typeof(num2)==='number' && num1<num2 && num1>=500){
+            palnum(num1,num2);
+        }else{
+            console.log('error data ;(');
+        }
+        readconsole.close();
+    });
 });
 function palnum(i,j){
-  for (i ; i<=j; i++){
-    if (mirror(i.toString())){
-      console.log(i);
+    for (i ; i<=j; i++){
+        if (mirror(i.toString())){
+            console.log(i);
+        }
     }
-  }
 }
 function mirror(str){
-  if (str===str.split('').reverse().join('')){
-    return true;
-  }
-  else{
-    return false;
-  }
+    if (str===str.split('').reverse().join('')){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
