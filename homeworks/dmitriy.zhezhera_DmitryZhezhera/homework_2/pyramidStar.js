489ba8d8679f005b0af5f35e32b638ef08da0d5f
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2018-11-08 12:25:27 +0200

    pyramid done

---
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_2/pyramidStar.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let drawPyramid=(height)=>{
+    for(let i=0,total=height+4,q=0;i<total;i++){
+       if(i>=4){
+           console.log(" ".repeat(total-i-1)+"#".repeat(i+q)
+           //,q+1
+           );     
+           q+=1;
+       }
+    }
+};
+
+rl.question('PLEASE ENTER HEIGHT ', (answer) => {
+    let height = parseInt(answer);
+    if (isNaN(height)) console.log("ERROR ", answer, " IS NOT A NUMBER !!!");
+    else drawPyramid(height);
+    rl.close();
+});
