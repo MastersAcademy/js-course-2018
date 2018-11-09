b7c562da7fb6b1ad7fa694a0094e6020825a1125
Thunderian <thunderian_ck@ukr.net>
2018-11-09 10:55:37 +0200

    Homework_2
    Fixes

--- homeworks/pavel.fesak_thunderian/homework_2/pyramid.js
+++ homeworks/pavel.fesak_thunderian/homework_2/pyramid.js
@@ -4,20 +4,21 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-  rl.question('Please enter height of your pyramid', (answer) => {
+rl.question('Please enter height of your pyramid', answer => {
     if (Number.isNaN(Number(answer))) {
         console.log('Invalid data, please enter a number');
     } else {
         const whitespace = ' ';
         const symbol = '#';
         let startNumber = 4;
-         for (let i = 0; i < answer; i++) {
-            
-            console.log(whitespace.repeat(answer - (i + 1)) + symbol.repeat(startNumber));
+        for (let i = 0; i < answer; i++) {
+            console.log(
+                whitespace.repeat(answer - (i + 1)) + symbol.repeat(startNumber)
+            );
             startNumber = startNumber + 2;
         }
     }
-     rl.close();
+    rl.close();
 });
 
 
