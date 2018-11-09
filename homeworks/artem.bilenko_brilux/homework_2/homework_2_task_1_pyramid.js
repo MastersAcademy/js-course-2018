38e850d8c030ce2b54be422d834070cc8eae4ddc
brilux <aavv22oo8ntfku>
2018-11-09 17:00:24 +0200

    fixes eslint, error handler fixes

--- homeworks/artem.bilenko_brilux/homework_2/homework_2_task_1_pyramid.js
+++ homeworks/artem.bilenko_brilux/homework_2/homework_2_task_1_pyramid.js
@@ -2,27 +2,25 @@ const readline = require('readline');
 
 const checkHeight = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
-checkHeight.question('Enter height of pyramid: ', height => {
+checkHeight.question('Enter height of pyramid: ', (height) => {
     checkHeight.close();
-    if (!isNaN(height)) {
-        let emptiness = ' ';
-        let symbol = '#';
+    if (!Number.isNaN(height)) {
+        const emptiness = ' ';
+        const symbol = '#';
         let amountEmptiness = 50;
         let amountSymbol = 2;
 
         for (let i = 0; i < height; i++) {
-            amountSymbol = amountSymbol + 2;
+            amountSymbol += 2;
             amountEmptiness--;
-            let printSymbol = symbol.repeat(amountSymbol);
-            let printEmptiness = emptiness.repeat(amountEmptiness);
+            const printSymbol = symbol.repeat(amountSymbol);
+            const printEmptiness = emptiness.repeat(amountEmptiness);
             console.log(printEmptiness + printSymbol);
         }
     } else {
         console.log('Please, enter a height of pyramid. It must be a number. For example: 15');
     }
 });
-
-
--- homeworks/artem.bilenko_brilux/homework_2/homework_2_task_2_numbers.js
+++ homeworks/artem.bilenko_brilux/homework_2/homework_2_task_2_numbers.js
@@ -2,24 +2,27 @@ const readline = require('readline');
 
 const checkDiapason = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
-checkDiapason.question('Enter diapason(from 500 to 1000). For example(600,700): ', diapason => {
+checkDiapason.question('Enter diapason(from 500 to 1000). For example(600,700): ', (diapason) => {
     checkDiapason.close();
-    let arrDiapason = diapason.split(',');
-    if (arrDiapason[0] < 500 || arrDiapason[1] > 1000 || arrDiapason[0] === arrDiapason[1]) {
+    const arrDiapason = diapason.split(',');
+    const diapasonStart = Number(arrDiapason[0]);
+    const diapasonEnd = Number(arrDiapason[1]);
+    const checkExecution = !Number.isNaN(arrDiapason[0]) && !Number.isNaN(arrDiapason[1]);
+    const errCheckDiap = diapasonStart < 500 || diapasonEnd > 1000 || diapasonStart >= diapasonEnd;
+    const errCheckEntry = Number.isNaN(diapasonStart) || Number.isNaN(diapasonEnd);
+    if (errCheckDiap || errCheckEntry) {
         console.log('Enter please valid diapason(from 500 to 1000). For example(600,700):');
-        return;
-    }
-    if (!isNaN(arrDiapason[0]) && !isNaN(arrDiapason[1])) {
-        let length = (arrDiapason[1] - (arrDiapason[0] - 1)) / 10 - 1;
-        let number = (arrDiapason[0] - 1).toString();
-        let arr = number.split('');
+    } else if (checkExecution) {
+        const length = (diapasonEnd - (diapasonStart - 1)) / 10 - 1;
+        const number = (diapasonStart - 1).toString();
+        const arrNumber = number.split('');
 
-        let leftNumber = arr[0];
-        let centralNumber = arr[1];
-        let rightNumber = arr[2];
+        let leftNumber = arrNumber[0];
+        let centralNumber = arrNumber[1];
+        let rightNumber = arrNumber[2];
 
         for (let i = 0; i < length; i++) {
             rightNumber = leftNumber;
@@ -31,9 +34,5 @@ checkDiapason.question('Enter diapason(from 500 to 1000). For example(600,700): 
             }
             console.log(leftNumber.toString() + centralNumber + rightNumber);
         }
-    } else {
-        console.log('Please, enter a valid diapason. For example: 500, 1000');
     }
 });
-
-
