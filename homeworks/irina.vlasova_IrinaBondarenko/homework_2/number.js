0d9af75e892fbdcb62e827445b70226cee4d3088
IrinaBondarenko <gukibn@gmail.com>
2018-11-09 23:54:03 +0200

    spaces were added

--- homeworks/irina.vlasova_IrinaBondarenko/homework_2/number.js
+++ homeworks/irina.vlasova_IrinaBondarenko/homework_2/number.js
@@ -1,6 +1,6 @@
 for (let i = 500; i <= 1000; i++) {
-    let s = i.toString();
-    let s1 = s.split('').reverse().join('');
+    const s = i.toString();
+    const s1 = s.split('').reverse().join('');
     if (s === s1) {
         console.log(i);
     }
--- homeworks/irina.vlasova_IrinaBondarenko/homework_2/pyramida.js
+++ homeworks/irina.vlasova_IrinaBondarenko/homework_2/pyramida.js
@@ -1,5 +1,5 @@
-let space = ' ';
-let symbol = '#';
-for (let i=1; i<=15; i++) {
-    console.log (space.repeat(15-i) + symbol.repeat(i*2+2));
+const space = ' ';
+const symbol = '#';
+for (let i = 1; i <= 5; i++) {
+    console.log(space.repeat(15 - i) + symbol.repeat(i * 2 + 2));
 }
