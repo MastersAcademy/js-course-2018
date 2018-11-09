8bbfac6a23188ee8065f493912be5a08c5917930
batteryfree <battery.free@gmail.com>
2018-11-09 16:23:58 +0200

    homework_1

--- homeworks/roman.vlasov_batteryfree/homework_2/drawpyramid.js
+++ homeworks/roman.vlasov_batteryfree/homework_2/drawpyramid.js
@@ -1,13 +1,9 @@
 const readline = require('readline');
 const readconsole = readline.createInterface(process.stdin, process.stdout);
-readconsole.question('Enter the height of the pyramid:',function(answer){
-    drawpyrapid(answer);
+readconsole.question('Enter the height of the pyramid:', answer => {
+    const s = ' ', s1 = '#';
+    for (let i = 1; i <= answer; i++) {
+        console.log(s.repeat(answer - i) + s1.repeat(2 * i + 2));
+    }
     readconsole.close();
 });
-function drawpyrapid(hpyramid){
-    let s=' ', s1='#';
-    for (let i=1; i<=hpyramid; i++){
-        console.log(s.repeat(hpyramid-i)+s1.repeat(2*i+2));
-    }
-}
-
--- homeworks/roman.vlasov_batteryfree/homework_2/palindromicnumbers.js
+++ homeworks/roman.vlasov_batteryfree/homework_2/palindromicnumbers.js
@@ -1,29 +1,24 @@
 const readline = require('readline');
 const readconsole = readline.createInterface(process.stdin, process.stdout);
-readconsole.question('Enter the number #1(>=500):',function(answer){
+readconsole.question('Enter the number #1(>=500):', answer => {
     const num1 = +answer;
-    readconsole.question('Enter the number #2(<number #1):',function(answer){
+    readconsole.question('Enter the number #2(<number #1):', answer => {
         const num2 = +answer;
-        if (typeof(num1)==='number' && typeof(num2)==='number' && num1<num2 && num1>=500){
-            palnum(num1,num2);
-        }else{
+        if (typeof(num1) === 'number' && typeof(num2) === 'number' && num1 < num2 && num1 >= 500) {
+            palnum(num1, num2);
+        } else {
             console.log('error data ;(');
         }
         readconsole.close();
     });
 });
-function palnum(i,j){
-    for (i ; i<=j; i++){
-        if (mirror(i.toString())){
+function palnum(i, j) {
+    for (i; i <= j; i++) {
+        if (mirror(i.toString())) {
             console.log(i);
         }
     }
 }
-function mirror(str){
-    if (str===str.split('').reverse().join('')){
-        return true;
-    }
-    else{
-        return false;
-    }
+function mirror(str) {
+    return str === str.split('').reverse().join('');
 }
