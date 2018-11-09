3dc7dcdb99785eb52bf0ab7925e9b0a56acd8435
junyaa <internet_ck@ukr.net>
2018-11-09 16:19:03 +0200

    some fixes

--- homeworks/yuriy.piddubnyak_junyaa/homework_2/task_1.js
+++ homeworks/yuriy.piddubnyak_junyaa/homework_2/task_1.js
@@ -2,6 +2,7 @@ let s;
 let i = '##';
 const p = ' ';
 let z;
+
 const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
@@ -13,9 +14,9 @@ rl.question('Введите количество строк?\n', (x) => {
         for (s = 0; s < x; s++) {
             i += '##';
             z -= 1;
-            console.log(p.repeat(z) + i); }
-        rl.close();
-    }
+            console.log(p.repeat(z) + i); 
+        }
+        rl.close();}
     else {
         console.log('Ошибка, количество строк должно быть числом!');
         rl.close();
