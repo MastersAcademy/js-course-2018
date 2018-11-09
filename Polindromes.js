be47a193b18e4026b6d9304e3fd489588bc2120c
Alexander Chunarev <alexanderchunarov1999@gmail.com>
2018-11-10 00:06:14 +0200

    Changed second task

--- Polindromes.js
+++ Polindromes.js
@@ -1,26 +1,11 @@
-const readline = require('readline');
-
-const enterDigit = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
 function checkPalindrom(str) {
     return str === str.split('').reverse().join('');
 }
 
-enterDigit.question('Enter the digit in available diapason, from 500 to 1000: ', (digit) => {
-    enterDigit.close();
-    if (digit >= 500 && digit <= 1000){
-        if (checkPalindrom(digit)){
-            console.log("The digit " + digit + " is palindrome");
-        } else {
-            console.log("The digit " + digit + " is not palindrome");
-        }
-
-    } else {
-        console.log("Wrong input!")
+for (let i = 500; i <= 1000; i++){
+    if (checkPalindrom(i.toString())){
+        console.log("The digit " + i + " is palindrome");
     }
-});
+}
 
 
