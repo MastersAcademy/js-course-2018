7e1bb3e3476054ca8cd2bcb9d1af643a8577ad5d
junyaa <internet_ck@ukr.net>
2018-11-09 10:55:04 +0200

    task_2.js

--- homeworks/yuriy.piddubnyak_junyaa/homework_2/task_2.js
+++ homeworks/yuriy.piddubnyak_junyaa/homework_2/task_2.js
@@ -2,13 +2,19 @@ var s = 500;
 var e = 1000;
 var x;
 var i;
+var z;
 
-for (x = s; x<=e ; x++){
+for (x = s; x<=e; x++){
 	for (i = s; i<=e; i++){
 
 if ((x.toString().split('').reverse().join('')) == i){
-	console.log('Палиндромы: '+x, '-',i)
+	
+	
+	console.log('Перевертыш: '+x, '-',i)
+	console.log('Палиндром:',x%i == 0,i);
 	}
-		}
+	
+			
+	}
 	
 }
