5137311268e59117bbf5d644cf8fb5f8a8da572f
borodkoleg <borodkoleg@gmail.com>
2018-11-10 17:24:48 +0200

    divided into two files

--- homeworks/oleg.borodko_olegborodko/homework_2/task_1.js
+++ homeworks/oleg.borodko_olegborodko/homework_2/task_1.js
@@ -7,12 +7,3 @@ for (let i = 1 + pyramidOffset; i <= pyramidStep; i++) {
     const bodyPart = '#'.repeat(i) + '#'.repeat(i - pyramidOffset);
     console.log(spacePart + bodyPart);
 }
-
-console.log();
-
-for (let i = 500; i <= 1000; i++) {
-    const line = i.toString();
-    if (line[line.length - 1] === line[0]) {
-        console.log(line);
-    }
-}
