f185c414476f319beed4e2fe65ee161bf758c819
Ivan-Artemenko <44719206+Ivan-Artemenko@users.noreply.github.com>
2018-11-10 07:58:18 +0200

    Improved homework_2_2
    Improved homework_2_2. I hope you enjoy it.

--- homeworks/Ivan.Artemenko_IvanArtemenko/homework_2/homework_2_2.js
+++ homeworks/Ivan.Artemenko_IvanArtemenko/homework_2/homework_2_2.js
@@ -1,9 +1,21 @@
-for (var i = 500; i<1000; i++){
-	var a, b, c;
-	a = ((i/100)%10);
-	b = ((i/10)%10);
-	c = (i%10);
-	if(Math.floor(a) == Math.round(c)){
-		console.log(Math.floor(a),Math.floor(b),c)
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+rl.question('Enter the first number from 500 to 1000 ', (number_1) => {
+	rl.question('Enter the second number from 500 to 1000 ', (number_2) => {
+		let a = number_1-0;
+		let b = number_2-1;
+if(a >= 500  && b < 1000){		
+	for (let i = a; i <= b; i++){
+		let q = ((i/100)%10);
+		let w = (i%10);
+		if(Math.floor(q) === Math.round(w)){
+		console.log(i);	
+		}
 	}
-}
+}else console.log('Invalid data enter a number from 500 to 1000');
+rl.close();
+});
+});
