a833f6fea95568ba51a353707a6962a73b46b2d5
YulyKo <julia.kotenko.forstudy@gmail.com>
2018-11-10 16:39:40 +0200

    homework_2 task2: beginning from 1

--- homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
@@ -4,8 +4,8 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question('final nomber (0; 10000] ', (answer) => {
-     for (let i = 0; i < answer; i++) {
+rl.question('final nomber ', (answer) => {
+     for (let i = 1; i < answer; i++) {
       let newNumber = i.toString().split('').reverse().join('');
        let rev = i.toString();
        if (newNumber === rev) {
