f61938d2d5ac0460bbe182f762c8a8f19b88766a
Марина Носенко <44245669+MN12345@users.noreply.github.com>
2018-11-11 01:04:38 +0200

    Update piramida2.js

--- homeworks/marina.nosenko_MN12345/homework_2/piramida2.js
+++ homeworks/marina.nosenko_MN12345/homework_2/piramida2.js
@@ -3,6 +3,6 @@ const space = ' '; // пробел
 const str = '#'; // символ 
 let y = 1; 
 while (y <= i) {
-    console.log (space.repeat(i - y) + str.repeat(y * 2 + 2)); 
+    console.log (space.repeat(i-y) + str.repeat(y*2+2)); 
     y++; 
 }
