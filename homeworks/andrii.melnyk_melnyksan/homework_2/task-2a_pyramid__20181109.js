19510b6522a8d32e9b7b6d2415b81002fbdf91d1
melnyksan <andriimelnyk986@gmail.com>
2018-11-10 08:50:27 +0200

    _melnyksan/homework_2__fixed__task-2a_pyramid_task-2b_palindromes__20181110

--- homeworks/andrii.melnyk_melnyksan/homework_2/task-2a_pyramid__20181109.js
+++ homeworks/andrii.melnyk_melnyksan/homework_2/task-2a_pyramid__20181109.js
@@ -1,9 +1,11 @@
-var maxValue = 15, strSpace, strHash;
+const maxValue = 15;
+let strSpace;
+let strHash;
 
 for (let i = 0; i < maxValue; i++) {
-    strSpace = "";
-    strHash = "###";
-    for (let j = 0; j < maxValue - i; j++) strSpace += " ";
-    for (let j = 0; j < 2 * i + 1; j++) strHash += "#";
+    strSpace = '';
+    strHash = '###';
+    for (let j = 0; j < maxValue - i; j++) strSpace += ' ';
+    for (let j = 0; j < 2 * i + 1; j++) strHash += '#';
     console.log(strSpace + strHash);
 }
--- homeworks/andrii.melnyk_melnyksan/homework_2/task-2b_palindromes__20181109.js
+++ homeworks/andrii.melnyk_melnyksan/homework_2/task-2b_palindromes__20181109.js
@@ -1,7 +1,7 @@
-var palindromes = [];
+const palindromes = [];
 
 for (let i = 500; i < 1000; i++) {
-    if(i.toString() === i.toString().split("").reverse().join("")) {
+    if (i.toString() === i.toString().split('').reverse().join('')) {
         palindromes.push(i);
     }
 }
