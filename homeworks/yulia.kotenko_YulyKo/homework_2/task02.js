32aaa4642fa70edb82fddad811b6f3c26f61f8aa
YulyKo <julia.kotenko.forstudy@gmail.com>
2018-11-10 18:05:00 +0200

    homework_2 task2: add request max value

--- homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
@@ -4,13 +4,17 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question('final nomber ', (answer) => {
-     for (let i = 1; i < answer; i++) {
-      let newNumber = i.toString().split('').reverse().join('');
-       let rev = i.toString();
-       if (newNumber === rev) {
-         console.log(i);
-       }
-     }
-    rl.close();
+rl.question('first nomber ', (answer) => {
+  r1.question('final number ', (answer2) => {
+      for (answer; answer < answer2; answer++) {
+
+        let newNumber = answer.toString().split('').reverse().join('');
+        let rev = answer.toString();
+        
+        if (newNumber === rev) {
+           console.log(answer);
+        }
+      }
+      rl.close();
+   });
 });
