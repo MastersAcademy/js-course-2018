06f9c601044bdb5357eb1c9651525411f80d2c9f
borodkoleg <borodkoleg@gmail.com>
2018-11-10 12:02:29 +0200

    fix

--- homeworks/oleg.borodko_olegborodko/homework_2/index.js
+++ homeworks/oleg.borodko_olegborodko/homework_2/index.js
@@ -1,5 +1,18 @@
-require('./task1');
+const pyramidLength = 15; // from 1 to n
+const pyramidOffset = 2; // from -1 to n
+const pyramidStep = pyramidLength + pyramidOffset;
+
+for (let i = 1 + pyramidOffset; i <= pyramidStep; i++) {
+    const spacePart = ' '.repeat(pyramidStep - i);
+    const bodyPart = '#'.repeat(i) + '#'.repeat(i - pyramidOffset);
+    console.log(spacePart + bodyPart);
+}
 
 console.log();
 
-require('./task2');
+for (let i = 500; i <= 1000; i++) {
+    const line = i.toString();
+    if (line[line.length - 1] === line[0]) {
+        console.log(line);
+    }
+}
