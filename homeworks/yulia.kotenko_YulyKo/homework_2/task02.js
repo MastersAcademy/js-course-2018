70c0071f0bd48f5fb1852b493835d10b8928e563
YulyKo <julia.kotenko.forstudy@gmail.com>
2018-11-10 18:32:58 +0200

    homework_2 task2: try easy level

--- homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
@@ -1,20 +1,8 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-rl.question('first nomber ', (answer) => {
-  r1.question('final number ', (answer2) => {
-      for (answer; answer < answer2; answer++) {
-
-        let newNumber = answer.toString().split('').reverse().join('');
-        let rev = answer.toString();
+for (let i = 500; i < 1000; i++) {
+  let newNumber = i.toString().split('').reverse().join('');
+        let rev = i.toString();
         
         if (newNumber === rev) {
-           console.log(answer);
+           console.log(i);
         }
-      }
-      rl.close();
-   });
-});
+} 
