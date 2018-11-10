cd6bdeddf3413318444a90c83136e8b4ab97dbe3
Yaroslav <yaroslav.belinskiy@qa-testlab.com>
2018-11-10 18:48:58 +0200

    finished homeworks, but needed review of the 1 task

--- homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
+++ homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
@@ -1,30 +1,58 @@
-const READLINE = require('readline');
-const rl = READLINE.createInterface(process.stdin, process.stdout);
+/*I know it could be better, especially with readline functionality, but I didn't find out how to make it correctly via 'rl.on' to ask it in a loop :(
+Also I dont know why readline throws an error, my pyramid works without it in two variants.*/
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+let parsed;
+let buildingPyramid = function() {
+    rl.question('Enter the height of pyramid: ', function(height) {
+        parsed = parseInt(height);
+        if(typeof(parsed) === 'number' && height % 1 === 0) {
+            console.log('Here you are!');
+            let initialStroke = '##';
+            let pyramidHeight = height;
+            let iterations = height + 1;
+            let space = ' ';
+            let amountOfSpace = 0;
+            for(let i = 2; i <= iterations; i++) {
+                let stroke = initialStroke.repeat(i);
+                amountOfSpace++;
+                if(amountOfSpace !== pyramidHeight) {
+                    let spaceBefore = space.repeat(pyramidHeight - amountOfSpace); 
+                    // let amountOfSpace = ((pyramidHeight * 2 + 2) - stroke.length) / 2;
+                    // let spaceBefore = space.repeat(amountOfSpace); 
+                    console.log(`${spaceBefore}${stroke}`);
+                } else {
+                    console.log(`${stroke}`);
+                }
+            }
+            process.exit();
+        } else {
+            console.log('Please, enter only integers!');
+            buildingPyramid();
+        }
+    });
+};
+buildingPyramid();
 
-rl.question('Enter the height of pyramid: ', function(height) {
-    
-    let parsed = parseInt(height);
-    
-    if(typeof(parsed) === 'number' && height % 1 === 0) {
-        var initialStroke = '##';
-        var pyramidHeight = height;
-        var space = ' ';
-        for(var i = 2; i <= pyramidHeight + 1; i++) {
-            var stroke = initialStroke.repeat(i);
-            var amountOfSpace = ((pyramidHeight * 2 + 2) - stroke.length) / 2;
-            var spaceBefore = space.repeat(amountOfSpace); 
-            console.log(`${spaceBefore}${stroke}`);
-        };
-        console.log('Here you are!');
-        process.exit();
+//working variant without readline
+
+/*
+console.log('Here you are!');
+let initialStroke = '##';
+let pyramidHeight = 15;
+let iterations = 15 + 1;
+let space = ' ';
+let amountOfSpace = 0;
+for(let i = 2; i <= iterations; i++) {
+    let stroke = initialStroke.repeat(i);
+    amountOfSpace++;
+    if(amountOfSpace !== pyramidHeight) {
+        let spaceBefore = space.repeat(pyramidHeight - amountOfSpace); 
+        // let amountOfSpace = ((pyramidHeight * 2 + 2) - stroke.length) / 2;
+        // let spaceBefore = space.repeat(amountOfSpace); 
+        console.log(`${spaceBefore}${stroke}`);
     } else {
-        rl.setPrompt('Please enter only intengers: ');
-        rl.prompt();
-        rl.on('line', function() {
-
-        });
-    };
-
-});
-
-
+        console.log(`${stroke}`);
+    }
+}
+*/
--- homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/secondScenario.js
+++ homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/secondScenario.js
@@ -1,34 +1,37 @@
-const READLINE = require('readline');
-const rl = READLINE.createInterface(process.stdin, process.stdout);
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
 
-rl.question('Enter Min and Max values of array: ', function(min, max) {
-
-    let parsedMin = parseInt(min);
-    let parsedMax = parseInt(max);
-    if((typeof(parsedMin) === 'number' && parsedMin % 1 === 0) && (typeof(parsedMax) === 'number' && parsedMax % 1 === 0)) {
-        let minValue = min;
-        let maxValue = max;
-        let array = [];
-        let sortedArray = [];
-        for(let i = minValue; i <= maxValue; i++) {
-            array.push(i);
-        }
-        array.forEach(e => {
-            var stringify = e.toString();
-            var splited = stringify.split('');
-            var tempArray = [];
-            splited.forEach(el => {
-                tempArray.unshift(el);
-                var specularArray = tempArray.join('');
-                if(specularArray === stringify) {
-                    sortedArray.push(specularArray);
-                };
+let filterNumbers = function() {
+    rl.question('Enter Min and Max values of array (Example: min,max): ', function(values) {
+        let arrayOfValues = values.split(',');
+        let parsedMin = parseInt(arrayOfValues[0]);
+        let parsedMax = parseInt(arrayOfValues[1]);
+        if((typeof(parsedMin) === 'number' && parsedMin % 1 === 0) && (typeof(parsedMax) === 'number' && parsedMax % 1 === 0)) {
+            let minValue = parsedMin;
+            let maxValue = parsedMax;
+            let array = [];
+            let sortedArray = [];
+            for(let i = minValue; i <= maxValue; i++) {
+                array.push(i);
+            }
+            array.forEach(e => {
+                let stringify = e.toString();
+                let splited = stringify.split('');
+                let tempArray = [];
+                splited.forEach(el => {
+                    tempArray.unshift(el);
+                    let specularArray = tempArray.join('');
+                    if(specularArray === stringify) {
+                        sortedArray.push(specularArray);
+                    }
+                });
             });
-        });
-        console.log(sortedArray);
-        process.exit();
-    } else {
-        console.log('Please enter only intengers')
-    };   
-    
-});
+            console.log(sortedArray);
+            process.exit();
+        } else {
+            console.log('Please enter only intengers');
+            filterNumbers();
+        }     
+    });
+};
+filterNumbers();
