ec30da361923cc7f5bed9e9440e124816ef88d41
zelinka <2pacshac@gmail.com>
2018-11-10 22:48:27 +0200

    Den.Sokurenko.homework_2

--- homeworks/Den.Sokurenko_Den4ik08/homework_2/piramida.js
+++ homeworks/Den.Sokurenko_Den4ik08/homework_2/piramida.js
@@ -1,11 +1,11 @@
- 1  function pyramid(n) {
- 2       
- 3        for(let i=2; i<= n; i++){
- 4  
- 5          let str = ' '.repeat(n-i);
- 6  
- 7          let str2 = '#'. repeat(i*2)
- 8       
- 9          console.log(str + str2 + str);
-10        }
-11      } 
+  1  function pyramid(n) {
+  2       
+  3        for(let i=2; i<= n; i++){
+  4  
+  5          let str = ' '.repeat(n-i);
+  6  
+  7          let str2 = '#'. repeat(i*2)
+  8       
+  9          console.log(str + str2 + str);
+ 10        }
+ 11      } 
