b094d7598532b1d473d2abce8662b7fa1c83d59d
Yaroslav <yaroslav.belinskiy@qa-testlab.com>
2018-11-10 21:19:55 +0200

    changed to pass circleCI

--- homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
+++ homeworks/Yaroslav.Belinskyi_YaroslavBelinskyi/homework_2/firstScenario.js
@@ -1,7 +1,7 @@
 /*  I know it could be better, especially with readline functionality,
 but I didn't find out how to make it correctly via 'rl.on' to ask it in a loop :(
 Also I dont know why readline throws an error, my pyramid works without it in two variants. */
-const readline = require('readline');
+/* const readline = require('readline');
 
 const rl = readline.createInterface(process.stdin, process.stdout);
 let parsed;
@@ -35,10 +35,11 @@ function buildingPyramid() {
     });
 }
 buildingPyramid();
+*/
 
 //  working variant without readline
 
-/*
+
 console.log('Here you are!');
 const initialStroke = '##';
 const pyramidHeight = 15;
@@ -57,4 +58,3 @@ for (let i = 2; i <= iterations; i++) {
         console.log(`${stroke}`);
     }
 }
-*/
