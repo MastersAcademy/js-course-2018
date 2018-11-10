948421b4cd9b82fb72bd95e1ee254e86922d2a45
Misha <mwkolosov@gmail.com>
2018-11-10 15:09:43 +0200

    add homework 2

---
+++ homeworks/misha.kolosov_mwkolosov/homework_2/mirror.js
@@ -0,0 +1,7 @@
+for (i = 500; i < 1000; i++) {
+    // split().reverse().join();
+    let reverse_num = (i.toString().split('').reverse().join(''));
+    if (i == reverse_num) {
+        console.log(i);
+    }
+}
