434f15fd15c78f713b3ac6cc40ee399a3e3466d6
Darknel <Jenykushnir@gmail.com>
2018-11-11 01:25:13 +0200

    Last update v2.0

--- homeworks/evgen.kushnir_darknel/homework_2/numbers.js
+++ homeworks/evgen.kushnir_darknel/homework_2/numbers.js
@@ -2,6 +2,6 @@ for (let test = 500; test < 1000; test++) {
     const numberTest = test.toString().split('').reverse().join('');
     const numberTestCheck = test.toString();
     if (numberTest === numberTestCheck) {
-        console.log(`${test} ${typeof test}`);
+        console.log(`${test}`);
     }
 }
