7acdebb89bed07edbec6ebd3797027ab9ae14a06
Yaroslav Danilik <yaroslavdanilik@gmail.com>
2018-11-10 12:06:42 +0200

    homework_2

---
+++ homeworks/yaroslav.danilik_MrTraceless/homework_2/mirrorInt.js
@@ -0,0 +1,10 @@
+function input(N) { 
+    let arr = N.toString().split(''); 
+    let reverse = N.toString().split('').reverse().join(''); 
+    arr[arr.length - 1] = reverse; 
+   
+    console.log(arr.join('')); 
+  } 
+   
+  for(let i = 500; i <= 1000; i++) 
+    input(i);
