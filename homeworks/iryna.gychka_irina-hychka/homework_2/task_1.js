bf29aabed141c1b4a66bb4ce1217e33611e1587b
Iryna Gychka <irishchest@gmail.com>
2018-11-10 14:54:44 +0200

    fixed code style

--- homeworks/iryna.gychka_irina-hychka/homework_2/task_1.js
+++ homeworks/iryna.gychka_irina-hychka/homework_2/task_1.js
@@ -1,11 +1,11 @@
 // Generates the pyramid
-function generatePyramid (maxRowsNumber) {
-    let spaces = ' ';
-    let hashTags = '#';
-    let maxLength = maxRowsNumber * 2 + 2;
+function generatePyramid(maxRowsNumber) {
+    const spaces = ' ';
+    const hashTags = '#';
+    const maxLength = maxRowsNumber * 2 + 2;
     for (let linesNumber = 1; linesNumber <= maxRowsNumber; linesNumber++) {
-        let hashTagLength = linesNumber * 2 + 2;
-        let spacesLength = (maxLength - hashTagLength) / 2;
+        const hashTagLength = linesNumber * 2 + 2;
+        const spacesLength = (maxLength - hashTagLength) / 2;
         console.log(spaces.repeat(spacesLength) + hashTags.repeat(hashTagLength) + spaces.repeat(spacesLength));
     }
 }
@@ -13,23 +13,20 @@ function generatePyramid (maxRowsNumber) {
 // Gets pyramids height and validate it
 const readline = require('readline');
 
-const rl = readline.createInterface ({
-	input: process.stdin,
-	output: process.stdout
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
 });
 
 console.log('Insert the pyramids height: ');
 
 rl.on('line', (input) => {
     let pyramidsHeight = input;
-    if (+pyramidsHeight && pyramidsHeight !== NaN && pyramidsHeight > 0) {
+    if (+pyramidsHeight && Number.isNaN(pyramidsHeight) && pyramidsHeight > 0) {
         pyramidsHeight = Math.floor(pyramidsHeight);
         generatePyramid(pyramidsHeight);
         rl.close();
-    }
-    else {
+    } else {
         console.log('Insert a positive number');
     }
 });
-
-
--- homeworks/iryna.gychka_irina-hychka/homework_2/task_2.js
+++ homeworks/iryna.gychka_irina-hychka/homework_2/task_2.js
@@ -1,14 +1,5 @@
-// Displays in the terminal mirror numbers
-function mirrowNumbers (startNumber, endNumber) {
-    for (let number = startNumber; number <= endNumber; number++) {
-        if (isPalindrome(number.toString())) {
-            console.log(number);
-        }
-    }
-}
-
 // Checks if a number is a palindrome
-function isPalindrome (number) {
+function isPalindrome(number) {
     let numberLength = number.length;
     let mirrowNumber = '';
     while (numberLength > 0) {
@@ -18,34 +9,43 @@ function isPalindrome (number) {
     return number === mirrowNumber;
 }
 
+// Displays in the terminal mirror numbers
+function mirrowNumbers(startNumber, endNumber) {
+    for (let number = startNumber; number <= endNumber; number++) {
+        if (isPalindrome(number.toString())) {
+            console.log(number);
+        }
+    }
+}
+
+
+
 // Input data processing and its validation
 const readline = require('readline');
 
 const rl = readline.createInterface ({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
 let startNumber;
 let endNumber;
 console.log('Insert the start number: ');
 
-rl.on('line', (input) => {
-    input = +input
-    if ( input !== NaN && Number.isInteger(input) && input > 0) {
+rl.on('line', (line) => {
+    const input = +line;
+    if (Number.isNaN(input) && Number.isInteger(input) && input > 0) {
         if (startNumber === undefined) {
             startNumber = input;
             console.log('Insert the end number: ');
+        } else if (input <= startNumber) {
+            console.log('Must be greater than start number');
         } else {
-            if (input <= startNumber) {
-                console.log("Must be greater than start number");
-            } else {
-                endNumber = input;
-                mirrowNumbers(startNumber, endNumber);
-                rl.close();
-            }
+            endNumber = input;
+            mirrowNumbers(startNumber, endNumber);
+            rl.close();
         }
     } else {
-        console.log("Must be a positive integer, try again");
+        console.log('Must be a positive integer, try again');
     }
 });
