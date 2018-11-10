6f2a1550bc9aee50080c972c6ac65b98ae3057bf
dzhygil <dzhygil.oleg@gmail.com>
2018-11-10 23:51:01 +0200

    homework_2

--- homeworks/oleg.dzhygil_dzhygil/homework_2/number.js
+++ homeworks/oleg.dzhygil_dzhygil/homework_2/number.js
@@ -1,7 +1,7 @@
-for (let i = 500; i < 1000; i++) {
-    const str = i.toString();
+for (let mirrornum = 500; mirrornum < 1000; mirrornum++) {
+    const str = mirrornum.toString();
     const reverse = str.split('').reverse().join('');
     if (str === reverse) {
-        console.log(i);
+        console.log(mirrornum);
     }
 }
