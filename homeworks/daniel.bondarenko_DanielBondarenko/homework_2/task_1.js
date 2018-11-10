e4683522dc894d4b0538d6085776e33c15535305
Daniel Bondarenko <daniel.bondarenko@terria.com>
2018-11-10 15:14:16 +0200

    Fixing homework_2

--- homeworks/daniel.bondarenko_DanielBondarenko/homework_2/task_1.js
+++ homeworks/daniel.bondarenko_DanielBondarenko/homework_2/task_1.js
@@ -1,21 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
 function outputThePyramid(height) {
     // typeof height from readline is always string
-    if (parseInt(height) === Number(height)) {
+    if (parseInt(height, 10) === Number(height)) {
         for (let i = 0; i < height; i++) {
-            console.log((' ').repeat(height - i - 1) + ('#').repeat(4 + 2 * i) + (' ').repeat(height - i - 1));
+            console.log(' '.repeat(height - i - 1) + '#'.repeat(4 + 2 * i));
         }
     } else {
         console.log('Please, enter a number');
     }
-    return 0;
 }
 
-const readline = require('readline');
-
-const rl = readline.createInterface({input: process.stdin, output: process.stdout});
-
 rl.question('Please, enter a pyramid height ', (height) => {
-    console.log(outputThePyramid(height));
+    outputThePyramid(height);
     rl.close();
 });
-
--- homeworks/daniel.bondarenko_DanielBondarenko/homework_2/task_2.js
+++ homeworks/daniel.bondarenko_DanielBondarenko/homework_2/task_2.js
@@ -1,6 +1,11 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
 function palindrome(from, to) {
-    if (parseInt(from) === Number(from) && parseInt(to) === Number(to) && parseInt(from) <= parseInt(to)) {
-        for (let i = parseInt(from); i <= parseInt(to); i++) {
+    if (parseInt(from, 10) === Number(from) && parseInt(to, 10) === Number(to)
+        && parseInt(from, 10) <= parseInt(to, 10)) {
+        for (let i = parseInt(from, 10); i <= parseInt(to, 10); i++) {
             if (i.toString().split('').reverse().join('') === i.toString()) {
                 console.log(i);
             }
@@ -8,17 +13,11 @@ function palindrome(from, to) {
     } else {
         console.log('Please enter a valid numbers');
     }
-    return 0;
 }
-
-
-const readline = require('readline');
-
-const rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
 rl.question('Please enter the first number: ', (from) => {
     rl.question('Please enter the second number: ', (to) => {
-        console.log(palindrome(from, to));
+        palindrome(from, to);
         rl.close();
     });
 });
