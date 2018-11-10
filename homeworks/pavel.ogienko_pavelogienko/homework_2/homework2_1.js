8bcce597205c5fe9a8aba7f46e936bb93dec934e
Michael Teply <redgeekpanda@gmail.com>
2018-11-10 20:44:16 +0200

    Revert "homework_2"

--- homeworks/pavel.ogienko_pavelogienko/homework_2/homework2_1.js
+++
@@ -1,5 +0,0 @@
-const space = ' ';
-const symbol = '#';
-for (let i = 1; i <= 15; i++) {
-    console.log(space.repeat(15 - i) + symbol.repeat(i * 2 + 2));
-}
--- homeworks/pavel.ogienko_pavelogienko/homework_2/homework2_2.js
+++
@@ -1,6 +0,0 @@
-for (let i = 500; i <= 1000; i++) {
-    const reversedNumber = i.toString().split('').reverse().join('');
-    if (reversedNumber === i.toString()) {
-        console.log(reversedNumber);
-    }
-}
