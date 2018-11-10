0ed8c8a5cb089fa283ce024506fde8251031fc42
YulyKo <julia.kotenko.forstudy@gmail.com>
2018-11-10 13:26:29 +0200

    Revert "homework_2 task2: fixed"
    This reverts commit 789c1f786862b6b539714da30a58d81587d367d4.

--- homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
+++ homeworks/yulia.kotenko_YulyKo/homework_2/task02.js
@@ -4,15 +4,23 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-rl.question('final nomber ', (answer) => {
-        for (let i  = 0; i < answer; i++) {
-
-            let newN = i.toString().split('').reverse().join('');
-            let rev = i.toString();
-
-            if (rev === newN) {
-                console.log(i);
+rl.question('final nomber (0; 10000] ', (answer) => {
+     if(answer<=10000){
+        for (let i = 0; i < answer; i++) {
+            let nomber = i;
+            let newN = nomber.toString();
+            if (newN[0]===newN[1] && newN[2]===undefined && newN[3]===undefined) {
+                console.log(nomber);
+            }else if (newN[0]===newN[2] && newN[0]!==newN[1] && newN[3]===undefined) {
+                console.log(nomber);
+            }else if(newN[0]===newN[2] && newN[2]===newN[1] && newN[3]===undefined){
+                console.log(nomber);
+            }else if (newN[0]===newN[1] && newN[1]===newN[2] && newN[2]===newN[3]) {
+                console.log(nomber);
+            }else if (newN[0]===newN[3] && newN[1]===newN[2] && newN[0]!==newN[1]) {
+                console.log(nomber);
             }
+        }
     }
     rl.close();
 });
