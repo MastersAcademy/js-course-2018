1b4ef16426c5927179c4ea43e5d7e669e4a51fd0
PavelOgienko <pavlik182@gmail.com>
2018-11-10 23:59:52 +0200

    fix2

--- homeworks/pavel.ogienko_pavelogienko/homework_2/homework2_2.js
+++ homeworks/pavel.ogienko_pavelogienko/homework_2/homework2_2.js
@@ -2,3 +2,4 @@ for (let i = 500; i <= 1000; i++) {
     const reversedNumber = i.toString().split('').reverse().join('');
     if (reversedNumber === i.toString()) {
         console.log(reversedNumber);}}
+        
