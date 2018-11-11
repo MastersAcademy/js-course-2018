13842d05b3b56cf0402b60e86efc12619cbecf24
VadimMir <Miroshnik_vy@ukr.net>
2018-11-11 16:08:11 +0200

    hw

--- homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_1.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_1.js
@@ -1,18 +1,17 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout,
 });
 
-rl.question('What is the height of the pyramid? ', (answer) => {
+rl.question('What is the height of the pyramid?', (answer) => {
+    const height = +answer;
 
-let height = + answer;
+    for (let i = 1; i < height + 1; i++) {
+        const piramida = ' '.repeat(height - i) + '#'.repeat(i) + '#'.repeat(i);
+        console.log(piramida);
+    }
 
-for (let i = 1; i < height + 1; i++){
-    let piramida = ' '.repeat(height - i) + '#'.repeat(i) + '#'.repeat(i);
-    console.log(piramida);
- }
-
-  rl.close();
+    rl.close();
 });
--- homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_2.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_2.js
@@ -1,26 +1,21 @@
 const readline = require('readline');
 
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout,
 });
 
 rl.question('Please enter the first number: ', (min) => {
     rl.question('Please enter the second number: ', (max) => {
-        let rangeMin = + min;
-        let rangeMax = + max;
+        const rangeMin = +min;
+        const rangeMax = +max;
 
-        for(let i = rangeMin; i < rangeMax; i++){
-            let checkPalidromes = i.toString().split('').reverse().join('');
-            if( checkPalidromes === i.toString()){
+        for (let i = rangeMin; i < rangeMax; i++) {
+            const checkPalidromes = i.toString().split('').reverse().join('');
+            if (checkPalidromes === i.toString()) {
                 console.log(i);
             }
         }
         rl.close();
     });
-} );
-
-
-
-
-
+});
