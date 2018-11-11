12997f43da17b5fe733ad354b72ec46bbb397df8
Igor <igor_ck@outlook.com>
2018-11-11 11:59:27 +0200

    add fixed homewok_2 gordiichuk

--- homeworks/igor.gordichuk_IgorHordiichuk/homework_2/palindrome.js
+++ homeworks/igor.gordichuk_IgorHordiichuk/homework_2/palindrome.js
@@ -1,35 +1,38 @@
+// palindromes searching
+function palindromeFinding(num1, num2) {
+    const palindromes = [];
+    for (let i = num1; i <= num2; i++) {
+        if (i.toString().split('').reverse().join('') === i.toString()) {
+            palindromes.push(i);
+        }
+    }
+    console.log(`Your palindromes are: ${palindromes.toString()}`);
+}
+
 // entering numbers by user
-function enterNums(){
+function enterNums() {
+    /* eslint-disable global-require */
     const readline = require('readline');
 
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
     });
 
     rl.question('Enter first number: ', (firstNum) => {
         rl.question('Enter last number: ', (lastNum) => {
-            if(+firstNum && +lastNum){
-                if(+firstNum > +lastNum){
-                    let change = firstNum;
-                    firstNum = lastNum;
-                    lastNum = change;
-                }palindromeFinding(+firstNum, +lastNum);
-            }else{
+            let num1 = +firstNum;
+            let num2 = +lastNum;
+            if (num1 && num2) {
+                if (num1 > num2) {
+                    const change = num1;
+                    num1 = num2;
+                    num2 = change;
+                }palindromeFinding(num1, num2);
+            } else {
                 console.log('Try to read something about the numbers');
             }
             rl.close();
         });
     });
 }enterNums();
-
-// palindromes searching
-function palindromeFinding(num1, num2){
-    let palindromes = [];
-    for (let i = num1; i <= num2; i++){
-        if(i.toString().split('').reverse().join('') === i.toString()){
-            palindromes.push(i);
-        }
-    }
-    console.log(`Your palindromes are: ${palindromes.toString()}`);
-}
--- homeworks/igor.gordichuk_IgorHordiichuk/homework_2/pyramid.js
+++ homeworks/igor.gordichuk_IgorHordiichuk/homework_2/pyramid.js
@@ -1,29 +1,30 @@
-// pyramid height finding out 
-function pyramidHeightFindout(){
+// build pyramid
+function buildPyramid(height) {
+    const pyramidBGround = ' ';
+    let pyramidBody = '^^';
+
+    for (let i = height; i >= 1; i--) {
+        pyramidBody += '^^';
+        console.log(`${pyramidBGround.repeat(i)} ${pyramidBody}`);
+    }
+}
+
+// pyramid height finding
+function pyramidHeightFindout() {
+    /* eslint-disable global-require */
     const readline = require('readline');
 
     const rl = readline.createInterface({
         input: process.stdin,
-        output: process.stdout
+        output: process.stdout,
     });
 
     rl.question('Enter your pyramid height: ', (height) => {
-        if(+height){
+        if (+height) {
             buildPyramid(height);
-        }else{
+        } else {
             console.log('Mission failed! Pharaoh will kill you! Run');
         }
         rl.close();
     });
 }pyramidHeightFindout();
-
-// build pyramid
-function buildPyramid(height){
-    let pyramidBGround = ' ';
-    let pyramidBody = '^^';
-
-    for( let i = height; i >= 1; i--){
-        pyramidBody += '^^';
-        console.log(`${pyramidBGround.repeat(i)} ${pyramidBody}`);
-    }
-}
