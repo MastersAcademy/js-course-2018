28ff25ac7713973fabe41df3a2f94b379c0032d5
Klymenko69 <Klymenko696@gmail.com>
2018-11-11 17:30:15 +0200

    initial commit

---
+++ homeworks/artem.klymenko_Klymenko69/homeworks_2/piramida.js
@@ -0,0 +1,13 @@
+var a = '####';
+    var b = ' '; 
+   
+    for(var i = 15; 1 < i; i--){ 
+        
+        
+    }
+   
+    for(var j = 1; j < 16; j++ ) {
+
+        }
+    
+        console.log(b.repeat(+i) + a.repeat(+j));
