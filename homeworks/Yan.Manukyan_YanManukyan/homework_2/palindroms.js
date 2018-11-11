b75158bad675fd857aafc001cf67862b44bb57d0
YanManukyan <manukyan79@ukr.net>
2018-11-11 15:21:28 +0200

    homework_2

--- homeworks/Yan.Manukyan_YanManukyan/homework_2/palindroms.js
+++ homeworks/Yan.Manukyan_YanManukyan/homework_2/palindroms.js
@@ -2,14 +2,14 @@ var a = 500;
 var b = 1000;
 //a = a.toString();
 //b = b.toString();;
-for(var i = a; i < b; i++){
-    if (i.toString() == reverse(i.toString())){
+for (var i = a; i < b; i++) {
+    if (i.toString() === reverse(i.toString())) {
         console.log(i);
     }
 }
-function reverse(str){
-    let reversed = "";
-    for(var i = str.length -1; i >= 0; i--){
+function reverse(str) {
+    let reversed = '';
+    for (var i = str.length - 1; i >= 0; i--) {
         reversed += str[i];
     }
     return reversed;
--- homeworks/Yan.Manukyan_YanManukyan/homework_2/pyramid.js
+++ homeworks/Yan.Manukyan_YanManukyan/homework_2/pyramid.js
@@ -1,9 +1,8 @@
 var space = ' ';
-var readline = require('readline');
-var str = "####";
+var str = '####';
 var j = 14;
-for(var i = 0; i < 15; i++){
+for (var i = 0; i < 15; i++) {
     console.log(space.repeat(j) + str);
-    str+= "##";
+    str += '##';
     j--;
 }
