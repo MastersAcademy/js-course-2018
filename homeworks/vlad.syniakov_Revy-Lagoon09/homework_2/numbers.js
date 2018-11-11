06e50c3df60c4366faed957804baffcb3765cd8e
Vlad Syniakov <vlad.syniakov@gmail.com>
2018-11-11 10:50:07 +0200

    Bless RNG

---
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_2/numbers.js
@@ -0,0 +1,10 @@
+    function input(a) {
+    const arr = a.toString().split('');
+    const reverse = a.toString().split('').reverse().join('');
+    arr[arr.length - 1] = reverse;
+    console.log(arr.join(''));
+}
+for (let i = 500; i <= 1000; i++) {
+    input(i);
+}
+
