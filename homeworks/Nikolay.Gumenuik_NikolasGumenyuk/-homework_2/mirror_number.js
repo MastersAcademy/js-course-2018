b62e471b87ea986d7802b8063fd618ee7e71a14f
NikolasGumenyuk <43613315+NikolasGumenyuk@users.noreply.github.com>
2018-11-11 15:49:41 +0200

    Update mirror_number.js

--- homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_2/mirror_number.js
+++ homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_2/mirror_number.js
@@ -1,16 +1,26 @@
 const readline = require('readline-sync');
-const min = readline.question("Your min value?");
-const max = readline.question("Your max value?");
-    console.log(max, min);
-    var num;
-    var x, y, z;
-    for (var i = min; i<max; i++){
-        num = i;
-        String(num);
-        x = num.toString().slice(0, 1);
-        y = num.toString().slice(1, 2);
-        z = num.toString().slice(2, 3);
-        if ((x === y && y === z) || x === z){
-            console.log(x + y + z);
+
+const r1 = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+r1.question('Enter number>100', (min) => {
+    r1.question('Enter number<1000', (max) => {
+        console.log(max, min);
+        let num;
+        let x;
+        let y;
+        let z;
+        for (let i = min; i < max; i++) {
+            num = i;
+            String(num);
+            x = num.toString().slice(0, 1);
+            y = num.toString().slice(1, 2);
+            z = num.toString().slice(2, 3);
+            if ((x === y && y === z) || x === z) {
+                console.log(x + y + z);
+            }
         }
-    }
+        r1.close();
+    });
+});
