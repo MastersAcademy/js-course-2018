d8917f42ab4c99a83d40c157901401da1e8f03a0
VladZelenko <Vlad6555@ukr.net>
2018-11-11 16:40:16 +0200

    Update index2.js

--- homeworks/Vlad.VladZelenko/homework_2/index2.js
+++ homeworks/Vlad.VladZelenko/homework_2/index2.js
@@ -7,12 +7,11 @@ const read = readline.createInterface({
 
 read.question('For example, enter the numbers 100, 1000 => ', (input) => {
     const [start, end] = input.split(', ').map(num => +num);
-
     for (let num = start; num <= end; num++) {
-        const arr = num.toString().split('');
-
-        if (arr[0] === arr[2]) {
-            console.log(+arr.join(''));
+        // Так воно буде рахувати більше чим 1000, хоча в тех завданні від 500 до 1000, 
+        // ну і виглядає воно вже не так оригінально, а більше як копіпаст, попередній варіант на 100% відпрацьовував тех. завдання
+        if (num.toString().split('').reverse().join('') === num.toString()) {
+            console.log(num);
         }
     }
     read.close();
