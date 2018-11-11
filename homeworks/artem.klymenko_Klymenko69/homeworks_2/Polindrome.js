d2a32ec324d06c7b7c75951c917620bd5f879abb
Klymenko69 <Klymenko696@gmail.com>
2018-11-11 17:58:02 +0200

    initial commit

--- homeworks/artem.klymenko_Klymenko69/homeworks_2/Polindrome.js
+++ homeworks/artem.klymenko_Klymenko69/homeworks_2/Polindrome.js
@@ -1,6 +1,5 @@
-var i;
-  for(var i = 500; i <= 1000; i++){
-      if (i.toString().split('').reverse().join('') === i.toString())
+	for(var i = 500; i <= 1000; i++){
+      if (i.toString().split('').reverse().join('') = i.toString())
           console.log(i);
       
   } 
