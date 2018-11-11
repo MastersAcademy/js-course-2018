8aa15d7c2e0c43b961b127ddd37b09c02838640f
IuriiSky <uzik.fk11@gmail.com>
2018-11-11 19:03:30 +0200

    Revert "homework_2"
    This reverts commit b430da5b227799eb68ac79652fb8eb3cd02a1604.

--- homeworks/iurii.stepanenko_IuriiSky/homework_2/numbers.js
+++ homeworks/iurii.stepanenko_IuriiSky/homework_2/numbers.js
@@ -1,10 +1,13 @@
 for (let i = 500; i < 1000; i++) {
-    const Palindrome = i.toString();
 
-    const first = Palindrome.charAt(0);
-    const last = Palindrome.charAt(2);
+    const iString = i.toString();
+    const length = iString.length;
+    const first = iString.charAt(0);
+    const last = iString.charAt(length-1);
 
     if (first === last) {
-        console.log(Palindrome);
+        console.log(iString);
+
     }
-}
+    
+}   
