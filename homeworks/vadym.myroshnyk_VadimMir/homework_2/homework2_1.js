0065211ef4ade956856199e5d3117dc810af1227
VadimMir <Miroshnik_vy@ukr.net>
2018-11-11 16:28:50 +0200

    hw2

--- homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_1.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_2/homework2_1.js
@@ -9,7 +9,9 @@ rl.question('What is the height of the pyramid?', (answer) => {
     const height = +answer;
 
     for (let i = 1; i < height + 1; i++) {
-        const piramida = ' '.repeat(height - i) + '#'.repeat(i) + '#'.repeat(i);
+        const spaces = ' '.repeat(height - i);
+        const symbol = '#'.repeat(i);
+        const piramida = spaces + symbol + symbol;
         console.log(piramida);
     }
 
