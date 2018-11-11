222c461e2a4840f342a90239e7399dcaf615d0cb
Helen-Mary <elenka814@gmail.com>
2018-11-11 13:53:37 +0200

    Bugs've fixed

--- homeworks/Maria.Priamuhina_Helen-Mary/homework_2/htask2_1.js
+++ homeworks/Maria.Priamuhina_Helen-Mary/homework_2/htask2_1.js
@@ -1,29 +1,26 @@
-'use strict';
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
 function isNumber(obj) {
-    return !isNaN(parseFloat(obj));
+    return !Number.isNaN(parseFloat(obj));
 }
-let flag = true;
 console.log('enter number of Pyramids` layers:');
 rl.on('line', (input) => {
-    if(isNumber(input)) {
-        let numSpace = input - 1;
-        let space = ' ';
-        let sharp = '##';
-        let numSharp = 2;
-        for(let iter = 0; iter < input; iter++) {
-            console.log(space.repeat(numSpace-iter) + sharp.repeat(numSharp+iter));
+    if (isNumber(input)) {
+        const numSpace = input - 1;
+        const space = ' ';
+        const sharp = '##';
+        const numSharp = 2;
+        for (let iter = 0; iter < input; iter++) {
+            console.log(space.repeat(numSpace - iter) + sharp.repeat(numSharp + iter));
         }
     }
     else {
         console.log('error.');
-        // flag = false;
     }
     rl.close();
 });
--- homeworks/Maria.Priamuhina_Helen-Mary/homework_2/htask2_2.js
+++ homeworks/Maria.Priamuhina_Helen-Mary/homework_2/htask2_2.js
@@ -1,33 +1,32 @@
-'use strict';
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
 console.log('enter an interval of values:');
 
 function IsSymetry(num) {
-    let str = String(num);
+    const str = String(num);
     let flag = true;
-    let strNew ='';
+    let strNew = '';
     for (let iter = str.length - 1; iter >= 0; iter--) {
         strNew += str[iter];
     }
-    if(str != strNew) {
+    if (str !== strNew) {
         flag = false;
     }
     return flag;
 }
 
 rl.on('line', (input) => {
-    let temp = input.split(' ');
-    let start = temp[0];
-    let end = temp[1];
-    for(let temp = start; temp < end; temp++) {
-        if(IsSymetry(temp)){
-            console.log(temp +' ');
+    const temp = input.split(' ');
+    const start = temp[0];
+    const end = temp[1];
+    for (let tempIter = start; tempIter < end; tempIter ++) {
+        if (IsSymetry(tempIter)) {
+            console.log(tempIter + ' ');
         }
     }
     rl.close();
