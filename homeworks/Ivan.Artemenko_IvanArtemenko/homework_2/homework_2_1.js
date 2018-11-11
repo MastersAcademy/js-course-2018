45d7617fa1b22687f6189bf2f83696d2280ffe44
Ivan-Artemenko <mechenuj32@gmail.com>
2018-11-11 17:26:54 +0200

    Update Home Work 2 + eslint done

--- homeworks/Ivan.Artemenko_IvanArtemenko/homework_2/homework_2_1.js
+++ homeworks/Ivan.Artemenko_IvanArtemenko/homework_2/homework_2_1.js
@@ -1,19 +1,20 @@
 const readline = require('readline');
+
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout,
 });
 rl.question('Enter the number of triangle levels from 3 to 15 ', (number) => {
-let number_1 = number-0;
-if(number_1 <= 16 && number_1 >= 3){
-    for(let i = 2; i <= number_1+1; i++){
-  	    let a = '**';	
-        let b = ' ';
-        let j = ((number_1+1)-i);
-        a = a.repeat(i);
-        b = b.repeat(j);
-        console.log(b+a);
-    }    
-}else console.log('You must enter a number from 3 to 15')
- rl.close();
+    const numeric = number - 0;
+    if (numeric <= 16 && numeric >= 3) {
+        for (let i = 2; i <= numeric + 1; i++) {
+            let a = '**';
+            let b = ' ';
+            const j = ((numeric + 1) - i);
+            a = a.repeat(i);
+            b = b.repeat(j);
+            console.log(b + a);
+        }
+    } else console.log('You must enter a number from 3 to 15');
+    rl.close();
 });
