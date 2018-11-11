201f82c32a9ce383415cdc3298a346bf398d8f5c
Olexandr <olexandr.reshetnyk@gmail.com>
2018-11-11 09:05:23 +0200

    maked my second homework

---
+++ homeworks/olexandr.reshetnyk_reshetnyk/homework_2/phyramid.js
@@ -0,0 +1,34 @@
+﻿let readline = require('readline-sync');
+let height = process.argv[2];
+
+if(typeof height === 'undefined')
+    height = readline.question('Type a number of rows: ');
+
+if(!isNumeric(height)){
+    console.log('Argument isn\'t a number.');
+    process.exit(-1);
+} else if(!inRange(height)){
+    console.log('Argument must be more than 1.');
+    process.exit(-1);
+}
+
+let lineWidth = 2 + height * 2;
+let symbolsCount = 2;
+let spacesCount = (lineWidth - symbolsCount) / 2;
+
+for(let i = 0; i < height; i++){
+    symbolsCount += 2;
+    spacesCount = (lineWidth - symbolsCount) / 2;
+    let spaces = ' '.repeat(spacesCount);
+    let symbols = '#'.repeat(symbolsCount ); 
+    let line = spaces + symbols + spaces;
+    console.log(line);
+}
+
+function isNumeric(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+function inRange(n){
+    return n > 1; 
+}
---
+++ homeworks/olexandr.reshetnyk_reshetnyk/homework_2/polynoms.js
@@ -0,0 +1,39 @@
+﻿let readline = require('readline-sync');
+
+let start = readline.question('Type a start number: ');
+let end = readline.question('Type an end number: ');
+
+//валидация
+if(!isNumeric(start) || !isNumeric(end) || end < start || start < 0){
+    console.log('Bad arguments!');
+    process.exit(-1);
+}
+
+let left, center, right;
+//перебор
+for(let i = start; i < end; i++){
+    //делим число на две равные части. если кол-во цифр в числе не четное, 
+    //то цифру по середине выкидываем в center переменную
+    let iLength = i.toString().length;
+    center = (iLength % 2 !== 0) ? i.toString().charAt(~~(iLength / 2)) : '';
+    left = (i.toString()).slice(0, Math.floor(iLength / 2));
+    right = (i.toString()).slice(Math.round(iLength / 2, iLength));
+    //проверяем, есть ли число числом-полиндромом
+    if(isPoly(left, right))
+        console.log(left + center +right);
+}
+
+function isPoly(left, right){
+    let length = left.toString().length;
+    let equalMatches = 0;
+    //сравниваем левю и правую часть зеркально порозрядно. 
+    for(let i = 0; i < length; i++){
+        if(left[i] === right[length - i - 1])
+            equalMatches++;
+    }
+    //если кол-во совпавших разрядов равно количеству розрядов - это число-полином.
+    return equalMatches === length ? true : false;
+}
+function isNumeric(n){
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
