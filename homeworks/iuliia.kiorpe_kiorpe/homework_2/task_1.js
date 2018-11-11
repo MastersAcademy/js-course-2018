aa899fd86b485e1895f16b832a6f1b3b359603bb
kiorpe <vagrant@homestead>
2018-11-11 14:33:46 +0000

    commit

--- homeworks/iuliia.kiorpe_kiorpe/homework_2/task_1.js
+++ homeworks/iuliia.kiorpe_kiorpe/homework_2/task_1.js
@@ -1,28 +1,23 @@
-let standardInput = process.stdin;
-standardInput.setEncoding('utf-8');
-console.log("Please input text in command line.");
-
-standardInput.on('data', function (strings) {
-    strings = parseInt(strings);
-	
-	if (strings > 0) {
-		buildPiramid(strings);
-	} else {
-		console.log('Your input is invalid.');
-	}
-	
-	process.exit();
-});
-
 function buildPiramid(x) {
-	let sharp = '#';
-	let space = ' ';
-	let h_lenght = x + 1;
+    const sharp = '#';
+    const space = ' ';
+    const hlenght = x + 1;
 
-	for (let i = 0; i < x; i++) {
-		console.log(space.repeat(h_lenght - (i + 2)) + sharp.repeat((i + 2) * 2));
-	}
+    for (let i = 0; i < x; i++) {
+        console.log(space.repeat(hlenght - (i + 2)) + sharp.repeat((i + 2) * 2));
+    }
 }
 
+const standardInput = process.stdin;
+standardInput.setEncoding('utf-8');
+console.log('Please input text in command line.');
 
-	
+standardInput.on('data', (strings) => {
+    strings = parseInt(strings, 10); // eslint-disable-line no-param-reassign
+    if (strings > 0) {
+        buildPiramid(strings);
+    } else {
+        console.log('Your input is invalid.');
+    }
+    process.exit();
+});
--- homeworks/iuliia.kiorpe_kiorpe/homework_2/task_2.js
+++ homeworks/iuliia.kiorpe_kiorpe/homework_2/task_2.js
@@ -1,28 +1,23 @@
-let standardInput = process.stdin;
-standardInput.setEncoding('utf-8');
-console.log("Please input min and max values devided by space");
-
-standardInput.on('data', function (data) {
-	let numbers	= data.split(" ");
-	let start = parseInt(numbers[0]);
-	let end = parseInt(numbers[1]);
-	
-	if (numbers.length === 2 && start < end) {
-		findMirrors(start, end);
-	} else {
-		console.log('Your input is invalid.');
-	}
-	
-	process.exit();
-});
-
-
-
 function findMirrors(a, b) {
-	for (let i = a; i <= b; i++) {
-		if (i == i.toString().split("").reverse().join("")) {
-			console.log(i);
-		}
-	}
+    for (let i = a; i <= b; i++) {
+        if (i === i.toString().split('').reverse().join('')) {
+            console.log(i);
+        }
+    }
 }
 
+const standardInput = process.stdin;
+standardInput.setEncoding('utf-8');
+console.log('Please input min and max values devided by space');
+
+standardInput.on('data', (data) => {
+    const numbers = data.split(' ');
+    const start = parseInt(numbers[0], 10);
+    const end = parseInt(numbers[1], 10);
+    if (numbers.length === 2 && start < end) {
+        findMirrors(start, end);
+    } else {
+        console.log('Your input is invalid.');
+    }
+    process.exit();
+});
