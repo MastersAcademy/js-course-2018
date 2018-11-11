d5a5cc65ea68f2cf8f49163c2500d817312d7a27
zelinka <2pacshac@gmail.com>
2018-11-11 13:12:06 +0200

    homework_2

--- homeworks/Den.Sokurenko_Den4ik08/homework_2/piramida.js
+++ homeworks/Den.Sokurenko_Den4ik08/homework_2/piramida.js
@@ -1,6 +1,5 @@
     function pyramid(n) {
          
-<<<<<<< HEAD
          for(let i=2; i<= n; i++){
    
             let str = ' '.repeat(n-i);
@@ -11,15 +10,4 @@
          }
        } 
 pyramid(15);
-=======
-          for(let i=2; i<= n; i++){
-    
-           let str = ' '.repeat(n-i);
-    
-           let str2 = '#'. repeat(i*2)
-        
-            console.log(str + str2 + str);
-         }
-       } 
-pyramid(15)
->>>>>>> 7e5e86d56b96817ea7f00023052d688d0db9c595
+
--- homeworks/Den.Sokurenko_Den4ik08/homework_2/polindrom.numbers.js
+++ homeworks/Den.Sokurenko_Den4ik08/homework_2/polindrom.numbers.js
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+
 for(var i = 500; i<1000; i++){
   if (i.toString () === i.toString ().split('').
  reverse ().join('')){
  console.log(i)}}
-=======
-  for(var i = 500; i<1000; i++){
-  if (i.toString () === i.toString ().split('').3 reverse ().join('')){
-  console.log(i)}}
->>>>>>> 7e5e86d56b96817ea7f00023052d688d0db9c595
+
