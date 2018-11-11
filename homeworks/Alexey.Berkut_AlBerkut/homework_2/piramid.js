8575ffbc72a0fca1918c87ff48b9b0a946f1bcf5
AlBerkut <44679993+AlBerkut@users.noreply.github.com>
2018-11-11 13:24:35 +0200

    Add files via upload

--- homeworks/Alexey.Berkut_AlBerkut/homework_2/piramid.js
+++ homeworks/Alexey.Berkut_AlBerkut/homework_2/piramid.js
@@ -1,9 +1,9 @@
-function build(num){
-    let symbl = '#';
-    let space = '_';
+function build(num) {
+    const symbl = '#';
+    const space = '_';
     let i = 2;
 
-    for(num > 0; num--;){
+    for (num; num > 0; num--) {
         i = i + 2;
         console.log(space.repeat(num) + symbl.repeat(i));
     }
