2f4639dfecc266a83dbdd6bb96ccd975dd45c54e
VladZelenko <Vlad6555@ukr.net>
2018-11-11 17:10:51 +0200

    Update index1.js

--- homeworks/Vlad.VladZelenko/homework_2/index1.js
+++ homeworks/Vlad.VladZelenko/homework_2/index1.js
@@ -9,7 +9,9 @@ read.question('Enter the height of the pyramid: ', (max) => {
     for (let i = 0; i < max; i++) {
         const space = ' ';
         const sign = '#';
-        console.log((space.repeat(max - i) + sign.repeat(i)) + (sign.repeat(i) + space.repeat(max - i)));   
+        const a = space.repeat(max - i);
+        const out = (a + sign.repeat(i)) + (sign.repeat(i) + space.repeat(max - i));
+        console.log(out);
     }
     read.close();
 });
