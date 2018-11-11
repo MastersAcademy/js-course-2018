b473bc7bff643c8fc3ffa909ae411b0cd151971d
Sergey <kondratuk.s.ck@gmail.com>
2018-11-11 13:51:09 +0200

    homework_2_fix_2

--- homeworks/sergey.kondratuk_kondratuks/homework_2/2.2.js
+++ homeworks/sergey.kondratuk_kondratuks/homework_2/2.2.js
@@ -1,8 +1,5 @@
-// eslint-disable-next-line no-undef
-for (i = 500; i <= 1000; i++) {
-    // eslint-disable-next-line no-undef
+for (let i = 500; i <= 1000; i++) {
     const numPal = i.toString().split('').reverse().join('');
-    // eslint-disable-next-line no-undef
     if (numPal === i.toString()) {
         console.log(numPal);
     }
