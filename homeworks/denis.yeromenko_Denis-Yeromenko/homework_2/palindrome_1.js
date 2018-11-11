cd4e9872464c1956b168449cc1ca79d779a67915
Denis <dden.yeromenko@gmail.com>
2018-11-11 17:23:21 +0200

    my folder was updated

--- homeworks/denis.yeromenko_Denis-Yeromenko/homework_2/palindrome_1.js
+++ homeworks/denis.yeromenko_Denis-Yeromenko/homework_2/palindrome_1.js
@@ -1,8 +1,8 @@
-for(i = 500; i <= 1000; i++){
-    let str = i.toString();
+for (let i = 500; i <= 1000; i++) {
+    const str = i.toString();
 
-let palind = str.split('').reverse().join('');
-if (str == palind){
-console.log(palind);
-};
-};
+    const palind = str.split('').reverse().join('');
+    if (str === palind) {
+        console.log(palind);
+    }
+}
--- homeworks/denis.yeromenko_Denis-Yeromenko/homework_2/piramida.js
+++ homeworks/denis.yeromenko_Denis-Yeromenko/homework_2/piramida.js
@@ -1,8 +1,7 @@
-let rows = 15, bloks = "#", spaces = " ", count_rows = rows, count_bloks = 4;
+const rows = 15; const bloks = '#'; const spaces = ' '; let countrows = rows; let countbloks = 4;
 
-for(let i=1; i<=rows; i++){
-  console.log(spaces.repeat(count_rows)+bloks.repeat(count_bloks));
-  count_rows -= 1;
-  count_bloks += 2
-
-};
+for (let i = 1; i <= rows; i++) {
+    console.log(spaces.repeat(countrows) + bloks.repeat(countbloks));
+    countrows -= 1;
+    countbloks += 2;
+}
