9f856747dc1ad1c12c5b23157fc69e02962366fe
Murayzes <murayzes@gmail.com>
2018-11-11 17:12:26 +0200

    corrected_homework_2_2v2

--- homeworks/vladislav.reshetylo_Murayzes/homework_2/homework_2_2.js
+++ homeworks/vladislav.reshetylo_Murayzes/homework_2/homework_2_2.js
@@ -1,9 +1,9 @@
 
 for(i = 500; i < 1000; i++){
     let numbers = i;
-    newArrey = numbers.toString();  // create an array and translate it into a string
-    splitArrey = newArrey.split('');    // break elements of an array 
-    if (splitArrey[0] == splitArrey[2]){
-        console.log(splitArrey);
+    newArray = numbers.toString();  // create an array and translate it into a string
+    splitArray = newArray.split('');    // break elements of an array 
+    if (splitArray[0] == splitArray[2]){
+        console.log(splitArray.join(''));
     }
 }
