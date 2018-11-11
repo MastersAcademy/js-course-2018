881e72eb9fe5fedbbe4aefb550836034044b7d39
miwagav <miwagav123@gmail.com>
2018-11-11 14:40:34 +0200

    Homework2

---
+++ homeworks/mykhaylo.gavrylyuk_miwagav/Homework_2/Polinom.js
@@ -0,0 +1,4 @@
+for (let i=500; i<1001; i++) {
+   let a = i.toString ();
+    if (a[0] === a[2]) {console.log (i);} ;
+}
---
+++ homeworks/mykhaylo.gavrylyuk_miwagav/Homework_2/piramida.js
@@ -0,0 +1,5 @@
+let space = " ";
+let hashtag = "#";
+for (let i=0; i<15;i++) {
+    console.log(space.repeat(14-i) + hashtag.repeat(4+2*i))};
+
