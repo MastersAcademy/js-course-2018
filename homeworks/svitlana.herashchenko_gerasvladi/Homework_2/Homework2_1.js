a90f317ea29bd89c260e066ab01c0b046dfe6c97
gerasvladi <svetgv@ukr.net>
2018-11-11 20:17:31 +0200

    Homework98

--- homeworks/svitlana.herashchenko_gerasvladi/Homework_2/Homework2_1.js
+++ homeworks/svitlana.herashchenko_gerasvladi/Homework_2/Homework2_1.js
@@ -1,3 +1,5 @@
-let space = " ";
-let tag = "#";
-for (let a = 0; a<15; a++) {console.log (space.repeat(14-a) + tag.repeat(4+a+a))};
+for (let a = 0; a < 15; a++) {
+    const space = ' '.repeat(14 - a);
+    const tag = '#'.repeat(4 + a + a);
+    console.log(space + tag);
+}
--- homeworks/svitlana.herashchenko_gerasvladi/Homework_2/Homework2_2.js
+++ homeworks/svitlana.herashchenko_gerasvladi/Homework_2/Homework2_2.js
@@ -1,4 +1,4 @@
-for (let a=500; a<1001; a++) {
-    let b = a.toString(10); 
-    if (b[0] === b[2]) {console.log (b)};
+for (let a = 500; a < 1001; a++) {
+    const b = a.toString(10);
+    if (b[0] === b[2]) { console.log(b); }
 }
