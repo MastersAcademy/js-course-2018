4aa0148b125c45a69945474b3b57858215184240
v-lavs <vanda71@ukr.net>
2018-11-11 12:39:40 +0200

    fix task 1

--- homeworks/vanda.lavska_v-lavs/homework_2/hw2_1.js
+++ homeworks/vanda.lavska_v-lavs/homework_2/hw2_1.js
@@ -1,71 +1,31 @@
-const  readline = require('readline');
+
+const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 
 rl.question('Enter height pyramid: ', (height) => {
-
+    function pyramid() {
+        if (`${height}` === '0' || Number.isNaN(Number(`${height}`))) {
+            console.log('ERROR. Please,  enter number > 0');
+        } else {
+            for (let i = 0; i < height; i++) {
+                const x = 2 * i + 4;
+                const y = (2 * height + 2 - x) / 2;
+                let str = '';
+                for (let j = 0; j < y; j++) {
+                    str += ' ';
+                }
+                for (let index = 0; index < x; index++) {
+                    str += '#';
+                }
+                console.log(str);
+            }
+        }
+    }
     pyramid(`${height}`);
 
     rl.close();
 });
-
-function pyramid(height) {
-    for (let i=0; i< height; i++) {
-        let x = 2 * i + 4;
-        let y = (2 * height + 2 - x)/2;
-        let str = '';
-        for ( let i=0; i< y; i++) {
-            str += ' ';
-        }
-        for (let i=0; i<x ; i++){
-            str += '#';
-        }
-        console.log(str);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
