3089883d00072daeb9ababc3ac0e6fdbe5ec184a
Daria Borovyk <44699241+daria-borovyk@users.noreply.github.com>
2018-11-11 13:10:05 +0200

    homework 2 with fixes

--- homeworks/daria.borovyk_daria-borovyk/homework_2/mirrorNumbers.js
+++ homeworks/daria.borovyk_daria-borovyk/homework_2/mirrorNumbers.js
@@ -1,35 +1,33 @@
 const readline = require('readline');
+
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
 });
 rl.question('Enter min > 100: ', (min) => {
     rl.question('Enter max < 99999: ', (max) => {
-        if ((min === max) || (max < min) || (min < 100) || (max < 100) || (Number.isNaN(Number(min))) || (Number.isNaN(Number(max)))) {
+        if ((min === max) || (max < min) || (min < 100)
+            || (max < 100) || (Number.isNaN(Number(min))) || (Number.isNaN(Number(max)))) {
             console.log('Error!');
-        }
-        else {
-            for (i = min; i <= max; i++) {
-                let number = i;
-                let n = number.toString();
-                if (n.length === 3) {
-                    if ((n[0] === n[2])) {
-                        console.log(n);
+        } else {
+            for (let i = min; i <= max; i++) {
+                const number = i;
+                const numString = number.toString();
+                if (numString.length === 3) {
+                    if ((numString[0] === numString[2])) {
+                        console.log(numString);
                     }
-                }
-                else if (n.length === 4) {
-                    if ((n[0] === n[3]) && (n[1] === n[2])) {
-                        console.log(n);
+                } else if (numString.length === 4) {
+                    if ((numString[0] === numString[3]) && (numString[1] === numString[2])) {
+                        console.log(numString);
                     }
-                }
-                else if (n.length === 5) {
-                    if ((n[0] === n[4]) && (n[1] === n[3])) {
-                        console.log(n);
+                } else if (numString.length === 5) {
+                    if ((numString[0] === numString[4]) && (numString[1] === numString[3])) {
+                        console.log(numString);
                     }
                 }
             }
         }
         rl.close();
-
     });
 });
