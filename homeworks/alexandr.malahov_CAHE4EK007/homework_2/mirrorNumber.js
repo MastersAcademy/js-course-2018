b9e6b145c72180768ea3c2deb9611d48d791f5da
CAHE4EK007 <iddigital83@gmail.com>
2018-11-12 12:13:42 +0200

    fix linter erorr

--- homeworks/alexandr.malahov_CAHE4EK007/homework_2/mirrorNumber.js
+++ homeworks/alexandr.malahov_CAHE4EK007/homework_2/mirrorNumber.js
@@ -1,29 +1,26 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-const MIN_RANGE_NUMBER = 500;
-const MAX_RANGE_NUMBER = 1000;
-
-function mirrorNumbers (number) {
-    for (let i = 1; i <= number; i++){
-        let str = `${i}`;
-        if (str === str.split('').reverse().join('')) {
-            console.log(str)
-        }
-    }
-}
-
-function checkRangeNumber (number) {
-    return (typeof +number !== 'number' || +number < MIN_RANGE_NUMBER || +number > MAX_RANGE_NUMBER);
-}
-
-rl.question('Enter a number in the range 500 - 1000: ' , function (answer) {
-    if (checkRangeNumber(answer)) {
-        console.log('Sorry your answer is not correct');
-        rl.close();
-    }
-    mirrorNumbers(answer);
-    rl.close();
-})
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+const MIN_RANGE_NUMBER = 500;
+const MAX_RANGE_NUMBER = 1000;
+function mirrorNumbers (number) {
+    for (let i = 1; i <= number; i++){
+        let str = `${i}`;
+        if (str === str.split('').reverse().join('')) {
+            console.log(str);
+        }
+    }
+}
+function checkRangeNumber (number) {
+    return (typeof +number !== 'number' || +number < MIN_RANGE_NUMBER || +number > MAX_RANGE_NUMBER);
+}
+rl.question('Enter a number in the range 500 - 1000: ' , function (answer) {
+    if (checkRangeNumber(answer)) {
+        console.log('Sorry your answer is not correct');
+        rl.close();
+    }
+    mirrorNumbers(answer);
+    rl.close();
+})
--- homeworks/alexandr.malahov_CAHE4EK007/homework_2/pyramid.js
+++ homeworks/alexandr.malahov_CAHE4EK007/homework_2/pyramid.js
@@ -1,27 +1,24 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-function getPyramid (height) {
-    let i = 2;
-    let j;
-    for (j = height; j >= 1; j--) {
-        i = i + 2;
-        console.log(' '.repeat(j) + '#'.repeat(i));
-    }
-}
-
-function checkPyramidHeight (height) {
-    return typeof height !== 'number' || height === 0 || height > 50;
-}
-
-rl.question('Enter height pyramid: ', (number) => {
-    if (checkPyramidHeight(number)) {
-        console.log('Sorry your answer is not correct');
-        rl.close();
-    }
-    getPyramid(number);
-    rl.close();
-});
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+function getPyramid (height) {
+    let i = 2;
+    let j;
+    for (j = height; j >= 1; j--) {
+        i = i + 2;
+        console.log(' '.repeat(j) + '#'.repeat(i));
+    }
+}
+function checkPyramidHeight (height) {
+    return typeof height !== 'number' || height === 0 || height > 50;
+}
+rl.question('Enter height pyramid: ', (number) => {
+    if (checkPyramidHeight(number)) {
+        console.log('Sorry your answer is not correct');
+        rl.close();
+    }
+    getPyramid(number);
+    rl.close();
+});
