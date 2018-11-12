b2de0ccb6a78b88d56cd0c7055dcc54ad445071c
Alexander <kulikau@gmail.com>
2018-11-12 14:37:33 +0200

    homework2 правки

--- homeworks/alexander.kulyk_KulykAlexander/homework_2/hw2_1.js
+++ homeworks/alexander.kulyk_KulykAlexander/homework_2/hw2_1.js
@@ -1,18 +1,19 @@
-let a = '##';
-let b = ' '; {
-    console.log(b.repeat(15)+a.repeat(2));
-    console.log(b.repeat(14)+a.repeat(3));
-    console.log(b.repeat(13)+a.repeat(4));
-    console.log(b.repeat(12)+a.repeat(5));
-    console.log(b.repeat(11)+a.repeat(6));
-    console.log(b.repeat(10)+a.repeat(7));
-    console.log(b.repeat(9)+a.repeat(8));
-    console.log(b.repeat(8)+a.repeat(9));
-    console.log(b.repeat(7)+a.repeat(10));
-    console.log(b.repeat(6)+a.repeat(11));
-    console.log(b.repeat(5)+a.repeat(12));
-    console.log(b.repeat(4)+a.repeat(13));
-    console.log(b.repeat(3)+a.repeat(14));
-    console.log(b.repeat(2)+a.repeat(15));
-    console.log(b.repeat(1)+a.repeat(16));
+const a = '##';
+const b = ' ';{
+    console.log(b.repeat(15) +a.repeat(2));
+    console.log(b.repeat(14) +a.repeat(3));
+    console.log(b.repeat(13) +a.repeat(4));
+    console.log(b.repeat(12) +a.repeat(5));
+    console.log(b.repeat(11) +a.repeat(6));
+    console.log(b.repeat(10) +a.repeat(7));
+    console.log(b.repeat(9) +a.repeat(8));
+    console.log(b.repeat(8) +a.repeat(9));
+    console.log(b.repeat(7) +a.repeat(10));
+    console.log(b.repeat(6) +a.repeat(11));
+    console.log(b.repeat(5) +a.repeat(12));
+    console.log(b.repeat(4) +a.repeat(13));
+    console.log(b.repeat(3) +a.repeat(14));
+    console.log(b.repeat(2) +a.repeat(15));
+    console.log(b.repeat(1) +a.repeat(16));
 }
+// end
--- homeworks/alexander.kulyk_KulykAlexander/homework_2/hw2_2.js
+++ homeworks/alexander.kulyk_KulykAlexander/homework_2/hw2_2.js
@@ -1,6 +1,7 @@
-for (let i=500; i<=1000; i++){
-    let str =String(i);
-    if (str[0]===str[2]) {
+for (let i =500; i <=1000; i++) {
+    const str = String(i);
+    if (str[0] ===str[2]) {
         console.log(str);
     }
 }
+// end
