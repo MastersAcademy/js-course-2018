74f0b409268347dd703ed025dcbc415b6c428bb3
SerhiyHurin <44698110+SerhiyHurin@users.noreply.github.com>
2018-11-12 12:13:53 +0200

    Update task_1.js

--- homeworks/serhiy.hurynovych_SerhiyHurin/homework_2/task_1.js
+++ homeworks/serhiy.hurynovych_SerhiyHurin/homework_2/task_1.js
@@ -1,5 +1,5 @@
 const gap = ' ';
 const body = '#';
 for (let i = 1; i <= 15; i++) {
-    console.log(gap.repeat(15 - i) + body.repeat(i*2 + 2));
+    console.log(gap.repeat(15 - i) + body.repeat(i * 2 + 2));
 }
