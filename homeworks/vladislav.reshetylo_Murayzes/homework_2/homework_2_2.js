ee81c8a4ee9eb9ead1a9b1413ad76222a3b61cd6
Murayzes <murayzes@gmail.com>
2018-11-13 12:45:34 +0200

    vladislav_reshetylo_hw_2_2v3

--- homeworks/vladislav.reshetylo_Murayzes/homework_2/homework_2_2.js
+++ homeworks/vladislav.reshetylo_Murayzes/homework_2/homework_2_2.js
@@ -1,9 +1,9 @@
-
-for(i = 500; i < 1000; i++){
-    let numbers = i;
-    newArray = numbers.toString();  // create an array and translate it into a string
-    splitArray = newArray.split('');    // break elements of an array 
-    if (splitArray[0] == splitArray[2]){
+let i = 0;
+for (i = 500; i < 1000; i++) {
+    const numbers = i;
+    const newArray = numbers.toString(); // create an array and translate it into a string
+    const splitArray = newArray.split(''); // break elements of an array
+    if (splitArray[0] === splitArray[2]) {
         console.log(splitArray.join(''));
     }
 }
