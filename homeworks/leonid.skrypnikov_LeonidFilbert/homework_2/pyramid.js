c88122d2152401d1753ba5635aa563d7b055217e
LeonidFilbert <leonidfilbert@gmail.com>
2018-11-14 19:23:18 +0200

    homework2

---
+++ homeworks/leonid.skrypnikov_LeonidFilbert/homework_2/pyramid.js
@@ -0,0 +1,23 @@
+const readline = require('readline');
+const SPACE_S = ' ';
+const SHARP_S = '##';
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Type count of floors ', (answer) => {
+  console.log(typeof answer);
+  if ((typeof +answer) != 'number') {
+    console.log('wrong type')
+    rl.close();
+    return;
+  }	
+
+  for (let i = 0; i < answer; i++) {
+    console.log(SPACE_S.repeat(answer - i) + SHARP_S.repeat(i + 2));
+  }
+
+  rl.close();
+});
