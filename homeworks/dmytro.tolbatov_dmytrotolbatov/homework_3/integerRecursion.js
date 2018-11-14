397c35af260cba38e26c71c8c69e9425063e52df
dmytro <dmytrotolbatov@gmail.com>
2018-11-14 23:18:12 +0200

    homework3

---
+++ homeworks/dmytro.tolbatov_dmytrotolbatov/homework_3/integerRecursion.js
@@ -0,0 +1,30 @@
+const readline = require('readline');
+
+function integerRecursion(current, maxNumber) {
+    if (current <= maxNumber) {
+        if (current % 2 === 0) {
+            console.log(current);
+        }
+
+        return integerRecursion(current + 1, maxNumber);
+    }
+}
+
+const integerQuestion = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+integerQuestion.question('What is the range of numbers? Print two numbers (ex: 10, 600).', (range) => {
+    const rangeArray = range.split(',');
+    const min = Number(rangeArray[0]);
+    const max = Number(rangeArray[1]);
+
+    if (rangeArray.length > 2 || Number.isNaN(min) || Number.isNaN(max) || min >= max) {
+        console.log('Incorrect Input!');
+    } else {
+        integerRecursion(min, max);
+    }
+
+    integerQuestion.close();
+});
