ca2b442ce6808c07c5234c24029702909cbd690d
yaroslav <yaroslav9190@gmail.com>
2018-11-14 15:27:12 +0200

    all fixed

--- homeworks/Yaroslav.Melnyk_Maritodd/homework_2.1/pyra.js
+++ homeworks/Yaroslav.Melnyk_Maritodd/homework_2.1/pyra.js
@@ -1,22 +1,24 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
- 
-function pyra(x){
+
+function pyra(num) {
     let j;
-    let i =2;
-    var spc = ' ';
-    var str = '#';
-    for (j = x; j >=1; j--){
+    let i = 2;
+    const spc = ' ';
+    const str = '#';
+    for (j = num; j >= 1; j--) {
         i += 2;
         console.log(spc.repeat(j) + str.repeat(i));
     }
 }
 
-rl.question('Enter number of strings: ', (x) => {
-    pyra(x);
+rl.question('Enter number of strings: ', (num) => {
+    pyra(num);
     rl.close();
 });
