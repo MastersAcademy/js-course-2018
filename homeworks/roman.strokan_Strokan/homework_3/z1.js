b0ba5f4b98d49135e97fb8718e8e16caced34237
Strokan <srsche01@gmail.com>
2018-11-15 02:21:34 +0200

    homework3 15.11 02:22

---
+++ homeworks/roman.strokan_Strokan/homework_3/z1.js
@@ -0,0 +1,46 @@
+
+// 1.1
+function func(a) {
+    const b = a * a;
+    // console.log(`${a}^2=${b}`);
+    return b;
+}
+// func(5);
+
+// 1.2
+function arrayCreation(a, b) {
+    const arr = [];
+    let c = 0;
+    let i = 0;
+    do {
+        arr[i] = c;
+        c += a;
+        i++;
+    } while (c <= b);
+    /* for (let j = 0; j < arr.length; j++) {
+        console.log(arr[j]);
+    } */
+    return arr;
+}
+arrayCreation(4, 32);
+
+// 1.3
+function arrayElementsSq(a) {
+    const arr2 = [];
+    for (let i = 0; i < a.length; i++) {
+        arr2[i] = func(a[i]);
+        // console.log(arr2[i]);
+    }
+    return arr2;
+}
+arrayElementsSq(arrayCreation(4, 32));
+
+// 1.4
+function arrayElementsSum(a) {
+    let sum = 0;
+    for (let i = 0; i < a.length; i++) {
+        sum += a[i];
+    }
+    return sum;
+}
+console.log(arrayElementsSum(arrayElementsSq(arrayCreation(4, 32))));
