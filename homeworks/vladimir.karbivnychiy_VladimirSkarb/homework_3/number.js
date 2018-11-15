5d5f1e81ef9ae0e8e2dd2d2472d11a464742c99c
Vladimir Karbivnychiy <vovan63@gmail.com>
2018-11-15 15:52:11 +0200

    Fix2

--- homeworks/vladimir.karbivnychiy_VladimirSkarb/homework_3/number.js
+++ homeworks/vladimir.karbivnychiy_VladimirSkarb/homework_3/number.js
@@ -8,7 +8,7 @@ console.log(`${53}^2=${func(53)}`);
 
 function block(a, b) {
     const array = [];
-    for (let i=0; i<=a; i+=b) {
+    for (let i = 0; i <= a; i += b) {
         array.push(i);
     }
     return array;
@@ -16,12 +16,12 @@ function block(a, b) {
 console.log(block(50, 5));
 console.log(block(32, 4));
 
-function arraySq(block) {
-    return block.map(element => element * element);
+function arraySq(array) {
+    return array.map(element => element * element);
 }
 console.log(arraySq(block(32, 4)));
 
-function sumArraySq(block) {
-    return block.reduce((sum, nextNum) => sum + nextNum);
+function sumArraySq(array) {
+    return array.reduce((sum, nextNum) => sum + nextNum);
 }
 console.log(sumArraySq(arraySq(block(32, 4))));
