84220db17c626427b26803d7a9df324137fcceb9
Strokan <srsche01@gmail.com>
2018-11-15 23:15:15 +0200

    hw3 fix 2

--- homeworks/roman.strokan_Strokan/homework_3/z1.js
+++ homeworks/roman.strokan_Strokan/homework_3/z1.js
@@ -2,43 +2,33 @@
 // 1.1
 function func(a) {
     const b = a * a;
-    // console.log(`${a}^2=${b}`);
     return b;
 }
-// func(5);
-
+console.log(func(5));
 // 1.2
 function arrayCreation(a, b) {
     const arr = [];
-    let c = 0;
-    for (let i = 0; c <= b; i++) {
-        arr[i] = c;
-        c += a;
+    for (let i = 0; i <= b; i += a) {
+        arr.push(i);
     }
-    /* for (let j = 0; j < arr.length; j++) {
-        console.log(arr[j]);
-    } */
     return arr;
 }
-arrayCreation(4, 32);
+
+console.log(arrayCreation(4, 32));
 
 // 1.3
 function arrayElementsSq(a) {
     const arr2 = [];
     for (let i = 0; i < a.length; i++) {
-        arr2[i] = func(a[i]);
-        // console.log(arr2[i]);
+        arr2.push(func(a[i]));
     }
     return arr2;
 }
-arrayElementsSq(arrayCreation(4, 32));
+console.log(arrayElementsSq(arrayCreation(4, 32)));
 
 // 1.4
 function arrayElementsSum(a) {
-    let sum = 0;
-    for (let i = 0; i < a.length; i++) {
-        sum += a[i];
-    }
+    const sum = a.reduce((x, y) => x + y);
     return sum;
 }
 console.log(arrayElementsSum(arrayElementsSq(arrayCreation(4, 32))));
--- homeworks/roman.strokan_Strokan/homework_3/z2.js
+++ homeworks/roman.strokan_Strokan/homework_3/z2.js
@@ -1,5 +1,3 @@
-let a;
-
 function exp2(n) {
     if (n > 1) {
         return exp2(n / 2);
@@ -11,8 +9,7 @@ function exp2(n) {
 }
 function mainFunc(x, y) {
     for (let i = x; i <= y; i++) {
-        a = exp2(i);
-        if (a === true) {
+        if (exp2(i) === true) {
             console.log(i);
         }
     }
