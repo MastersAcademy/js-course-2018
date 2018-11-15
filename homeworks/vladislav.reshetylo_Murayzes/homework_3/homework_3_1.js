d035937516dbe6f510bd2b0d81c0a5589eff616c
Murayzes <murayzes@gmail.com>
2018-11-15 21:57:21 +0200

    vladislav_reshetylo_hw_3_1

---
+++ homeworks/vladislav.reshetylo_Murayzes/homework_3/homework_3_1.js
@@ -0,0 +1,35 @@
+// task 1.1
+function square(n) {
+    return n * n;
+}
+console.log(square(5));
+console.log(square(15));
+console.log(square(53));
+
+// task 1.2
+function createArray(step, value) {
+    const newArray = [];
+    let i = 0;
+    for (i = 0; i <= value; i += step) {
+        newArray.push(i);
+    }
+    return newArray;
+}
+console.log(createArray(5, 50));
+console.log(createArray(4, 32));
+
+// task 1.3
+function squareArray(newArray) {
+    const number = Number => Number * Number;
+    const sqrArray = newArray.map(number);
+    return sqrArray;
+}
+console.log(squareArray(createArray(4, 32)));
+
+// task 1.4
+function sumArray(newArray) {
+    const reducer = (accuumulator, currentValue) => accuumulator + currentValue;
+    const sArray = newArray.reduce(reducer);
+    return sArray;
+}
+console.log(sumArray(squareArray(createArray(4, 32))));
