f0bd03f0391bdfcf68bea0de2068c486af7beb2b
junyaa <internet_ck@ukr.net>
2018-11-16 12:51:57 +0200

    homework_3

---
+++ homeworks/yuriy.piddubnyak_junyaa/homework_3/task_1.js
@@ -0,0 +1,7 @@
+
+function square(a) {
+    return (a ** 2);
+}
+console.log(square(5));
+console.log(square(15));
+console.log(square(53));
---
+++ homeworks/yuriy.piddubnyak_junyaa/homework_3/task_2.js
@@ -0,0 +1,13 @@
+function func(x) {
+    if (typeof x !== 'number') {
+        return 'Not a number';
+    }
+    return (Math.log(x) / Math.log(2)) % 1 === 0;
+}
+const min = 10;
+const max = 600;
+for (let a = min; a <= max; a++) {
+    if (func(a) == 1) {
+        console.log(a);
+    }
+}
