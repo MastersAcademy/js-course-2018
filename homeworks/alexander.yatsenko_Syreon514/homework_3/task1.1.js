4294e931d469eab6e3e86a364ddf47ec6386d12e
Alexander Yatsenko <syreon90@gmail.com>
2018-11-16 21:03:29 +0200

    homework_3 v1.1

--- homeworks/alexander.yatsenko_Syreon514/homework_3/task1.1.js
+++ homeworks/alexander.yatsenko_Syreon514/homework_3/task1.1.js
@@ -1,11 +1,7 @@
-const firstVariable = 5;
-const secondVariable = 15;
-const thirdVariable = 53;
-
 function square(number) {
     return number * number;
 }
 
-console.log(`${firstVariable} ^ 2 = ${square(firstVariable)}`);
-console.log(`${secondVariable} ^ 2 = ${square(secondVariable)}`);
-console.log(`${thirdVariable} ^ 2 = ${square(thirdVariable)}`);
+console.log(square(5));
+console.log(square(15));
+console.log(square(53));
--- homeworks/alexander.yatsenko_Syreon514/homework_3/task1.2.js
+++ homeworks/alexander.yatsenko_Syreon514/homework_3/task1.2.js
@@ -1,11 +1,3 @@
-const firstStartingPoint = 0;
-const firstEndingPoint = 50;
-const firstStep = 5;
-
-const secondStartingPoint = 0;
-const secondEndingPoint = 32;
-const secondStep = 4;
-
 function createArray(start, finish, step) {
     const results = [];
     for (let i = start; i <= finish; i += step) {
@@ -14,5 +6,5 @@ function createArray(start, finish, step) {
     return results;
 }
 
-console.log(createArray(firstStartingPoint, firstEndingPoint, firstStep));
-console.log(createArray(secondStartingPoint, secondEndingPoint, secondStep));
+console.log(createArray(0, 50, 5));
+console.log(createArray(0, 32, 4));
