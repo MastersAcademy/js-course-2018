f64308a71c915bb717739dd955aa70353707745c
YulyKo <juliakotenko.forstudy@gmail.com>
2018-11-17 01:12:49 +0200

    homework_3 task01:  output of values with border entry and step

--- homeworks/yulia.kotenko_YulyKo/homework_3/task01.js
+++ homeworks/yulia.kotenko_YulyKo/homework_3/task01.js
@@ -5,3 +5,23 @@ function riseToDegree(a) {
 console.log(riseToDegree(5));
 console.log(riseToDegree(15));
 console.log(riseToDegree(53));
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+rl.question('Max value ', (answer) => {
+    rl.question('Step ', (answer2) => {
+        let arr = [];
+        let step = +answer2;
+        let size = +answer;
+    for (let i = 0; i <= size; i+=step) {
+        arr.push(i);
+    }
+    console.log(arr);
+            rl.close();
+    });
+});
+
