b0a5fee0b2e01e11a92173e483a86ca060d97d74
KTsarenko <konstantin.tsarenko@gmail.com>
2018-11-16 22:41:35 +0200

    task 1.2

--- homeworks/konstantin.tsarenko_KTsarenko/homework_3/hw3-3.js
+++ homeworks/konstantin.tsarenko_KTsarenko/homework_3/hw3-3.js
@@ -1,7 +1,24 @@
+// task 1.1
+
 let numb1 = 5;
 let numb2 = 15;
 let numb3 = 53;
 numb1 **= 2;
 numb2 **= 2;
 numb3 **= 2;
-console.log(numb1 + ',' + numb2 + ',' + numb3);
+const resArr = [numb1, numb2, numb3];
+console.log(`Task 1.1: ${resArr}`);
+
+// task 1.2.1
+
+const minValue = 0;
+const stepValue = 5;
+const maxValue = 50;
+let i = minValue;
+const resValue = [];
+for (i; i <= maxValue; i += stepValue) {
+    resValue.push(i);
+}
+console.log(`Task 1.2: ${resValue}`);
+
+// task 1.2
