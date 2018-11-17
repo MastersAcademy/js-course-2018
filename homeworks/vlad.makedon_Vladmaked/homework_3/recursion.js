8fff443dcfae80113c1ffac436c465f9e249a9e0
Vladmaked <makedohska@ukr.net>
2018-11-17 18:24:13 +0200

    fixed mistakes

--- homeworks/vlad.makedon_Vladmaked/homework_3/recursion.js
+++ homeworks/vlad.makedon_Vladmaked/homework_3/recursion.js
@@ -3,16 +3,22 @@ const rl = require('readline').createInterface(process.stdin, process.stdout);
 rl.question('Enter the beginning of the range: ', (startRange) => {
     rl.question('Enter end of range: ', (endRange) => {
         rl.close();
-        function getNum(n, start, end) {
-            if ((n >= start) && (n <= end)) {
-                console.log(n);
+        function numCheck(n) {
+            if (n > 3 && Number.isInteger(n)) {
+                return numCheck(n / 2);
             }
-            if (n > end) {
-                return n;
+            if (n === 2) {
+                return true;
             }
-            return getNum(n * 2, startRange, endRange);
+            return false;
         }
-        const start = Math.ceil(Math.log(startRange, 2));
-        getNum(2 ** start);
+        function getNum(start, end) {
+            for (let i = start; i <= end; i++) {
+                if (numCheck(i) === true && i !== 2) {
+                    console.log(i);
+                }
+            }
+        }
+        getNum(startRange, endRange);
     });
 });
