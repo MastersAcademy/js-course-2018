95fe62f45f19c3ae06d7a300e220f46a7e0dd4b1
brilux <aavv22oo8ntfku>
2018-11-17 04:42:59 +0200

    homework_3 fixes

--- homeworks/artem.bilenko_brilux/homework_3/homework_3_task_1.js
+++ homeworks/artem.bilenko_brilux/homework_3/homework_3_task_1.js
@@ -4,40 +4,29 @@ const inputData = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-
-inputData.question('Enter the number to square it: ', (x) => {
+inputData.question('Enter the number to square it: ', (number) => {
     inputData.question('Enter the max value of array to create it: ', (maxValue) => {
         inputData.question('Enter the step of array to create it: ', (step) => {
             inputData.close();
+            const pow = x => x * x;
+            console.log(pow(number));
 
-            const pow = () => x * x;
-            console.log(pow());
-
-            let arr = [];
-
-            const fillArray = () => {
-                for (let i = 0; i < maxValue / step + 1; i++) {
-                    arr.push(i * step);
+            const intStep = parseInt(step, 10);
+            const arr = [];
+            const fillArray = (size, stepSize) => {
+                for (let i = 0; i <= size; i += stepSize) {
+                    arr.push(i);
                 }
                 return arr;
             };
+            console.log(fillArray(maxValue, intStep));
 
-            console.log(fillArray(maxValue, step));
+            const powArray = array => array.map(element => element * element);
+            console.log(powArray(arr));
+            const arrForSum = powArray(arr);
 
-            const powArray = () => {
-                arr = arr.map(element => element * element);
-                return arr;
-            };
-            console.log(powArray());
-
-            const sumArray = () => {
-                let sum = 0;
-                for (let i = 0; i < arr.length; i++) {
-                    sum += arr[i];
-                }
-                return sum;
-            };
-            console.log(sumArray());
+            const sumArray = array => array.reduce((element, current) => element + current);
+            console.log(sumArray(arrForSum));
         });
     });
 });
--- homeworks/artem.bilenko_brilux/homework_3/homework_3_task_2.js
+++ homeworks/artem.bilenko_brilux/homework_3/homework_3_task_2.js
@@ -9,25 +9,23 @@ inputData.question('Enter the min value of diapason: ', (minValue) => {
     inputData.question('Enter the max value of diapason: ', (maxValue) => {
         inputData.close();
         const arr = [];
-        const result = [];
         for (let i = minValue; i < maxValue; i++) {
             arr.push(i);
         }
-        for (let i = 0; i < arr.length; i++) {
-            const check = (x) => {
-                if (x === 1) {
-                    return 1;
-                }
-                if (x > 1 && x < 2) {
-                    return 0;
-                }
+        const check = (x) => {
+            if (x === 1) {
+                return true;
+            }
+            if (x >= 2) {
                 return check(x / 2);
-            };
+            }
+            return false;
+        };
+        for (let i = 0; i < arr.length; i++) {
             const x = arr[i];
-            if (check(x) === 1) {
-                result.push(x);
+            if (check(x) === true) {
+                console.log(x);
             }
         }
-        console.log(result);
     });
 });
