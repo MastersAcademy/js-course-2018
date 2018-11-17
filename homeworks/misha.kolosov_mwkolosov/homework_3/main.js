767e6a976e68ab3fb462b0660d72df3a60f36068
Misha <mwkolosov@gmail.com>
2018-11-17 15:34:36 +0200

    edit hm2

--- homeworks/misha.kolosov_mwkolosov/homework_3/main.js
+++ homeworks/misha.kolosov_mwkolosov/homework_3/main.js
@@ -1,15 +1,15 @@
 
 // Task 1.1
-function square(a) {
-    return a * a;
+function square(num) {
+    return num * num;
 }
 console.log(square(53));
 
 // Task 1.2
 
-function arr(a, b) {
+function arr(max, step) {
     const arrM = [];
-    for (let i = 0; i <= a; i += b) {
+    for (let i = 0; i <= max; i += step) {
         arrM.push(i);
     }
     return arrM;
@@ -46,9 +46,11 @@ console.log(arrSum());
 const recursion = (n) => {
     if (n === 1) {
         return 1;
-    } if (n < 1) {
+    }
+    if (n < 1) {
         return 0;
-    } return recursion(n / 2);
+    }
+    return recursion(n / 2);
 };
 
 for (let i = 1; i <= 600; i++) {
