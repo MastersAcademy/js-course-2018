040dd555879f972cd2487dde5184fcb925ee7cb0
Alexey Berkut <fallenorn@gmail.com>
2018-11-17 17:09:53 +0200

    ok

--- homeworks/Alexey.Berkut_AlBerkut/homework_2/piramid.js
+++ homeworks/Alexey.Berkut_AlBerkut/homework_2/piramid.js
@@ -1,11 +1,12 @@
 function build(num) {
+    let li = num;
     const symbl = '#';
     const space = '_';
     let i = 2;
 
-    for (num; num > 0; num--) {
-        i = i + 2;
-        console.log(space.repeat(num) + symbl.repeat(i));
+    for (li > 0; li--;) {
+        i += 2;
+        console.log(space.repeat(li) + symbl.repeat(i));
     }
 }
 
