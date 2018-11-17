eb57e1007d1db6f584b3d5ae214f9e07d56bd705
KTsarenko <konstantin.tsarenko@gmail.com>
2018-11-17 12:30:19 +0200

    task 1.5

--- homeworks/konstantin.tsarenko_KTsarenko/homework_3/hw3-3.js
+++ homeworks/konstantin.tsarenko_KTsarenko/homework_3/hw3-3.js
@@ -47,12 +47,46 @@ resSqr(sqr(arrCreate(0, 32, 4)));
 
 function arraySum(array) {
     let sum = 0;
-    console.log(array);
     for (let i = 0; i < array.length; i++) {
         sum += parseInt(array[i], 10);
-        console.log(sum);
     }
     console.log(`Task 1.4: ${sum}`);
 }
 
 arraySum(sqr(arrCreate(0, 32, 4)));
+
+// task 1.5
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('Ведите минимальное значение: ', (minValue) => {
+    rl.question('Ведите максимальное значение: ', (maxValue) => {
+        rl.close();
+        const arrValue = [];
+        const resValue = [];
+        for (let i = minValue; i < maxValue; i++) {
+            arrValue.push(i);
+        }
+        for (let i = 0; i < arrValue.length; i++) {
+            const check = (x) => {
+                if (x === 1) {
+                    return 1;
+                }
+                if (x > 1 && x < 2) {
+                    return 0;
+                }
+                return check(x / 2);
+            };
+            const x = arrValue[i];
+            if (check(x) === 1) {
+                resValue.push(x);
+            }
+        }
+        console.log(`Task 1.5: ${resValue}`);
+    });
+});
