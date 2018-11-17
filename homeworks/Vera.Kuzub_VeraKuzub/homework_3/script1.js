6a54054dc4bf2904a7b2e233266c28fcd6f095fd
VeraKuzub <v.kuzub@gmail.com>
2018-11-17 17:20:28 +0200

    added homework_3

---
+++ homeworks/Vera.Kuzub_VeraKuzub/homework_3/script1.js
@@ -0,0 +1,37 @@
+function findDegreeOfTwo(num) {
+    return num * num;
+}
+
+function createArray(num, step) {
+    const arr = [];
+    let number = 0;
+    let i = 0;
+    while (number <= num) {
+        arr[i] = number;
+        i++;
+        number += step;
+    }
+    return arr;
+}
+
+function sum(arr) {
+    let result = 0;
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
+}
+
+console.log('5^2: ', findDegreeOfTwo(5));
+console.log('15^2: ', findDegreeOfTwo(15));
+console.log('53^2: ', findDegreeOfTwo(53));
+
+console.log('createArray(50,5): ', createArray(50, 5));
+console.log('createArray(32,4): ', createArray(32, 4));
+
+
+const array = createArray(32, 4);
+console.log('Input array: ', array);
+const newArray = array.map(findDegreeOfTwo);
+console.log('all the values of the array elements bring to the square: ', newArray);
+console.log('Sum of all the values of the array elements: ', sum(newArray));
