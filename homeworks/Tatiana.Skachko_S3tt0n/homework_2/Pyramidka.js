c6b3e65d4815a9c17229f11623473b64857d65c1
S3tt0n <0ksf0rd222@gmail.com>
2018-11-17 23:21:29 +0200

    Added h2 tag

--- homeworks/Tatiana.Skachko_S3tt0n/homework_2/Pyramidka.js
+++
@@ -1,7 +0,0 @@
-let pyrHeight = 17;
-let firstStr = '##';
-let secondStr = ' ';
-
-for (i = 2; i < pyrHeight; i++) {
-    console.log(secondStr.repeat((pyrHeight * firstStr.length - firstStr.length * i) / 2) + firstStr.repeat(i));
-}
