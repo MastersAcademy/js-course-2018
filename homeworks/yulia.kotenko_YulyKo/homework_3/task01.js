1d548d277f0e00799305b48265fca1902e16cdd8
YulyKo <juliakotenko.forstudy@gmail.com>
2018-11-17 23:34:54 +0200

    homework_3 task01: 1.2 done

--- homeworks/yulia.kotenko_YulyKo/homework_3/task01.js
+++ homeworks/yulia.kotenko_YulyKo/homework_3/task01.js
@@ -2,3 +2,14 @@ function riseToDegree(a) {
     return a * a;
 }
 console.log(`5: ${riseToDegree(5)}\n15: ${riseToDegree(15)}\n53: ${riseToDegree(53)}`);
+
+function createArr() {
+    const arr = [];
+    const max = 50;
+    const step = 5;
+    for (let i = 0; i <= max; i += step) {
+        arr.push(i);
+    }
+    return arr;
+}
+console.log(createArr());
