bf7e74902dc10f6f1fbda5728ea087a8438787d7
Dmytro Tyshchenko <kwazar2008@ukr.net>
2018-11-17 22:41:42 +0200

    Task_1

---
+++ homeworks/Dmytro.Tyshchenko_DmytroTy/homework_3/square2.js
@@ -0,0 +1,35 @@
+// Task 1.1
+
+function square(num) {
+    return num * num;
+}
+
+let numbs = [5, 15, 53];
+numbs.forEach(num => console.log(`${num}^2 = ${square(num)}`));
+
+// Task 1.2
+
+function createMassiv(max, step) {
+    const result = [];
+    let num = 0;
+    for (let i = 0; num <= max; i++) {
+        result[i] = num;
+        num += step;
+    }
+    return result;
+}
+
+console.log(createMassiv(50, 5));
+console.log(createMassiv(32, 4));
+
+// Task 1.3
+
+numbs = createMassiv(32, 4);
+const sqNumbs = numbs.map(square);
+console.log(sqNumbs);
+
+// Task 1.4
+
+let sum = 0;
+sqNumbs.forEach((num) => { sum += num; });
+console.log(sum);
