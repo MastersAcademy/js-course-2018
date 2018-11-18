4b1da2e2ecdd70b14e6fb3e5d8f3447cbc67d0fe
PavelOgienko <pavlik182@gmail.com>
2018-11-18 11:14:48 +0200

    fix

--- homeworks/pavel.ogienko_pavelogienko/homework_3/homework3_1.js
+++ homeworks/pavel.ogienko_pavelogienko/homework_3/homework3_1.js
@@ -1,11 +1,11 @@
 //task1.1
 
-function square(number) {
+function pow(number) {
     return number * number;
 }
-console.log(square(5));
-console.log(square(15));
-console.log(square(53));
+console.log(pow(5));
+console.log(pow(15));
+console.log(pow(53));
 
 //task1.2
 
