f57fa55b43f88c2417a36f78a47fe082ec617b83
Igor <34414563+IgorHordiichuk@users.noreply.github.com>
2018-11-18 04:00:19 +0200

    Update arr_func.js

--- homeworks/igor.gordichuk_IgorHordiichuk/homework_3/arr_func.js
+++ homeworks/igor.gordichuk_IgorHordiichuk/homework_3/arr_func.js
@@ -3,9 +3,9 @@
 function squareNum(num) {
     return num * num;
 }
-console.log(`Square root of 5 is: ${squareNum(5)}`);
-console.log(`Square root of 5 is: ${squareNum(15)}`);
-console.log(`Square root of 5 is: ${squareNum(53)}`);
+console.log(`Square of 5 is: ${squareNum(5)}`);
+console.log(`Square of 5 is: ${squareNum(15)}`);
+console.log(`Square of 5 is: ${squareNum(53)}`);
 
 // task 1.2
 
@@ -22,7 +22,7 @@ console.log(`Last number is 32, step are 4: ${arrWithStep(32, 4)}`);
 // task 1.3
 
 const arrSquareNum = arrWithStep(32, 4).map(squareNum);
-console.log(`Square root of all of the nums between 0 adn 32 with step 4 are: \n${arrSquareNum}`);
+console.log(`Square of all of the nums between 0 adn 32 with step 4 are: \n${arrSquareNum}`);
 
 // task 1.4
 
