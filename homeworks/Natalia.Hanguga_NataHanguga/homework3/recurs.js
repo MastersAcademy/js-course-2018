a6ccc8c4bb24352b6311594ddeb683f1c8b28bda
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-18 15:37:20 +0200

    Update recurs.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework3/recurs.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework3/recurs.js
@@ -1,23 +1,13 @@
-let n;
-for (let i = 0; i < 10; i++) {
-    n = 2 ** i;
-    if (n > 10 && n < 600) {
-        // console.log(n);
+function twoSquare(num) {
+    if (num % 2 === 0) {
+        return twoSquare(num / 2);
+    }
+    return num === 1;
+}
+const result = [];
+for (let i = 10; i <= 600; i++) {
+    if (twoSquare(i)) {
+        result.push(i);
     }
 }
-
-function rec(i) {
-    let n;
-    if (i === 1) {
-        return 1;
-    }
-    else {
-        // if (n === 2 ** i) {
-        //  console.log(n);
-        return i * rec(i - 1);
-        // }
-        n++;
-
-    }
-}
-rec(10);
+console.log(`Power's of 2 : ${result}`);
