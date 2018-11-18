b2fa6519ab32032c4ddada96c92a9a37901837f4
Klymenko69 <Klymenko696@gmail.com>
2018-11-18 15:59:20 +0200

    initial commit

---
+++ homeworks/artem.klymenko_Klymenko69/homeworks_3/2.js
@@ -0,0 +1,18 @@
+// 2
+
+const rec = (numb) => {
+    if (numb === 1) {
+        return true;
+    } if (numb < 1) {
+        return false;
+    }
+    return rec(numb / 2);
+};
+const poe = (minNumb, maxNumb) => {
+    for (let i = minNumb; i <= maxNumb; i++) {
+        if (rec(i)) {
+            console.log(i);
+        }
+    }
+};
+poe(10, 600);
