b15012ceecae3a163ddd853d8cd619e2ee11950c
NikolasGumenyuk <43613315+NikolasGumenyuk@users.noreply.github.com>
2018-11-18 16:12:15 +0200

    Update namPow.js

--- homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/namPow.js
+++ homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/namPow.js
@@ -1,21 +1,9 @@
-const readline = require('readline');
-
-const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
 function squareNum(...args) {
     for (let i = 0; i < args.length; i++) {
         console.log(args[i] ** 2);
     }
 }
-
-r1.question('First number: ', (first) => {
-    r1.question('Second number: ', (second) => {
-        r1.question('Third number: ', (third) => {
-            squareNum(first, second, third);
-            r1.close();
-        });
-    });
-});
+const first = 5;
+const second = 15;
+const third = 53;
+squareNum(first, second, third);
