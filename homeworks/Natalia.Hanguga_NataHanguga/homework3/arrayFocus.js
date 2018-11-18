a0703194538059c8484c2217fc749594e913b70b
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-18 18:12:33 +0200

    Update arrayFocus.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework3/arrayFocus.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework3/arrayFocus.js
@@ -1,7 +1,7 @@
-//task1.2
+//  task1.2
+
 function createArray() {
     const arr = [];
-    let arr1 = [];
     const max = 32;
     const step = 4;
     for (let n = 0; n < max; n += step) {
@@ -11,14 +11,18 @@ function createArray() {
     console.log(arr);
     return arr
 }
-    //task 1.3
+
+    //  task 1.3
+
 function arrElemSquare(){
     const arr = createArray();
     let arr1 = arr.map(el => el*el);
     console.log(arr1);
     return arr1;
 }
-    //task1.4
+
+    //  task1.4
+
 function sumArray(){
     const arr1 = arrElemSquare();
     const sum = arr1.reduce((a, b) => a + b);
