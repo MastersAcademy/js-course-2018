e9cce89185ecd582ba6974941a35d79363f0f45a
NikolasGumenyuk <43613315+NikolasGumenyuk@users.noreply.github.com>
2018-11-18 15:03:03 +0200

    Update createArray.js

--- homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/createArray.js
+++ homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/createArray.js
@@ -4,23 +4,19 @@ const r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
-let arr = [];
+const arr = [];
 let n = 0;
 r1.question('Max element in array: ', (max) => {
     r1.question('Step of array: ', (step) => {
         while (n < Number(max)) {
-            arr.push(n)
+            arr.push(n);
             n += Number(step);
             console.log(n);
         }
         console.log(arr);
-        let arr1 = arr.map((el) => {
-            return el ** 2;
-        });
+        const arr1 = arr.map((el) => return el ** 2);
         console.log(arr1);
-        let sum = arr1.reduce((a, b) => {
-            return a + b;
-        });
+        const sum = arr1.reduce((a, b) => return a + b);
         console.log(sum);
         r1.close();
 
