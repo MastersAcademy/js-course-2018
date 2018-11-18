08811e3e7836c0bbc776fac4766b3b83e7f7f471
Helen-Mary <44731861+Helen-Mary@users.noreply.github.com>
2018-11-18 04:00:01 +0200

    Update htask1.js

--- homeworks/Maria.Priamuhina_Helen-Mary/Homework3/htask1.js
+++ homeworks/Maria.Priamuhina_Helen-Mary/Homework3/htask1.js
@@ -2,59 +2,59 @@ const readline = require('readline');
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
- /*task1.1*/
+/* task1.1 */
 function Square(num) {
-      return num * num;
-  }
-  
-  rl.on('line', (input) => {
-    const num = input;
+  return num * num;
+}
+
+rl.on('line', (input) => {
+  const num = input;
   rl.close();
   console.log(Square(num));
-  });
+});
 
-  /*task 1.2*/
+/* task 1.2 */
 function CreateArray(end, step) {
   const array = [0];
   const newEnd = Number(end);
   const newStep = Number(step);
   let temp = 0;
-  for (let i = 1; (temp + newStep) <= newEnd; i++) {
+  for (let i = 1; (temp + newStep) <= newEnd; i += 1) {
     array[i] = temp + newStep;
     temp += newStep;
   }
-  for (let i = 0; i < array.length; i++) { console.log(array[i]); }
+  for (let i = 0; i < array.length; i += 1) { console.log(array[i]); }
   return array;
 }
-rl.on('line', (input) => {
-  const temp = input.split(' ');
-  const end = temp[0];
-  const step = temp[1];
-rl.close();
-const arr = CreateArray(end, step);
-console.log('\n');
-SquareArray(arr);
-});
 
-/*task 1.3*/
+/* task 1.4 */
+function SumOfElems(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i += 1) {
+    sum += array[i];
+  }
+  console.log(sum);
+}
+/* task 1.3 */
 function SquareArray(array) {
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < array.length; i += 1) {
     const temp = Square(array[i]);
+    // eslint-disable-next-line no-param-reassign
     array[i] = temp;
     console.log(array[i]);
   }
   console.log('\n');
   SumOfElems(array);
 }
-
-/*task 1.4*/
-function SumOfElems(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  console.log(sum);
-}
+rl.on('line', (input) => {
+  const temp = input.split(' ');
+  const end = temp[0];
+  const step = temp[1];
+  rl.close();
+  const arr = CreateArray(end, step);
+  console.log('\n');
+  SquareArray(arr);
+});
