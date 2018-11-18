8d4289459b26aeebeb341b839b730384faa7a2b1
melnyksan <andriimelnyk986@gmail.com>
2018-11-18 19:08:44 +0200

    js-hw-3__3a_array-function_full__3b_recursion_done__20181118

--- homeworks/andrii.melnyk_melnyksan/homework_3/js-hw-3b_function-recursion.js
+++ homeworks/andrii.melnyk_melnyksan/homework_3/js-hw-3b_function-recursion.js
@@ -3,31 +3,27 @@
 // Output numbers, that is power of two (1, 2, 4, 8, 16, ..., 512, etc).
 // Use the recursion to check if the number is the power of the two.
 
-// const isPowOfTwo = function isPowOfTwo(n) {
-//     if (typeof n !== 'number') {
-//         return 'Not a number';
-//     }
-//     return n && (n & (n - 1)) === 0;
-// };
+const isPowOfTwo = function isPowerOfTwo(num) {
+    if (num < 1) {
+        return false;
+    } if (num === 1) {
+        return true;
+    }
+    return isPowerOfTwo(num / 2);
+};
 
-// const isAllNumbersPowerOfTwo = function () {
-//     const min = 10;
-//     const max = 600;
-//     const arrayService = [];
+const isAllNumbersPowerOfTwo = function () {
+    const min = 10;
+    const max = 600;
+    const arrayService = [];
 
-//     for (let i = min; i <= max; i++) {
-//         if (isPowOfTwo(i) === true) {
-//             arrayService.push(i);
-//         }
-//     }
+    for (let i = min; i <= max; i++) {
+        if (isPowOfTwo(i) === true) {
+            arrayService.push(i);
+        }
+    }
 
-//     return arrayService;
-// };
+    return arrayService;
+};
 
-// console.log('\nisAllNumbersPowerOfTwo', isAllNumbersPowerOfTwo(), '\n');
-
-// Sorry, I stuck on this task.
-// I could not find another way to check if the number is the power of the two
-// or come up with my own way.
-// So I commented the code above to make a commit and send the pool request.
-// while I still try to understand recursion, this is my draft so far.
+console.log('\nisAllNumbersPowerOfTwo', isAllNumbersPowerOfTwo(), '\n');
