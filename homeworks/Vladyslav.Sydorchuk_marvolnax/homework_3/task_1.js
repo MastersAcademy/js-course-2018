41b8dc3f2d38e4cfcd710b833d9e2e84f104e38a
Marvolnax <marvolnax@gmail.com>
2018-11-18 22:02:34 +0200

    homework_3

--- homeworks/Vladyslav.Sydorchuk_marvolnax/homework_3/task_1.js
+++ homeworks/Vladyslav.Sydorchuk_marvolnax/homework_3/task_1.js
@@ -1,14 +1,14 @@
 // task 1.1
-function Square(x) {
+function square(x) {
     return x * x;
 }
 
-console.log(Square(5));
-console.log(Square(15));
-console.log(Square(53));
+console.log(square(5));
+console.log(square(15));
+console.log(square(53));
 
 // task 1.2
-function NewArray(max, step) {
+function newArray(max, step) {
     const arr = [];
     for (let i = 0; i <= max; i += step) {
         arr.push(i);
@@ -16,18 +16,18 @@ function NewArray(max, step) {
     return arr;
 }
 
-console.log(NewArray(50, 5));
-console.log(NewArray(32, 4));
+console.log(newArray(50, 5));
+console.log(newArray(32, 4));
 
 // task 1.3
-function ArraySquare(arr) {
+function arraySquare(arr) {
     return arr.map(x => x * x);
 }
-console.log(ArraySquare(NewArray(32, 4)));
+console.log(arraySquare(newArray(32, 4)));
 
 // task 1.4
-function ArraySum(arr) {
+function arraySum(arr) {
     return arr.reduce((sum, curent) => sum + curent);
 }
 
-console.log(ArraySum(ArraySquare(NewArray(32, 4))));
+console.log(arraySum(arraySquare(newArray(32, 4))));
