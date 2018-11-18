cb5200b4b0876380c80d256e33bcb3ac41072833
NikolasGumenyuk <43613315+NikolasGumenyuk@users.noreply.github.com>
2018-11-18 16:10:16 +0200

    Update createArray.js

--- homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/createArray.js
+++ homeworks/Nikolay.Gumenuik_NikolasGumenyuk/-homework_3/createArray.js
@@ -1,24 +1,17 @@
-const readline = require('readline');
+function createArray() {
+    const arr = [];
+    let arr1 = [];
+    const max = 32;
+    const step = 4;
+    for (let n = 0; n < max; n += step) {
+        arr.push(n);
+        console.log(n);
+    }
+    console.log(arr);
+    arr1 = arr.map(el => el ** 2);
+    console.log(arr1);
+    const sum = arr1.reduce((a, b) => a + b);
+    console.log(sum);
+}
 
-const r1 = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-const arr = [];
-let arr1 = [];
-let n = 0;
-r1.question('Max element in array: ', (max) => {
-    r1.question('Step of array: ', (step) => {
-        while (n < Number(max)) {
-            arr.push(n);
-            n += Number(step);
-            console.log(n);
-        }
-        console.log(arr);
-        arr1 = arr.map(el => el ** 2);
-        console.log(arr1);
-        const sum = arr1.reduce((a, b) => a + b);
-        console.log(sum);
-        r1.close();
-    });
-});
+createArray();
