854c5c66f54ab39b80e2db84375a15d2fd843c73
Klymenko69 <Klymenko696@gmail.com>
2018-11-19 17:35:37 +0200

    Piramida.js change

--- homeworks/artem.klymenko_Klymenko69/homeworks_2/Piramida.js
+++ homeworks/artem.klymenko_Klymenko69/homeworks_2/Piramida.js
@@ -1,7 +1,7 @@
 let a = '####';
-    let b = ' '; 
-    for(let i = 15; 1 < i; i--){ 
+let b = ' '; 
+for(let i = 15; 1 < i; i--){ 
  	}
-    for(let j = 1; j < 16; j++ ) {
+for(let j = 1; j < 16; j++ ) {
         }
-        console.log(b.repeat(+i) + a.repeat(+j));
+console.log(b.repeat(+i) + a.repeat(+j));
