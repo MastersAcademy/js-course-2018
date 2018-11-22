585f45a39c1bfa242997eef4486e395c4a3fd780
AlekseyPoddubnii <kymotocycle@gmail.com>
2018-11-22 19:30:38 +0200

    Work with Promise

--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_4/functions.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_4/functions.js
@@ -14,29 +14,47 @@ function minusTen(number) {
 
 // Task 1.1
 
-function renderMyCallback(number) {
-    setTimeout(
-        (firstArg) => {
-            setTimeout(
-                (secondArg) => {
-                    setTimeout(
-                        (thirdArg) => {
-                            console.log(minusTen(thirdArg));
-                        },
-                        1000,
-                        multiplyThree(secondArg),
-                    );
-                    console.log(multiplyThree(secondArg));
-                },
-                1000,
-                plusTen(firstArg),
-            );
-            console.log(plusTen(firstArg));
-        },
-        1000,
-        number,
-    );
-}
-renderMyCallback(11);
+// function renderMyCallback(number) {
+//     setTimeout(
+//         (firstArg) => {
+//             setTimeout(
+//                 (secondArg) => {
+//                     setTimeout(
+//                         (thirdArg) => {
+//                             console.log(minusTen(thirdArg));
+//                         },
+//                         1000,
+//                         multiplyThree(secondArg),
+//                     );
+//                     console.log(multiplyThree(secondArg));
+//                 },
+//                 1000,
+//                 plusTen(firstArg),
+//             );
+//             console.log(plusTen(firstArg));
+//         },
+//         1000,
+//         number,
+//     );
+// }
+// renderMyCallback(11);
 
 // Task 1.2
+// decFunction = declareted function at began
+
+function promiseDelay(number, decFunction, ms = 1000) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            const promiseValue = decFunction(number);
+            console.log(promiseValue);
+            resolve(promiseValue);
+        }, ms);
+    });
+}
+
+function renderMyPromise(number) {
+    promiseDelay(number, plusTen)
+        .then(result => promiseDelay(result, multiplyThree))
+        .then(result => promiseDelay(result, minusTen));
+}
+renderMyPromise(11);
