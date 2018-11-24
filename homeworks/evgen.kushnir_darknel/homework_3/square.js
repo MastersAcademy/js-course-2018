3bf220a19ecc077153c00783d2b90f1d2cf6a366
Darknel <Jenykushnir@gmail.com>
2018-11-25 00:46:23 +0200

    Fix homework_3

--- homeworks/evgen.kushnir_darknel/homework_3/square.js
+++ homeworks/evgen.kushnir_darknel/homework_3/square.js
@@ -6,31 +6,25 @@ console.log(square(5));
 console.log(square(15));
 console.log(square(53));
 
-// Task 1.2 (1)
-function myArray() {
-    const newArr = [];
-    let i = 0;
-    for (; i <= 50; i += 5) {
-        newArr.push(i);
+// Task 1.2
+function myArray(max, step) {
+    const result = [];
+    for (let num = 0; num <= max; num += step) {
+        result.push(num);
     }
-    console.log(newArr);
+    return result;
 }
-myArray();
-// Task 1.2 (2)
-function mySecondArray() {
-    const newArr = [];
-    for (let i = 0; i <= 32; i += 4) {
-        newArr.push(i);
-    }
-    console.log(newArr);
-}
-mySecondArray();
+
+console.log(myArray(50, 5));
+console.log(myArray(32, 4));
+
 // Task 1.3
-const myThirdArray = [0, 4, 8, 12, 16, 20, 24, 28, 32];
-const arrSquare = myThirdArray.map(square);
+
+const mySecondArray = myArray(32, 4);
+const arrSquare = mySecondArray.map(square);
 console.log(arrSquare);
 
 // Task 1.4
-const myFourthArray = [0, 16, 64, 144, 256, 400, 576, 784, 1024];
+const myFourthArray = arrSquare;
 const sum = myFourthArray.reduce((a, b) => a + b, 0);
 console.log(sum);
