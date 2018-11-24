541793b566c3b351648fecf5f705fb5dfd992dcd
miwagav <miwagav123@gmail.com>
2018-11-24 10:36:30 +0200

    1

--- homeworks/Mykhaylo.Gavrylyuk_MiwaGav/Homework_3/Funkciyi.js
+++ homeworks/Mykhaylo.Gavrylyuk_MiwaGav/Homework_3/Funkciyi.js
@@ -1,38 +1,39 @@
-function kvadrat(x, y, z) {
-    console.log(`1) ${x}^2 = ${x * x}`);
-    console.log(`2) ${y}^2 = ${y * y}`);
-    console.log(`3) ${z}^2 = ${z * z}`);
+/* eslint-disable no-console */
+function kvadrat(x) {
+  return x * x;
 }
-kvadrat(5, 15, 53);
+console.log(kvadrat(5));
+console.log(kvadrat(15));
+console.log1(kvadrat(53));
 function masiv(a, b) {
-    const mas = [];
-    for (let i = 0; i <= a; i += b) {
-        mas.push(i);
-    }
-    console.log(mas);
+  const mas = [];
+  for (let i = 0; i <= a; i += b) {
+    mas.push(i);
+  }
+  console.log(mas);
 }
 masiv(50, 5);
 masiv(32, 4);
 function masiv2(a, b) {
-    const mas2 = [];
-    for (let i = 0; i <= a; i += b) {
-        mas2.push(i * i);
-    }
-    console.log(mas2);
+  const mas2 = [];
+  for (let i = 0; i <= a; i += b) {
+    mas2.push(i * i);
+  }
+  console.log(mas2);
 }
 masiv2(32, 4);
 function summas(a, b) {
-    const mas2 = [];
-    for (let i = 0; i <= a; i += b) {
-        mas2.push(i * i);
+  const mas2 = [];
+  for (let i = 0; i <= a; i += b) {
+    mas2.push(i * i);
+  }
+  function massum(massiv) {
+    let sum = 0;
+    for (let j = 0; j < massiv.length; j += 1) {
+      sum += massiv[j];
     }
-    function massum(massiv) {
-        let sum = 0;
-        for (let j = 0; j < massiv.length; j++) {
-            sum += massiv[j];
-        }
-        return (sum);
-    }
-    console.log(massum(mas2));
+    return (sum);
+  }
+  console.log(massum(mas2));
 }
 summas(32, 4);
