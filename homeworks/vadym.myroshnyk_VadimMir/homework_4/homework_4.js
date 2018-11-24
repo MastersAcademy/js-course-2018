165ac675b9aa5264181d5c06fd702d80f1f809ae
VadimMir <Miroshnik_vy@ukr.net>
2018-11-24 15:54:04 +0200

    hm2

---
+++ homeworks/vadym.myroshnyk_VadimMir/homework_4/homework_4.js
@@ -0,0 +1,63 @@
+// callback
+function logNumbers(number) {
+    return number + 10;
+}
+
+function twoStep(numberTwo) {
+}
+
+function threeStep(numberThree) {
+    return twoStep(numberThree) - 20;
+}
+
+setTimeout(()=>{
+    console.log(logNumbers(0));
+}, 1000);
+
+setTimeout(()=>{
+    console.log(twoStep(logNumbers(0)));
+}, 2000);
+
+setTimeout(()=>{
+    console.log(threeStep(10));
+}, 3000);
+
+
+// promise
+const promise = new Promise((resolve, reject) => {
+    setTimeout(()=>{
+        console.log(logNumbers(0));
+    }, 1000);
+
+    setTimeout(()=>{
+        console.log(twoStep(logNumbers(0)));
+    }, 2000);
+
+    setTimeout(()=>{
+        console.log(threeStep(10));
+    }, 3000);
+});
+
+promise
+    .then()
+    .catch(err => console.log('err'));
+
+
+// async await
+async function logNum(num) {
+    await setTimeout(()=>{
+        console.log(logNumbers(0));
+    }, 1000);
+
+    await setTimeout(()=>{
+        console.log(twoStep(logNumbers(0)));
+    }, 2000);
+
+    await setTimeout(()=>{
+        console.log(threeStep(10));
+    }, 3000);
+}
+
+logNum(10)
+    .then()
+    .catch(err => console.log(err));
