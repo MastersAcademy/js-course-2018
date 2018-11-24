e8d88a3fcc31dd2ed27a67bf79bf74ecf04335ed
Revy-Lagoon09 <44705162+Revy-Lagoon09@users.noreply.github.com>
2018-11-25 01:16:40 +0200

    Update square.js

--- homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/square.js
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/square.js
@@ -6,7 +6,7 @@ console.log(square(2));
 console.log(square(15));
 console.log(square(53));
 // 1.2
-function stepArray(step, maxNum) {  
+function stepArray(step, maxNum) {
     const array1 = [];
     for (let i = 0; i <= maxNum; i += step) {
         array1.push(i);
@@ -17,7 +17,7 @@ stepArray(5, 50);
 stepArray(4, 32);
 
 // 1.3
-function squareArray() {   
+function squareArray() {
     const array3 = [];
     const array = [0, 4, 8, 12, 16, 20, 24, 28, 32];
     for (let i = 0; i < array.length; i++) {
