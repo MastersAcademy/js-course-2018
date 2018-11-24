35a0fffe398387a63175c599dba4fed421739c85
IrinaBondarenko <gukibn@gmail.com>
2018-11-24 16:39:51 +0200

    corretion linter mistakes

--- homeworks/irina.vlasova_IrinaBondarenko/homework_4/logNumbers.js
+++ homeworks/irina.vlasova_IrinaBondarenko/homework_4/logNumbers.js
@@ -5,14 +5,14 @@ function foo(a, time, callback) {
     },
     time);
 }
- function LogNumbers1(a) {
+function LogNumbers1(a) {
     foo(a + 10, 1000, (res) => {
-        foo(res * 3, 2000, (res) => {
-            foo(res - 20, 3000, () => {});
+        foo(res * 3, 2000, (res1) => {
+            foo(res1 - 20, 3000, () => {});
         });
     });
 }
- function bar(b, time) {
+function bar(b, time) {
     return new Promise((resolve) => {
         setTimeout(() => {
             console.log(b);
@@ -20,12 +20,12 @@ function foo(a, time, callback) {
         }, time);
     });
 }
- function LogNumbers2(b) {
+function LogNumbers2(b) {
     bar(b + 10, 1000)
         .then(res => bar(res * 3, 2000))
         .then(res => bar(res - 20, 3000));
 }
- async function LogNumbers3(c) {
+async function LogNumbers3(c) {
     let d = 0;
     d = await bar(c + 10, 1000);
     d = await bar(d * 3, 2000);
