a63f602170db144f45678561e196770bec3fac36
gerasvladi <svetgv@ukr.net>
2018-11-25 15:58:04 +0200

    d

---
+++ homeworks/svitlana.herashchenko_gerasvladi/Homework_4/setTimeouts.js
@@ -0,0 +1,65 @@
+function logNumbersPlus(number, callback) {
+    setTimeout(() => {
+        callback(number + 10);
+    }, 1000);
+}
+function logNumbersMultiply(number, callback) {
+    setTimeout(() => {
+        callback(number * 3);
+    }, 2000);
+}
+function logNumbersMinus(number, callback) {
+    setTimeout(() => {
+        callback(number - 20);
+    }, 3000);
+}
+function logNumbers() {
+    logNumbersPlus(10, (plus) => {
+        console.log(plus);
+        logNumbersMultiply(plus, (mult) => {
+            console.log(mult);
+            logNumbersMinus(mult, (minus) => {
+                console.log(minus);
+            });
+        });
+    });
+}
+logNumbers();
+function plusPromise(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(number + 10), 1000);
+    });
+}
+function multiplyPromise(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(number * 3), 2000);
+    });
+}
+function minusPromise(number) {
+    return new Promise((resolve) => {
+        setTimeout(() => resolve(number - 20), 3000);
+    });
+}
+function Promises() {
+    plusPromise(10)
+        .then((plus) => {
+            console.log(plus);
+            return multiplyPromise(plus);
+        })
+        .then((multiply) => {
+            console.log(multiply);
+            return minusPromise(multiply);
+        })
+        .then(minus => console.log(minus));
+}
+Promises();
+let inNumber = 10;
+async function Asyncs() {
+    inNumber = await plusPromise(inNumber);
+    await console.log(inNumber);
+    inNumber = await multiplyPromise(inNumber);
+    await console.log(inNumber);
+    inNumber = await minusPromise(inNumber);
+    await console.log(inNumber);
+}
+Asyncs();
