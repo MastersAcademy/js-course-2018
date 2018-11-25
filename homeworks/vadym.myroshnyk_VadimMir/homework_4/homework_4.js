29796f7851300c0c71267f6594bd56490d4ffc9e
VadimMir <Miroshnik_vy@ukr.net>
2018-11-25 13:42:28 +0200

    1

--- homeworks/vadym.myroshnyk_VadimMir/homework_4/homework_4.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_4/homework_4.js
@@ -1,64 +1,59 @@
+const number = 10;
+const sum = number + 10;
+const mult = sum * 3;
+const minus = mult - 20;
+
 // callback
-function logNumbers(number) {
-    return number + 10;
+function callbackFunction(num, time, callback) {
+    setTimeout(
+        () => {
+            console.log(num);
+            callback();
+        }, time
+    )
 }
 
-function twoStep(numberTwo) {
-    return logNumbers(numberTwo) * 3;
+function logNumber() {
+    callbackFunction(sum, 1000, () => {
+      callbackFunction(mult, 1000, () => {
+          callbackFunction(minus, 1000, () => {})
+      })
+    })
 }
 
-function threeStep(numberThree) {
-    return twoStep(numberThree) - 20;
-}
-
-setTimeout(()=>{
-    console.log(logNumbers(0));
-}, 1000);
-
-setTimeout(()=>{
-    console.log(twoStep(logNumbers(0)));
-}, 2000);
-
-setTimeout(()=>{
-    console.log(threeStep(10));
-}, 3000);
-
+logNumber(number);
 
 // promise
-const promise = new Promise((resolve, reject) => {
-    setTimeout(()=>{
-        console.log(logNumbers(0));
-    }, 1000);
-
-    setTimeout(()=>{
-        console.log(twoStep(logNumbers(0)));
-    }, 2000);
-
-    setTimeout(()=>{
-        console.log(threeStep(10));
-    }, 3000);
-});
-
-promise
-    .then()
-    .catch(err => console.log('err'));
-
-
-// async await
-async function logNum(num) {
-    await setTimeout(()=>{
-        console.log(logNumbers(0));
-    }, 1000);
-
-    await setTimeout(()=>{
-        console.log(twoStep(logNumbers(0)));
-    }, 2000);
-
-    await setTimeout(()=>{
-        console.log(threeStep(10));
-    }, 3000);
+function promiseFunction(num, time) {
+    return new Promise((resolve, reject) => {
+        setTimeout(
+            () => {
+                resolve(num);
+            }, time
+        )
+    })
 }
 
-logNum(10)
-    .then()
-    .catch(err => console.log(err));
+function addAll() {
+    promiseFunction(sum, 1000)
+    .then(() => promiseFunction(mult, 1000))
+    .then(() => promiseFunction(minus, 1000))
+        .then(resalt => {
+            console.log(resalt)
+        })
+}
+
+addAll(number);
+
+// async await
+
+async function addAllnumber() {
+    const sumNumber = await promiseFunction(sum, 1000);
+    console.log(sumNumber);
+    const multNumber = await promiseFunction(mult, 1000);
+    console.log(multNumber);
+    const minusNumber = await promiseFunction(minus, 1000);
+    console.log(minusNumber);
+}
+
+addAllnumber(number);
