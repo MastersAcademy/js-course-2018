6a7288df44d16d3c4d9abeb254c5da1bb0fc7cda
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-25 16:33:40 +0200

    Update steps.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
@@ -4,9 +4,9 @@ const num = 20;
 
 function setTimePromise(number) {
     const promise = new Promise((resolve) => {
-        setTimeout((number) => resolve(console.log(number += 10)), 1000);
-        setTimeout((number) => resolve(console.log(number *= 3)), 1000);
-        setTimeout((number) => resolve(console.log(number -= 20)), 1000);
+        setTimeout(number => resolve(console.log(number += 10)), 1000);
+        setTimeout(number => resolve(console.log(number *= 3)), 1000);
+        setTimeout(number => resolve(console.log(number -= 20)), 1000);
     });
 }
 
@@ -14,9 +14,9 @@ setTimePromise(num);
 // vanila js
 
 function setTime(number) {
-    setTimeout((number) => console.log(number += 10), 1000);
-    setTimeout((number) => console.log(number *= 3), 2000);
-    setTimeout((number) => console.log(number -= 20), 3000);
+    setTimeout(number => console.log(number += 10), 1000);
+    setTimeout(number => console.log(number *= 3), 2000);
+    setTimeout(number => console.log(number -= 20), 3000);
 }
 
 setTime(num);
@@ -26,8 +26,8 @@ setTime(num);
 async function setTimeAsync(number) {
     let promise = new Promise((resolve, reject) => {
         setTimeout((number) => resolve(number += 10), 1000);
-        setTimeout((number) => resolve(console.log(number *= 3)), 2000);
-        setTimeout((number) => resolve(console.log(number -= 20)), 3000);
+        setTimeout((number) => resolve(number *= 3)), 2000);
+        setTimeout((number) => resolve(number -= 20)), 3000);
     });
 
     let res = await promise;
@@ -44,6 +44,6 @@ function setTimeCallback(number, callback) {
     setTimeout((number) => callback(number -= 20), 3000);
 }
 
-setTimeCallback(num, function(num) {
+setTimeCallback(num, (num)=> {
     console.log(num);
 });
