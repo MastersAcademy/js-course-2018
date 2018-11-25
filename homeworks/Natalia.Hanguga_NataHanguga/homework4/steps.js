e4a061a16079bd7b635f4a3054b7207bd22864f7
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-25 19:08:11 +0200

    Update steps.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
@@ -1,16 +1,16 @@
 // promise
+let num = 20;
 
-function setTimePromise() {
-    let number = 20;
+function setTimePromise(number) {
     const promise = new Promise((resolve) => {
         setTimeout(() => (resolve(console.log(number += 10))), 1000);
-        setTimeout(() => (resolve(console.log(number *= 3))), 1000);
-        setTimeout(() => (resolve(console.log(number -= 20))), 1000);
     });
+    promise.then(result=>(setTimeout(() => (console.log(number *= 3)), 1000)))
+    promise.then(result=>(setTimeout(() => (console.log(number -= 20)), 2000)))
 }
 
 setTimePromise(num);
-// vanila js
+
 
 function setTime(number) {
     setTimeout(() => console.log(number += 10), 1000);
@@ -23,26 +23,54 @@ setTime(num);
 // asunc/await function
 
 async function setTimeAsync(number) {
-    let promise = new Promise((resolve, reject) => {
+    let promise1 = new Promise((resolve, reject) => {
         setTimeout(() => resolve(number += 10), 1000);
-        setTimeout(() => resolve(number *= 3), 2000);
-        setTimeout(() => resolve(number -= 20), 3000);
     });
 
-    let res = await promise;
-    console.log(res);
+    let res1 = await promise1;
+    console.log(res1);
+    
+    let promise2 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(number *= 3), 1000);
+    });
+
+    let res2 = await promise2;
+    console.log(res2);
+
+    let promise3 = new Promise((resolve, reject) => {
+        setTimeout(() => resolve(number -= 20), 1000);
+    });
+
+    let res3 = await promise3;
+    console.log(res3);
+
 }
 
 setTimeAsync(num);
 
 // callback function
 
-function setTimeCallback(number, callback) {
+let setTimeCallback1=(number, callback)=> {
     setTimeout(() => callback(number += 10), 1000);
-    setTimeout(() => callback(number *= 3), 2000);
-    setTimeout(() => callback(number -= 20), 3000);
 }
 
-setTimeCallback(num, (num) => {
+setTimeCallback1(num, (num) => {
     console.log(num);
+    let setTimeCallback2=(number, callback)=> {
+        setTimeout(() => callback(number *= 3), 1000);
+    }
+    
+    setTimeCallback2(num, (num) => {
+        console.log(num);
+        let setTimeCallback3=(number, callback)=> {
+            setTimeout(() => callback(number -= 20), 1000);
+        }
+        
+        setTimeCallback3(num, (num) => {
+            console.log(num);
+        });
+    });
 });
+
+
+
