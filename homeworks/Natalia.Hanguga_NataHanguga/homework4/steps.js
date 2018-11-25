94d85ff881a020ff2d4fd521cf947b736d365d8f
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-25 16:04:16 +0200

    Update steps.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
@@ -1,33 +1,33 @@
 const num = 20;
 
-//promise 
+// promise 
 
-function setTimePromise(num) {
-    let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(console.log(num += 10)), 1000);
-        setTimeout(() => resolve(console.log(num *= 3)), 1000);
-        setTimeout(() => resolve(console.log(num -= 20)), 1000);
+function setTimePromise(number) {
+    const promise = new Promise((resolve) => {
+        setTimeout(() => return resolve(console.log(number += 10)), 1000);
+        setTimeout(() => return resolve(console.log(number *= 3)), 1000);
+        setTimeout(() => return resolve(console.log(number -= 20)), 1000);
     });
 }
 
 setTimePromise(num);
-//vanila js
+// vanila js
 
-function setTime(num) {
-    setTimeout(() => console.log(num += 10), 1000);
-    setTimeout(() => console.log(num *= 3), 2000);
-    setTimeout(() => console.log(num -= 20), 3000);
+function setTime(number) {
+    setTimeout(() => console.log(number += 10), 1000);
+    setTimeout(() => console.log(number *= 3), 2000);
+    setTimeout(() => console.log(number -= 20), 3000);
 }
 
 setTime(num);
 
-//asunc/await function
+// asunc/await function
 
-async function setTimeAsync(num) {
+async function setTimeAsync(number) {
     let promise = new Promise((resolve, reject) => {
-        setTimeout(() => resolve(num += 10), 1000);
-        setTimeout(() => resolve(console.log(num *= 3)), 2000);
-        setTimeout(() => resolve(console.log(num -= 20)), 3000);
+        setTimeout(() => resolve(number += 10), 1000);
+        setTimeout(() => resolve(console.log(number *= 3)), 2000);
+        setTimeout(() => resolve(console.log(number -= 20)), 3000);
     });
 
     let res = await promise;
@@ -36,12 +36,12 @@ async function setTimeAsync(num) {
 
 setTimeAsync(num);
 
-//callback function
+// callback function
 
-function setTimeCallback(num, callback) {
-    setTimeout(() => callback(num += 10), 1000);
-    setTimeout(() => callback(num *= 3), 2000);
-    setTimeout(() => callback(num -= 20), 3000);
+function setTimeCallback(number, callback) {
+    setTimeout(() => callback(number += 10), 1000);
+    setTimeout(() => callback(number *= 3), 2000);
+    setTimeout(() => callback(number -= 20), 3000);
 }
 
 setTimeCallback(num, function(num) {
