7682004a01dba36525d8e3e7acb4d98a29e3be2f
RPVanarx <roman.plynko@gmail.com>
2018-11-25 21:54:29 +0200

    edit

--- homeworks/roman.plynko_RPVanarx/homework_4/task2.js
+++ homeworks/roman.plynko_RPVanarx/homework_4/task2.js
@@ -1,5 +1,5 @@
 const path = 'https://kwork.ru/pics/t3/85/427668-1534523285.jpg';
-function loadImage(file, url, progressCallback) {
+function loadImage(url, progressCallback) {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
 
@@ -9,31 +9,28 @@ function loadImage(file, url, progressCallback) {
             console.log('upload begin');
         };
 
-        request.upload.onprogress = event => progressCallback(event);
+        request.upload.onprogress = progressCallback;
 
         request.onload = () => {
-            if (request.status === 200) {
-                console.log('end upload');
-                resolve(request.response);
-            } else {
-                reject(request.statusText);
-            }
+            console.log('end upload');
+            resolve(request.statusText);
         };
 
-        request.send(file);
+        request.onerror = () => {
+            console.log('error');
+            reject(request.statusText);
+        };
+
+        request.send();
     });
 }
 
 function progress(event) {
-    return new Promise((resolve, reject) => {
-        if (event.lengthComputable) {
-            console.log(`${event.loadedDate / event.totalDate * 100}%`);
-            resolve();
-        } else {
-            console.log('100%');
-            reject();
-        }
-    });
+    if (event.lengthComputable) {
+        console.log(`${event.loadedDate / event.totalDate * 100}%`);
+    } else {
+        console.log('100%');
+    }
 }
 
-loadImage({}, path, progress).then().catch(err => console.log(err));
+loadImage(path, progress).then().catch(err => console.log(err));
