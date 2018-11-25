fd9bbdcf6ca82ec4e0f1767a8e60da2a46743487
Nata <38878737+NataHanguga@users.noreply.github.com>
2018-11-25 16:48:41 +0200

    Update steps.js

--- homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
+++ homeworks/Natalia.Hanguga_NataHanguga/homework4/steps.js
@@ -2,8 +2,8 @@ const num = 20;
 
 // promise
 
-function setTimePromise(num) {
-    let num = number.prom;
+function setTimePromise(num1) {
+    let num1 = number.prom;
     const promise = new Promise((resolve) => {
         setTimeout(number => resolve(console.log(number.prom += 10)), 1000);
         setTimeout(number => resolve(console.log(number.prom *= 3)), 1000);
