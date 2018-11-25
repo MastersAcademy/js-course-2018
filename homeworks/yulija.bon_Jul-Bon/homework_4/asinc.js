69b654afda6fe504a006f8b4ce386a105f11f3d1
Jul Bon <yulija.bon007@gmail.com>
2018-11-25 12:52:44 +0200

    homework 4 refresh

--- homeworks/yulija.bon_Jul-Bon/homework_4/asinc.js
+++ homeworks/yulija.bon_Jul-Bon/homework_4/asinc.js
@@ -1,21 +1,21 @@
 /* callback */
 
-// function logNumbers(num) {
-//     return setTimeout(() => {
-//         const x = num + 10;
-//         console.log(x);
-//         return setTimeout(() => {
-//             const y = x * 3;
-//             console.log(y);
-//             return setTimeout(() => {
-//                 const z = y - 20;
-//                 console.log(z);
-//             }, 3000);
-//         }, 2000);
-//     }, 1000);
-// }
-//
-// logNumbers(100);
+function logNumbers(num) {
+    return setTimeout(() => {
+        const x = num + 10;
+        console.log(x);
+        return setTimeout(() => {
+            const y = x * 3;
+            console.log(y);
+            return setTimeout(() => {
+                const z = y - 20;
+                console.log(z);
+            }, 3000);
+        }, 2000);
+    }, 1000);
+}
+
+logNumbers(100);
 
 /* Promise */
 
@@ -29,20 +29,20 @@ function logNumbersPromice(num) {
 logNumbersPromice(10);
 
 /* Asinc Function */
-//
-// async function logNumbersAsinc(num) {
-//     let x = num;
-//     await setTimeout(() => {
-//         console.log(x += 10);
-//     }, 1000);
-//
-//     await setTimeout(() => {
-//         console.log(x *= 3);
-//     }, 2000);
-//
-//     await setTimeout(() => {
-//         console.log(x -= 20);
-//     }, 3000);
-// }
-//
-// logNumbersAsinc(10);
+
+async function logNumbersAsinc(num) {
+    let x = num;
+    await setTimeout(() => {
+        console.log(x += 10);
+    }, 1000);
+
+    await setTimeout(() => {
+        console.log(x *= 3);
+    }, 2000);
+
+    await setTimeout(() => {
+        console.log(x -= 20);
+    }, 3000);
+}
+
+logNumbersAsinc(10);
