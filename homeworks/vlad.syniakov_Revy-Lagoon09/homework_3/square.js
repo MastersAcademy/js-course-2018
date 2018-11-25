8062915155d30218788f7a5d7531a21f2373424b
Revy-Lagoon09 <44705162+Revy-Lagoon09@users.noreply.github.com>
2018-11-25 15:51:14 +0200

    Update square.js
    зробив return в попередній функції, все запахало наче)

--- homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/square.js
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/square.js
@@ -5,6 +5,7 @@ function square(num) {
 console.log(square(2));
 console.log(square(15));
 console.log(square(53));
+
 // 1.2
 function stepArray(step, maxNum) {
     const array1 = [];
@@ -12,23 +13,18 @@ function stepArray(step, maxNum) {
         array1.push(i);
     }
     console.log(array1);
+    return array1;
 }
 stepArray(5, 50);
 stepArray(4, 32);
 
 // 1.3
-function squareArray() {
-    const array3 = [];
-    const array = [0, 4, 8, 12, 16, 20, 24, 28, 32];
-    for (let i = 0; i < array.length; i++) {
-        (array3.push(array[i] ** 2));
-    }
-    return array3;
-}
-console.log(squareArray());
+    const newArr = stepArray(4,32);
+    const squareArr = newArr.map(square);
+    console.log(squareArr);
 // 1.4
 function sumArray3() {
-    const arr = squareArray();
+    const arr = squareArr;
     const sum = arr.reduce((result, num) => result + num, 0);
     return sum;
 }
