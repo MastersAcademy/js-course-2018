d290a7c5a63842a9f9dcefc271228720df31d3bb
Jul Bon <yulija.bon007@gmail.com>
2018-11-25 14:47:05 +0200

    homew-4

--- homeworks/yulija.bon_Jul-Bon/homework_2/task-2.js
+++
@@ -1,8 +0,0 @@
-let number = 500;
-while (number < 1000) {
-    number++;
-    const numberToString = number.toString();
-    if (numberToString.charAt(0) === numberToString.charAt(2)) {
-        console.log(numberToString);
-    }
-}
