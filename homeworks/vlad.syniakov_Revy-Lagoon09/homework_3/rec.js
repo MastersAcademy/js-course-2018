e4de95fc08f073d310b636201479dea0fb64bfc0
Revy-Lagoon09 <44705162+Revy-Lagoon09@users.noreply.github.com>
2018-11-25 15:58:38 +0200

    Update rec.js

--- homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/rec.js
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_3/rec.js
@@ -1,10 +1,9 @@
 // rec
-function rec(num) {   
-     return num % 2 === 0 ? rec(num / 2) : num === 1;
+function rec(num) { 
+    return num % 2 === 0 ? rec(num / 2) : num === 1;
 }
 for (let i = 10; i <= 600; i++) {
     if (rec(i)) {
         console.log(i);
     }
 }
-
