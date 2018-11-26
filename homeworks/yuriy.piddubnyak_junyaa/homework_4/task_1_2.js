99f11fd29def72133d7f5d93109cfc88767410a6
junyaa <internet_ck@ukr.net>
2018-11-26 13:30:17 +0200

    new fix

--- homeworks/yuriy.piddubnyak_junyaa/homework_4/task_1_2.js
+++ homeworks/yuriy.piddubnyak_junyaa/homework_4/task_1_2.js
@@ -1,22 +1,26 @@
 const promise = new Promise((resolve) => {
     resolve(100);
 });
-promise.then((value) => {
-    setTimeout(() => {
-    }, 0);
+
+function timer(value) {
     return value + 10;
-}).then((value) => {
+}
+function timer2(value) {
     setTimeout(() => {
         console.log(value);
     }, 1000);
     return value * 3;
-}).then((value) => {
+}
+function timer3(value) {
     setTimeout(() => {
         console.log(value);
     }, 2000);
     return value - 20;
-}).then((value) => {
+}
+function timer4(value) {
     setTimeout(() => {
         console.log(value);
     }, 3000);
-});
+}
+
+promise.then(v => timer(v)).then(v => timer2(v)).then(v => timer3(v)).then(v => timer4(v));
