1f77accd744c3711a8b6815a0b22113492f82e9c
Olexandr <olexandr.reshetnyk@gmail.com>
2018-11-28 16:06:12 +0200

    fixes requested by TarasTurchenko

--- homeworks/olexandr.reshetnyk_reshetnyk/homework_4/task4.js
+++ homeworks/olexandr.reshetnyk_reshetnyk/homework_4/task4.js
@@ -15,54 +15,38 @@ function logNumbers1(num) {
     }, 1000);
 }
 
-function logNumbers2(num) {
-    const promise = new Promise(((resolve) => {
+function getValue(num, calcFunc) {
+    return new Promise(((resolve) => {
         setTimeout(() => {
-            const value1 = num + 10;
-            console.log(`[logNumber2] ${value1}`);
-            resolve(value1);
+            const value = calcFunc(num);
+            console.log(`${value}`);
+            resolve(value);
         }, 1000);
     }));
+}
 
-    promise
-        .then(value1 => new Promise(((resolve) => {
-            setTimeout(() => {
-                const value2 = value1 * 3;
-                console.log(`[logNumber2] ${value2}`);
-                resolve(value2);
-            }, 1000);
-        })))
-        .then(value2 => new Promise(((resolve) => {
-            setTimeout(() => {
-                const value3 = value2 - 20;
-                console.log(`[logNumber2] ${value3}`);
-                resolve(value3);
-            }, 1000);
-        })));
+function calcFirstValue(n) {
+    return n + 10;
+}
+
+function calcSecondValue(n) {
+    return n * 3;
+}
+
+function calcThirdValue(n) {
+    return n - 20;
+}
+
+function logNumbers2(num) {
+    getValue(num, calcFirstValue)
+        .then(value1 => getValue(value1, calcSecondValue))
+        .then(value2 => getValue(value2, calcThirdValue));
 }
 
 async function logNumbers3(num) {
-    const result1 = await new Promise(((resolve) => {
-        const value1 = num + 10;
-        setTimeout(() => {
-            console.log(`[logNumber3] ${value1}`);
-            resolve(value1);
-        }, 1000);
-    }));
-    const result2 = await new Promise(((resolve) => {
-        const value2 = result1 * 3;
-        setTimeout(() => {
-            console.log(`[logNumber3] ${value2}`);
-            resolve(value2);
-        }, 1000);
-    }));
-    await new Promise(((resolve) => {
-        const value3 = result2 - 20;
-        setTimeout(() => {
-            console.log(`[logNumber3] ${value3}`);
-            resolve(value3);
-        }, 1000);
-    }));
+    const result1 = await getValue(num, calcFirstValue);
+    const result2 = await getValue(result1, calcSecondValue);
+    await getValue(result2, calcThirdValue);
 }
 
 logNumbers1(input);
