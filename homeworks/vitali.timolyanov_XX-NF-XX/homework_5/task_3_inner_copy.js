5e1e4c5ec7f07a1854d2f1771a744e435751d0e3
Vitali Timolyanov <nf.ebox@gmail.com>
2018-11-29 04:36:53 +0200

    Add inner copy taks #3

---
+++ homeworks/vitali.timolyanov_XX-NF-XX/homework_5/task_3_inner_copy.js
@@ -0,0 +1,19 @@
+function innerCopy(object, depth = 1) {
+    const copy = Object.assign({}, object);
+    if (depth > 0) {
+        const objectEntries = Object.entries(copy).filter(entry => typeof entry[1] === 'object');
+        objectEntries.forEach((entry) => {
+            copy[entry[0]] = innerCopy(entry[1], depth - 1);
+        });
+    }
+    return copy;
+}
+
+const obj = { b: 'c', d: { e: 'f' } };
+const objCopy = innerCopy(obj);
+
+console.log('obj.d:');
+console.log(obj.d);
+console.log('objCopy.d:');
+console.log(objCopy.d);
+console.log(`obj.d === objCopy.d: ${obj.d === objCopy.d}`);
