7dd1ea403de4518ae63fba91d3464db33d99ca40
VadimMir <Miroshnik_vy@ukr.net>
2018-11-29 23:07:17 +0200

    up1

--- homeworks/vadym.myroshnyk_VadimMir/homework_5/homework_5.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_5/homework_5.js
@@ -1,9 +1,9 @@
-//tesk 1
-const someArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+// tesk 1
+const someArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function sumArrayFunction () {
-    let arr = [];
-    for (let i in someArr) {
+function sumArrayFunction() {
+    const arr = [];
+    for (const i in someArr) {
         arr[i] = someArr[i];
         const sumArray = arr.reduce((a,b) => a + b, 0);
         console.log(sumArray);
@@ -12,14 +12,14 @@ function sumArrayFunction () {
 
 sumArrayFunction();
 
-//tesk 2
+// tesk 2
 const classes = ['header', 'menu', 'menu_item', 'tabs',
     'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu',
     'menu_item', 'menu', 'menu_item'];
 
 function returnObjKey(obj) {
-    let result = {};
-    for (let i in classes) {
+    const result = {};
+    for (const i in classes) {
         let sum = 0;
             sum = classes.filter(item => item === classes[i]).length;
         result[classes[i]] = sum ;
@@ -29,39 +29,38 @@ function returnObjKey(obj) {
 
 returnObjKey();
 
-//tesk 3
+// tesk 3
 const  obj = {b: "c",
-             d: {
-               e:"f"},
-             };
+    d: {
+        e:"f"},
+};
 
-function innerCopy() {
+function innerCopy(a) {
     return a = Object.assign({}, obj);
 }
 const objCopy = innerCopy();
 console.log(objCopy);
 console.log(objCopy.d);
 
-//tesk 4
+// tesk 4
 const people = [
-    {id: 1, name: 'Nick', friends: [2,5,6]},
-    {id: 2, name: 'John', friends: [1, 3]},
-    {id: 3, name: 'Mike', friends: [2, 5]},
-    {id: 4, name: 'Janny', friends: null},
-    {id: 5, name: 'Edward', friends: [1, 3]},
-    {id: 6, name: 'Jeen', friends: [5, 1]}
+    { id: 1, name: 'Nick', friends: [2,5,6] },
+    { id: 2, name: 'John', friends: [1, 3] },
+    { id: 3, name: 'Mike', friends: [2, 5] },
+    { id: 4, name: 'Janny', friends: null },
+    { id: 5, name: 'Edward', friends: [1, 3] },
+    { id: 6, name: 'Jeen', friends: [5, 1] },
 ];
 
 const getPeople = function(userId) {
-   for (let i in people) {
-       if(people[i].id === userId) {
-           for (let j in people[i].friends) {
-               let number = people[i].friends[j];
-               console.log(people[number - 1]);
-           }
-       }
-   }
-
+   for (const i in people) {
+        if(people[i].id === userId) {
+            for (const j in people[i].friends) {
+                const number = people[i].friends[j];
+                console.log(people[number - 1]);
+            }
+        }
+    }
 };
 
 getPeople(2);
