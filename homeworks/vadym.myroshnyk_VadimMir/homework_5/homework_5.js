80b6b45c22825501da5a4d41bd3b10743a14b6da
VadimMir <Miroshnik_vy@ukr.net>
2018-11-29 22:54:03 +0200

    hw_5

---
+++ homeworks/vadym.myroshnyk_VadimMir/homework_5/homework_5.js
@@ -0,0 +1,67 @@
+//tesk 1
+const someArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
+
+function sumArrayFunction () {
+    let arr = [];
+    for (let i in someArr) {
+        arr[i] = someArr[i];
+        const sumArray = arr.reduce((a,b) => a + b, 0);
+        console.log(sumArray);
+    }
+}
+
+sumArrayFunction();
+
+//tesk 2
+const classes = ['header', 'menu', 'menu_item', 'tabs',
+    'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu',
+    'menu_item', 'menu', 'menu_item'];
+
+function returnObjKey(obj) {
+    let result = {};
+    for (let i in classes) {
+        let sum = 0;
+            sum = classes.filter(item => item === classes[i]).length;
+        result[classes[i]] = sum ;
+    }
+    console.log (result);
+}
+
+returnObjKey();
+
+//tesk 3
+const  obj = {b: "c",
+             d: {
+               e:"f"},
+             };
+
+function innerCopy() {
+    return a = Object.assign({}, obj);
+}
+const objCopy = innerCopy();
+console.log(objCopy);
+console.log(objCopy.d);
+
+//tesk 4
+const people = [
+    {id: 1, name: 'Nick', friends: [2,5,6]},
+    {id: 2, name: 'John', friends: [1, 3]},
+    {id: 3, name: 'Mike', friends: [2, 5]},
+    {id: 4, name: 'Janny', friends: null},
+    {id: 5, name: 'Edward', friends: [1, 3]},
+    {id: 6, name: 'Jeen', friends: [5, 1]}
+];
+
+const getPeople = function(userId) {
+   for (let i in people) {
+       if(people[i].id === userId) {
+           for (let j in people[i].friends) {
+               let number = people[i].friends[j];
+               console.log(people[number - 1]);
+           }
+       }
+   }
+
+};
+
+getPeople(2);
