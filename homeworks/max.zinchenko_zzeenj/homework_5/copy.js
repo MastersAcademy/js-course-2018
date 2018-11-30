8d29b696cb59ffa394ea5d245f0357892da39550
zzeenj <zzeenj@gmailcom>
2018-11-30 15:30:59 +0200

    fixed

--- homeworks/max.zinchenko_zzeenj/homework_5/copy.js
+++ homeworks/max.zinchenko_zzeenj/homework_5/copy.js
@@ -1,13 +1,14 @@
 const Obj = { b: 'c', d: { e: 'f' } };
 function copy(a) {
-    return Object.assign({}, a);
-}
-function copy2(a) {
-    const Copy = {};
+    const objectCopy = {};
     Object.keys(a).forEach((key) => {
-        Copy.d = a[key];
+        if (typeof a[key] === 'object') {
+            objectCopy[key] = copy(a[key]);
+        } else {
+            objectCopy[key] = a[key];
+        }
     });
-    return Copy;
+    return objectCopy;
 }
 console.log(copy(Obj));
-console.log(copy2(Obj));
+console.log(copy(Obj.d));
