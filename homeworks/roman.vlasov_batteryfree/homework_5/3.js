183d22340d3e204138ff813f1ce29e3f86d100ac
batteryfree <44664353+batteryfree@users.noreply.github.com>
2018-11-30 21:15:55 +0200

    function innerCopy was changed.

--- homeworks/roman.vlasov_batteryfree/homework_5/3.js
+++ homeworks/roman.vlasov_batteryfree/homework_5/3.js
@@ -3,8 +3,21 @@ const originalObj = { b: 'c', d: { e: 'f' } };
 function innerCopy(obj) {
     const keys = Object.keys(obj);
     const newObj = {};
-    keys.forEach((key) => { newObj[key] = obj[key]; });
+    keys.forEach((key) => { 
+        if(typeof obj[key] === 'object') {
+            newObj[key] = innerCopy(obj[key]);
+        } else {
+        newObj[key] = obj[key];}
+     });
     return newObj;
 }
+const newObject = innerCopy(originalObj);
 
-console.log(innerCopy(originalObj));
+console.log(originalObj);
+console.log(newObject);
+newObject.b = 7;
+newObject.d.e = 5;
+console.log
+console.log('***checking after changes***');
+console.log(originalObj);
+console.log(newObject);
