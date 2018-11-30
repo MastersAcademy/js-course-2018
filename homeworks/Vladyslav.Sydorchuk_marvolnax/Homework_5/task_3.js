8f3981a18ddf0f2b560d0f161151b62e9d72a4ba
Marvolnax <marvolnax@gmail.com>
2018-11-30 16:58:57 +0200

    Homework_5

---
+++ homeworks/Vladyslav.Sydorchuk_marvolnax/Homework_5/task_3.js
@@ -0,0 +1,13 @@
+// task_3
+
+function innerCopy(obj) {
+    const newObj = {};
+    Object.keys(obj).forEach((key) => { newObj[key] = obj[key]; });
+    return newObj;
+}
+
+    const someObj = { b: 'c', d: { e: { s: 'f' } } };
+    const objCopy = innerCopy(someObj);
+
+console.log(objCopy);
+console.log(objCopy.d.e);
