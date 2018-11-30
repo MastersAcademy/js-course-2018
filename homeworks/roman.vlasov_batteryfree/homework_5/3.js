81ca97b04a1d6dc39030640ab729132987114200
batteryfree <44664353+batteryfree@users.noreply.github.com>
2018-11-30 21:21:53 +0200

    Fix

--- homeworks/roman.vlasov_batteryfree/homework_5/3.js
+++ homeworks/roman.vlasov_batteryfree/homework_5/3.js
@@ -3,11 +3,11 @@ const originalObj = { b: 'c', d: { e: 'f' } };
 function innerCopy(obj) {
     const keys = Object.keys(obj);
     const newObj = {};
-    keys.forEach((key) => { 
-        if(typeof obj[key] === 'object') {
+    keys.forEach((key) => {
+        if (typeof obj[key] === 'object') {
             newObj[key] = innerCopy(obj[key]);
         } else {
-        newObj[key] = obj[key];}
+            newObj[key] = obj[key]; }
      });
     return newObj;
 }
@@ -17,7 +17,6 @@ console.log(originalObj);
 console.log(newObject);
 newObject.b = 7;
 newObject.d.e = 5;
-console.log
 console.log('***checking after changes***');
 console.log(originalObj);
 console.log(newObject);
