47b48aed39635035d37caffc62bdd1a755adb822
Aleksey <kymotocycle@gmail.com>
2018-11-30 13:02:45 +0200

    git commit -m 'Closures (+ reduce), start with second task'

---
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_5/closures.js
@@ -0,0 +1,43 @@
+
+// Declerate examples
+
+const exampleOne = [10, -10, 10, -10, 10];
+const exampleTwo = [0, 0, 0, 0, 0];
+const exampleThree = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+
+function plusNumbers(number) {
+    let summ = number;
+    return (integer) => {
+        summ += integer;
+        return summ;
+    };
+}
+
+function showNumbers(inputArray) {
+    const callPlus = plusNumbers(0);
+    console.log(inputArray.map(callPlus));
+}
+
+showNumbers(exampleOne);
+showNumbers(exampleTwo);
+showNumbers(exampleThree);
+
+
+// Reduce
+console.log('Just fun with reduce');
+
+function getSum(arr) {
+    const result = [];
+
+    const totalSum = arr.reduce((sum, item) => {
+        result.push(sum);
+        return sum + item;
+    });
+    result.push(totalSum);
+    console.log(result);
+}
+
+getSum(exampleOne);
+getSum(exampleTwo);
+getSum(exampleThree);
---
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_5/objkeys.js
@@ -0,0 +1,10 @@
+const classes = ['header', 'menu', 'menu_item', 'tabs', 'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu', 'menu_item', 'menu', 'menu_item'];
+
+classes.forEach((element) => {
+    const objWithKeys = { };
+
+
+    console.log(element);
+
+    return objWithKeys;
+});
