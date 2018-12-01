14275e8721018a4999a537efe0ed6f7553bbac6a
Revy-Lagoon09 <44705162+Revy-Lagoon09@users.noreply.github.com>
2018-12-02 01:27:06 +0200

    Update 3.js

--- homeworks/vlad.syniakov_Revy-Lagoon09/homework_5/3.js
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_5/3.js
@@ -1,13 +1,10 @@
-const Obj = {
-    b: 'c',
-    d: { e: 'f' },
-};
+const Obj = {b: 'c', d: { e: 'f' }}
 function innerCopy(mainObj) {
     const copyObj = {};
-    for (const key in mainObj) {
-        if (copyObj[key] = mainObj[key]);
-        return copyObj;
+    for ( const key in mainObj) {
+        copyObj[key] = mainObj[key];
     }
+    return copyObj;
 }
 console.log(innerCopy(Obj));
 console.log(innerCopy(Obj.d));
