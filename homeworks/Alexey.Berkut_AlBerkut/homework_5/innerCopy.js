946ec950b890afaf1004079a4288d5b7f30ed019
AlBerkut <44679993+AlBerkut@users.noreply.github.com>
2018-12-02 11:23:36 +0200

    Update innerCopy.js

--- homeworks/Alexey.Berkut_AlBerkut/homework_5/innerCopy.js
+++ homeworks/Alexey.Berkut_AlBerkut/homework_5/innerCopy.js
@@ -1,8 +1,23 @@
-const obj = { b: 'c', d: { e: 'f' } };
-
+/* Small copy. Nested object passed reference;
 function innerCopy(input) {
     const objCopy = Object.assign({}, input);
     return objCopy;
 }
 
-console.log(innerCopy(obj));
+console.log(innerCopy(obj)); */
+
+function interCopy(input) {
+    const result = {}; // new empty object;
+    Object.keys(input).forEach((key) => {
+        if (typeof (input[key]) === 'object') {
+            interCopy(input[key]);
+        }
+        result[key] = input[key];
+    });
+    return result;
+}
+
+const obj = { b: 'c', d: { e: 'f' } };
+const objCopy = interCopy(obj);
+
+console.log(objCopy);
