03654268e2eaaae9c70fd599e5d42f94485b42c8
junyaa <internet_ck@ukr.net>
2018-12-02 15:09:10 +0200

    fix

--- homeworks/yuriy.piddubnyak_junyaa/homework_5/task_2.js
+++ homeworks/yuriy.piddubnyak_junyaa/homework_5/task_2.js
@@ -1,12 +1,13 @@
-const arr = ['header', 'menu', 'menu_item', 'tabs',
+const array = ['header', 'menu', 'menu_item', 'tabs',
     'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu',
     'menu_item', 'menu', 'menu_item'];
-const result = {};
-arr.forEach((a) => {
-    result[a] = result[a] + 1 || 1;
-});
-for (const key in result) {
-    if ({}.hasOwnProperty.call(result, key)) {
-        console.log(key + result[key]);
-    }
+function objCreator(arr) {
+    const result = {};
+    arr.forEach((a) => {
+        result[a] = result[a] + 1 || 1;
+    });
+
+    return result;
 }
+
+console.log(objCreator(array));
