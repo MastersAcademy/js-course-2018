1087ffc68b7477b3029d5938af9638b08ae11bde
Nataliya Kokovina <kokovina.nat>
2018-12-02 18:06:51 +0200

    add fiex task2 and task3

--- homeworks/natalia.kokovina_NataliKokovina/homework_5/task2.js
+++ homeworks/natalia.kokovina_NataliKokovina/homework_5/task2.js
@@ -3,16 +3,13 @@ const classes = ['header', 'menu', 'menu_item', 'tabs',
     'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu', 'menu_item', 'menu', 'menu_item'];
 
 function toObject(arr) {
-    const obj = {};
-    arr.forEach((propertyName) => {
-        if (propertyName in obj) {
-            obj[propertyName]++;
-            return obj;
-        }
-        obj[propertyName] = 1;
-
+    const newObj = {};
+    arr.reduce((obj, propertyName) => {
+        if (propertyName in newObj) {
+            return newObj[propertyName]++;
+        } newObj[propertyName] = 1;
         return obj;
-    });
-    console.log(obj);
+    }, {});
+    console.log(newObj);
 }
 toObject(classes);
--- homeworks/natalia.kokovina_NataliKokovina/homework_5/task3.js
+++ homeworks/natalia.kokovina_NataliKokovina/homework_5/task3.js
@@ -4,7 +4,11 @@ const obj = { b: 'c', d: { e: 'f' } };
 function innerCopy(someObj) {
     const objCopy = {};
     Object.keys(someObj).forEach((key) => {
-        objCopy[key] = someObj[key];
+        if (typeof objCopy[key] === 'object') {
+            objCopy[key] = innerCopy(someObj[key]);
+        } else {
+            objCopy[key] = someObj[key];
+        }
     });
     return objCopy;
 }
@@ -13,3 +17,8 @@ const copyObj = innerCopy(obj);
 
 console.log(copyObj);
 console.log(copyObj.d);
+
+// copyObj and obj are not dependent on each other, because:
+// obj.b = 'a';
+// console.log(copyObj.b); // c
+// console.log(obj.b); // a
