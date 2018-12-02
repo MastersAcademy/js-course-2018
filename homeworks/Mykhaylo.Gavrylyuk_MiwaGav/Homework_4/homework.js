80ef31c9aeb05abb942c2fda5c630e4d162e80ef
MiwaGav <44661366+MiwaGav@users.noreply.github.com>
2018-12-02 12:06:30 +0200

    Update homework.js

--- homeworks/Mykhaylo.Gavrylyuk_MiwaGav/Homework_4/homework.js
+++ homeworks/Mykhaylo.Gavrylyuk_MiwaGav/Homework_4/homework.js
@@ -1,30 +1,21 @@
-/* eslint-disable no-console */
 function logNumbersPlus(number, callback) {
-    setTimeout(() => {
-        callback(number + 10);
-    }, 1000);
+    setTimeout(() => { callback(number + 10); }, 1000);
 }
 function logNumbersMultiply(number, callback) {
-    setTimeout(() => {
-        callback(number * 3);
-    }, 2000);
+    setTimeout(() => { callback(number * 3); }, 2000);
 }
 function logNumbersMinus(number, callback) {
-    setTimeout(() => {
-        callback(number - 20);
-    }, 3000);
+    setTimeout(() => { callback(number - 20); }, 3000);
 }
-function logNumbers() {
-    logNumbersPlus(10, (plus) => {
-        console.log(plus);
-        logNumbersMultiply(plus, (mult) => {
-            console.log(mult);
-            logNumbersMinus(mult, (minus) => {
-                console.log(minus);
-            });
+logNumbersPlus(10, (plus) => {
+    console.log(plus);
+    logNumbersMultiply(plus, (mult) => {
+        console.log(mult);
+        logNumbersMinus(mult, (minus) => {
+            console.log(minus);
         });
     });
-}
+});
 logNumbers();
 function plusPromise(number) {
     return new Promise((resolve) => {
@@ -41,19 +32,16 @@ function minusPromise(number) {
         setTimeout(() => resolve(number - 20), 3000);
     });
 }
-function Promises() {
-    plusPromise(10)
-        .then((plus) => {
-            console.log(plus);
-            return multiplyPromise(plus);
-        })
-        .then((multiply) => {
-            console.log(multiply);
-            return minusPromise(multiply);
-        })
-        .then(minus => console.log(minus));
-}
-Promises();
+plusPromise(10)
+    .then((plus) => {
+        console.log(plus);
+        return multiplyPromise(plus);
+    })
+    .then((multiply) => {
+        console.log(multiply);
+        return minusPromise(multiply);
+    })
+    .then(minus => console.log(minus));
 let inNumber = 10;
 async function Asyncs() {
     inNumber = await plusPromise(inNumber);
