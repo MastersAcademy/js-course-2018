43eee49739228045d77d87626ba24efac8ae6d1f
junyaa <internet_ck@ukr.net>
2018-12-02 19:56:14 +0200

    update

--- homeworks/yuriy.piddubnyak_junyaa/homework_4/task_1_3.js
+++ homeworks/yuriy.piddubnyak_junyaa/homework_4/task_1_3.js
@@ -1,27 +1,22 @@
-const number = 100;
 const promise = new Promise((resolve) => {
-    resolve(number);
+    resolve(100);
 });
-async function func() {
-    await promise;
-    promise.then((value) => {
-        setTimeout(() => {
-        }, 0);
-        return value + 10;
-    }).then((value) => {
-        setTimeout(() => {
-            console.log(value);
-        }, 1000);
-        return value * 3;
-    }).then((value) => {
-        setTimeout(() => {
-            console.log(value);
-        }, 2000);
-        return value - 20;
-    }).then((value) => {
-        setTimeout(() => {
-            console.log(value);
-        }, 3000);
-    });
-}
-func();
+promise.then((value) => {
+    setTimeout(() => {
+    }, 0);
+    return value + 10;
+}).then((value) => {
+    setTimeout(() => {
+        console.log(value);
+    }, 1000);
+    return value * 3;
+}).then((value) => {
+    setTimeout(() => {
+        console.log(value);
+    }, 2000);
+    return value - 20;
+}).then((value) => {
+    setTimeout(() => {
+        console.log(value);
+    }, 3000);
+});
