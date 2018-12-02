fc3f6eec734b04bab21c0d94307d9bd70ec2b23f
Revy-Lagoon09 <44705162+Revy-Lagoon09@users.noreply.github.com>
2018-12-02 13:23:12 +0200

    Update 1.js

--- homeworks/vlad.syniakov_Revy-Lagoon09/homework_5/1.js
+++ homeworks/vlad.syniakov_Revy-Lagoon09/homework_5/1.js
@@ -1,11 +1,11 @@
-function Summarizer(value) {
+function heySummarizer(value) {
     let summ = value;
     return (num) => {
         summ += num;
         return summ;
     };
 }
-const summarizer = Summarizer(0);
+const summarizer = heySummarizer(0);
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 const sumArr = arr.map(summarizer);
 console.log((sumArr));
