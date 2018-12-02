ccea53ca331c94472d7994cd04aab8a81ca2a06a
Aleksey <kymotocycle@gmail.com>
2018-12-02 11:36:39 +0200

    variable for newArray, some check for old and new array

--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_5/copy.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_5/copy.js
@@ -1,10 +1,24 @@
-const oldArray = [{ b: 'c', d: { e: 'f' } }];
 
-function newArray(arr) {
+const oldArray = { b: 'c', d: { e: 'f' } };
+
+const newArray = (arr) => {
     const newObject = {};
     Object.keys(arr).forEach((propertyKey) => {
-        newObject[propertyKey] = arr[propertyKey];
+        if (typeof (arr[propertyKey]) === 'object') {
+            newObject[propertyKey] = newArray(arr[propertyKey]);
+        } else {
+            newObject[propertyKey] = arr[propertyKey];
+        }
     });
     return newObject;
-}
-console.log(newArray(oldArray));
+};
+
+const copy = newArray(oldArray);
+
+console.log(copy);
+
+console.log(oldArray);
+
+oldArray.b = 123;
+console.log(oldArray);
+console.log(copy);
