eed52ca34be002fb11dba312b1e624ed5e040800
Iryna Gychka <irishchest@gmail.com>
2018-12-02 14:58:18 +0200

    Used map instead of push, fixed search friends function

--- homeworks/iryna.gychka_irina-hychka/homework_5/task_1.js
+++ homeworks/iryna.gychka_irina-hychka/homework_5/task_1.js
@@ -4,7 +4,13 @@ const yArray = [0, 0, 0, 0, 0];
 const zArray = [10, -10, 10, -10, 10];
 
 function sumArray(n) {
-    return n.map((v, i) => (n.slice(0, i + 1).reduce((sum, el) => (sum + el))));
+    return n.map((v, i) => {
+        let sum = 0;
+        for (let j = 0; j <= i; j++) {
+            sum += n[j];
+        }
+        return sum;
+    });
 }
 
 // Task 1.2 Counts the duplicated values
