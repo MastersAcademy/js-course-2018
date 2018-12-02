9c3e90c9623bee8004341a8d7c3b6c79e7b8ce72
PavelOgienko <pavlik182@gmail.com>
2018-12-02 14:11:08 +0200

    fix2

--- homeworks/pavel.ogienko_pavelogienko/homework_5/task_2.js
+++ homeworks/pavel.ogienko_pavelogienko/homework_5/task_2.js
@@ -2,15 +2,15 @@ const classes = ['header', 'menu', 'menu_item', 'tabs',
     'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu',
     'menu_item', 'menu', 'menu_item'];
 
-    const countElements = (array) => {
-        const result = {};
-        array.forEach((item) => {
-            if (!result[item]) {
-                result[item] = 1;
-            }
-            ++result[item];
-        });
-        return result;
-    };
-    
+const countElements = (array) => {
+    const result = {};
+    array.forEach((item) => {
+        if (!result[item]) {
+        result[item] = 1;
+    }
+    ++result[item];
+    });
+    return result;
+};
+
 console.log(countElements(classes));
--- homeworks/pavel.ogienko_pavelogienko/homework_5/task_4.js
+++ homeworks/pavel.ogienko_pavelogienko/homework_5/task_4.js
@@ -1,12 +1,3 @@
-const people = [
-    { id: 1, name: 'Nick', friends: [2, 5, 6] },
-    { id: 2, name: 'John', friends: [1, 3] },
-    { id: 3, name: 'Mike', friends: [2, 5] },
-    { id: 4, name: 'Janny', friends: null },
-    { id: 5, name: 'Edward', friends: [1, 3] },
-    { id: 6, name: 'Jeen', friends: [5, 1] },
-];
-
 const getPeople = (people, userId) => {
     const result = [];
     const friendsIds = people.find(item => item.id === userId).friends;
@@ -15,5 +6,13 @@ const getPeople = (people, userId) => {
     });
     return result;
 };
+const people = [
+    { id: 1, name: 'Nick', friends: [2, 5, 6] },
+    { id: 2, name: 'John', friends: [1, 3] },
+    { id: 3, name: 'Mike', friends: [2, 5] },
+    { id: 4, name: 'Janny', friends: null },
+    { id: 5, name: 'Edward', friends: [1, 3] },
+    { id: 6, name: 'Jeen', friends: [5, 1] },
+];
 
 console.log(getPeople(people, 2));
