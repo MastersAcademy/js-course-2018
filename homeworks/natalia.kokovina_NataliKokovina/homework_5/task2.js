7aa405126e9f07d7fc5cebefc274ddf5b4db3dbd
Nataliya Kokovina <kokovina.nat>
2018-12-02 19:53:10 +0200

    fix task2

--- homeworks/natalia.kokovina_NataliKokovina/homework_5/task2.js
+++ homeworks/natalia.kokovina_NataliKokovina/homework_5/task2.js
@@ -3,13 +3,12 @@ const classes = ['header', 'menu', 'menu_item', 'tabs',
     'tab_item', 'menu', 'link', 'tabs', 'tab_item', 'menu', 'menu_item', 'menu', 'menu_item'];
 
 function toObject(arr) {
-    const newObj = {};
-    arr.reduce((obj, propertyName) => {
-        if (propertyName in newObj) {
-            return newObj[propertyName]++;
-        } newObj[propertyName] = 1;
+    return arr.reduce((obj, propertyName) => {
+        const newObj = obj;
+        if (propertyName in obj) {
+            newObj[propertyName]++;
+        } else newObj[propertyName] = 1;
         return obj;
     }, {});
-    console.log(newObj);
 }
-toObject(classes);
+console.log(toObject(classes));
