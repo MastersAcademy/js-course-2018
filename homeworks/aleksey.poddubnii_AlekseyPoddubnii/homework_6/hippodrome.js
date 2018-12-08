695af6451a52456dff4c1be18b99229472d119a8
Aleksey <kymotocycle@gmail.com>
2018-12-08 13:50:05 +0200

    fixes, hippodrome

--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_6/hippodrome.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_6/hippodrome.js
@@ -1,12 +1,78 @@
-function getRandomInt(min, max) {
-    Math.ceil(min);
-    Math.floor(max);
-    return Math.floor(Math.random() * (max - min)) + min;
+
+class Horse {
+    constructor(name, breed) {
+        this.name = name;
+        this.breed = breed;
+    }
 }
 
-console.log(getRandomInt(10, 16));
+class Racer extends Horse {
+    constructor(name, breed) {
+        super(name, breed);
+        this.distance = 0;
+        this.speed = 0;
+        this.setSpeed();
+    }
 
-function getRandomArbitrary(min, max) {
-    return Math.random() * (max - min) + min;
+    setSpeed() {
+        this.speed = Math.floor(Math.random() * (16 - 10)) + 10;
+    }
+
+    run() {
+        for (let i = 1; i <= 10; i++) {
+            setTimeout(() => {
+                this.distance += this.speed;
+                this.setSpeed();
+            }, 1000 * i);
+        }
+    }
 }
-console.log(getRandomArbitrary(10, 15));
+
+class Race {
+    constructor() {
+        this.horses = [];
+    }
+
+    createRace() {
+        const players = [
+            { name: 'Steven Rogers', breed: 'Captain America' },
+            { name: 'Reed Richards', breed: 'Mister Fantastic' },
+            { name: 'Max Eyz', breed: 'Magneto' },
+            { name: 'Henry Philip Hank McCoy', breed: 'Beast' },
+            { name: 'Wade Winston Wilson', breed: 'Deadpool' },
+            { name: 'Tony Stark', breed: 'Iron Man' },
+            { name: 'Carol Susan Jane Danvers', breed: 'Captain Marvel' },
+            { name: 'Loki', breed: 'Loki' },
+            { name: 'Tor', breed: 'Tor' },
+            { name: 'Robert Bruce', breed: 'The Hulk' },
+        ];
+        players.forEach((racer) => {
+            this.horses.push(new Racer(racer.name, racer.breed));
+        });
+    }
+
+    startRace() {
+        console.log('Lets the battle begin');
+        this.horses.forEach(racer => racer.run());
+        for (let i = 1; i <= 10; i++) {
+            setTimeout(() => {
+                console.log('On this step we can see result\'s:');
+                this.horses.forEach(racer => console.log(`Name: ${racer.name}, Distance ${racer.distance}`));
+            }, 1000 * i);
+        }
+    }
+
+    finishRace() {
+        setTimeout(() => {
+            this.horses.sort((a, b) => b.distance - a.distance);
+            console.log(`Winners is: ${this.horses[0].name}, the ${this.horses[0].breed}, with distance ${this.horses[0].distance}`);
+            this.horses.sort((a, b) => a.distance - b.distance);
+            console.log(`Looser is: ${this.horses[0].name}, the ${this.horses[0].breed}, with distance ${this.horses[0].distance}`);
+        }, 11000);
+    }
+}
+
+const start = new Race();
+start.createRace();
+start.startRace();
+start.finishRace();
--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_6/user.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_6/user.js
@@ -2,9 +2,6 @@
 const user = {};
 
 Object.defineProperty(user, 'name', {
-    enumerable: true,
-    configurable: true,
-    writeble: true,
     set: (input) => {
         this.name = input.toLowerCase().trim().replace(/\b\w/g, name => name.toUpperCase());
     },
@@ -14,9 +11,6 @@ Object.defineProperty(user, 'name', {
 const plus = '+';
 
 Object.defineProperty(user, 'phone', {
-    enumerable: true,
-    configurable: true,
-    writeble: true,
     set: (phone) => {
         if (phone[0] === '+') {
             this.phone = plus + phone.split(/\D/).join('');
@@ -28,6 +22,9 @@ Object.defineProperty(user, 'phone', {
 });
 
 user.name = 'aNna-mAria joHNs';
+console.log(user.name);
+
+user.name = 'SOMe fOR Test it';
 console.log(user.name);
 
 user.phone = '+38(096)-111-22-33';
