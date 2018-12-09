b4f10ffb731e51ae31c4290c07c1ca5ebdf4a28e
Vladimir Kosenko <soldier3@rambler.ru>
2018-12-09 17:38:18 +0200

    homework 6

---
+++ homeworks/vladimir.kosenko_soldier3/homework_6/work.js
@@ -0,0 +1,30 @@
+const user = {};
+
+
+Object.defineProperties(user, {
+    fullName: {
+        get() {
+            return this.fullName1;
+        },
+        set(x) {
+            this.fullName1 = x.toLowerCase().replace(/\b\w/g, letter => letter.toUpperCase());
+        },
+    },
+    phone: {
+        get() {
+            return this.phone1;
+        },
+        set(x) {
+            this.phone1 = x.match(/^\+[\d]*|\d/g).join('');
+        },
+    },
+});
+
+
+user.fullName = 'aNna-mAria joHNs';
+console.log(user.fullName);
+
+user.phone = '+38(096)-111-22-33';
+console.log(user.phone);
+user.phone = '38(096)+111b22+33';
+console.log(user.phone);
