5bd0209c8052740a9d5058cfcba3266a38985e02
IrinaBondarenko <gukibn@gmail.com>
2018-12-09 12:36:42 +0200

    cheked RegExp, used arrow function

--- homeworks/irina.vlasova_IrinaBondarenko/homework_6/defineProperty.js
+++ homeworks/irina.vlasova_IrinaBondarenko/homework_6/defineProperty.js
@@ -1,23 +1,32 @@
 const user = {};
-Object.defineProperty(user, 'fullName', {
-    configurable: true,
-    enumerable: true,
-    set: (name) => {
-        const a = name.replace(/([A-Za-z])/g, s => s.toLowerCase());
-        this.fullName = a.replace(/(^|\s|[-])\S/g, s => s.toUpperCase());
-    },
-    get: () => this.fullName,
-});
-
-Object.defineProperty(user, 'phone', {
-    configurable: true,
-    enumerable: true,
-    set: (number) => {
-        this.phone = number.replace(/((?![/+])|(?!^))\D/g, '');
-    },
-    get: () => this.phone,
-});
-
+function addName(keyName, obj) {
+    let name;
+    Object.defineProperty(obj, keyName, {
+        configurable: true,
+        enumerable: true,
+        set(value) {
+            name = value.toLowerCase().replace(/\b\w/g, s => s.toUpperCase());
+        },
+        get() {
+            return name;
+        },
+    });
+}
+function addNumber(keyName, obj) {
+    let num;
+    Object.defineProperty(obj, keyName, {
+        configurable: true,
+        enumerable: true,
+        set(value) {
+            num = value.replace(/(?!^\+)\D/g, '');
+        },
+        get() {
+            return num;
+        },
+    });
+}
+addName('fullName', user);
+addNumber('phone', user);
 user.fullName = 'aNna-mAria joHNs';
 console.log(user.fullName);
 user.phone = '+38(096)-111-22-33';
