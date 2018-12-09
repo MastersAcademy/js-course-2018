21150f4ab0fc292cd4f2552299e3f7610930a9ca
Vladimir Karbivnychiy <vovan63@gmail.com>
2018-12-09 17:55:39 +0200

    Fix

--- homeworks/vladimir.karbivnychiy_VladimirSkarb/homework_6/task1.js
+++ homeworks/vladimir.karbivnychiy_VladimirSkarb/homework_6/task1.js
@@ -1,23 +1,32 @@
 const user = {};
-Object.defineProperties(user, {
-    fullName: {
-        set: (name) => {
-            this.fullName = name.replace(/([[A-Za-z])\S/g, s => s.toLowerCase()).replace(/(^|\s|[-])\S/g, s => s.toUpperCase());
-        },
-        get: () => this.fullName,
+function corrName(keyName, obj) {
+    let name;
+    Object.defineProperty(obj, keyName, {
         enumerable: true,
         configurable: true,
-    },
-    phone: {
-        set: (phoneNum) => {
-            this.phone = phoneNum.replace(/(?!^\+)\D+/g, '');
+        set(value) {
+            name = value.toLowerCase().replace(/(^|\s|[-])\S/g, s => s.toUpperCase());
         },
-        get: () => this.phone,
+        get() {
+            return name;
+        }
+    });
+}
+function corrNumber(keyName, obj) {
+    let num;
+    Object.defineProperty(obj, keyName, {
         enumerable: true,
         configurable: true,
-    },
-});
-
+        set(value) {
+            num = value.replace(/(?!^\+)\D+/g, '');
+        },
+        get() {
+            return num;
+        }
+    });
+}
+corrName('fullName', user);
+corrNumber('phone', user);
 user.fullName = 'aNna-mAria joHNs';
 console.log(user.fullName);
 user.phone = '+38(096)-111-22-33';
