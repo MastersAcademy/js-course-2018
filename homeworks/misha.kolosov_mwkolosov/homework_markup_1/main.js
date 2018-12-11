0b1b6dcbe060374940dc46a55bde8eb5cd7ecd7d
mwkolosov <mwkolosov@gmail.com>
2018-12-11 14:07:14 +0200

    Delete main.js

--- homeworks/misha.kolosov_mwkolosov/homework_markup_1/main.js
+++
@@ -1,6 +0,0 @@
-function classToggle() {
-    const navs = document.querySelectorAll('.nav');
-    navs.forEach(nav => nav.classList.toggle('nav__mobile'));
-}
-
-document.querySelector('.button_toogle').addEventListener('click', classToggle);
