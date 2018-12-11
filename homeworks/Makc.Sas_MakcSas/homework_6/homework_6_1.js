3c6bac947fd7d09cc26f5c60758b03888aed8f2b
MaksSas <sasmaks123@gmail.com>
2018-12-11 21:04:38 +0200

    fix: homework_6

--- homeworks/Makc.Sas_MakcSas/homework_6/homework_6_1.js
+++ homeworks/Makc.Sas_MakcSas/homework_6/homework_6_1.js
@@ -1,27 +1,31 @@
-const user = {};
+const users = {};
 const phone = {};
-let fullName;
-let phoneNumber;
-Object.defineProperties(user, {
-    fullName: {
-        set(name) {
-            fullName = name.toString().toLowerCase().replace(/\b\w/g, value => value.toUpperCase());
+function userObjects(user) {
+    let fullName;
+    let phoneNumber;
+    Object.defineProperties(users, {
+        fullName: {
+            set(name) {
+                fullName = name.toString().toLowerCase().replace(/\b\w/g, value => value.toUpperCase());
+            },
+            get() {
+                return fullName;
+            },
         },
-        get() {
-            return fullName;
+    });
+    Object.defineProperties(phone, {
+        phoneNumber: {
+            set(numPhone) {
+                phoneNumber = '+'.concat(numPhone.toString().replace(/\D/g, ''));
+            },
+            get() {
+                return phoneNumber;
+            },
         },
-    },
-});
-Object.defineProperties(phone, {
-    phoneNumber: {
-        set(numPhone) {
-            phoneNumber = '+'.concat(numPhone.toString().replace(/\D/g, ''));
-        },
-        get() {
-            return phoneNumber;
-        },
-    },
-});
+    });
+    return user;
+}
+const user = userObjects({});
 user.fullName = 'aNna-mAria joHNs';
 console.log(user.fullName);
 user.phone = '+38(096)-111-22-33';
