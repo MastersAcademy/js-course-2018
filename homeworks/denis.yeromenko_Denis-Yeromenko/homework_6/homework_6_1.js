c5259248f9875077cc66e8215b20b5cd2e9969f6
Denis-Yeromenko <den.yeromenko@gmail.com>
2018-12-12 14:57:54 +0200

    homework_6_1.js was fixed

--- homeworks/denis.yeromenko_Denis-Yeromenko/homework_6/homework_6_1.js
+++ homeworks/denis.yeromenko_Denis-Yeromenko/homework_6/homework_6_1.js
@@ -1,32 +1,26 @@
 const user = {};
-
-Object.defineProperty(user, 'fullName', {
-    get: () => this.fullName,
-
-    set: (value) => {
-        let clearName = value.trim().toLowerCase().split(/\s+/);
-        for (let i = 0; i < clearName.length; i++) {
-            clearName[i] = clearName[i].charAt(0).toUpperCase() + clearName[i].slice(1);
-        }
-        clearName = clearName.join(' ').split(/-/);
-        for (let i = 0; i < clearName.length; i++) {
-            clearName[i] = clearName[i].charAt(0).toUpperCase() + clearName[i].slice(1);
-        }
-        this.fullName = clearName.join('-');
+Object.defineProperties(user, {
+    fullName: {
+        get() {
+            return this.userFullName;
+        },
+        set(value) {
+            const re = /\b\w/g;
+            this.userFullName = value.trim().toLowerCase().replace(re, str => str.toUpperCase());
+        },
+    },
+    phone: {
+        get() {
+            return this.userPhone;
+        },
+        set(value) {
+            const re = /^\+[\d]*|\d/g;
+            this.userPhone = value.trim().match(re).join('');
+        },
     },
 });
 
-Object.defineProperty(user, 'phone', {
-    get: () => this.phone,
-
-    set: (value) => {
-        let newPhoneNumber = '';
-        if (value.trim()[0] === '+') { newPhoneNumber += '+'; }
-        this.phone = newPhoneNumber + (value.trim().match(/\d/g)).join('');
-    },
-});
-
-user.fullName = '  aLi-Ibn-haTab   kiziLbek jr.';
+user.fullName = '  aLi-Ibn-haTab kiziLbek jr.';
 console.log(user.fullName);
 user.fullName = 'aNna-mAria joHNs';
 console.log(user.fullName);
