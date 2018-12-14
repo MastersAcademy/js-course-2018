e18c6de65003aa59c4e7826bac995b6b85e10128
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2018-12-14 03:15:57 +0200

    hw-7 added test60 and test51

--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_7/fibo.test.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_7/fibo.test.js
@@ -1,6 +1,18 @@
 const fibo = require('./fibo.js');
 
-const perfection = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-test('is Equal to perfection array', () => {
-    expect(fibo(99)).toEqual(perfection);
+const perfection99 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+test('is Equal to perfection99 array', () => {
+    expect(fibo(99)).toEqual(perfection99);
+});
+
+
+const perfection60 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+test('is Equal to perfection60 array', () => {
+    expect(fibo(60)).toEqual(perfection60);
+});
+
+
+const perfection51 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+test('is Equal to perfection51 array', () => {
+    expect(fibo(51)).toEqual(perfection51);
 });
