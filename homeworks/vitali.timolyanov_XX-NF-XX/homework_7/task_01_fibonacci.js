d5c2f80ced06550400f6ab53c265da98621655d8
Vitali Timolyanov <nf.ebox@gmail.com>
2018-12-14 21:38:22 +0200

    Refactor: move logic to functions

--- homeworks/vitali.timolyanov_XX-NF-XX/homework_7/task_01_fibonacci.js
+++ homeworks/vitali.timolyanov_XX-NF-XX/homework_7/task_01_fibonacci.js
@@ -1,15 +1,20 @@
 const MIN_RANDOM = 50;
 const MAX_RANDOM = 100;
 
-function getRandomFibonacciSequence() {
-    const difference = MAX_RANDOM - MIN_RANDOM;
-    const maxNumber = Math.trunc(Math.random() * (difference + 1) + MIN_RANDOM);
-    const numbers = [0, 1];
-
-    for (let i = 1; i <= maxNumber; i += numbers[numbers.length - 2]) {
-        numbers.push(i);
-    }
-    return numbers;
+function getRandomNumber(min, max) {
+    return Math.trunc(Math.random() * (max - min + 1) + min);
 }
 
-module.exports = getRandomFibonacciSequence;
+function createFibonacciSequence(limit) {
+    const sequence = [0, 1];
+    for (let i = 1; i <= limit; i += sequence[sequence.length - 2]) {
+        sequence.push(i);
+    }
+    return sequence;
+}
+
+function getFibonacciSequence() {
+    return createFibonacciSequence(getRandomNumber(MIN_RANDOM, MAX_RANDOM));
+}
+
+module.exports = getFibonacciSequence;
