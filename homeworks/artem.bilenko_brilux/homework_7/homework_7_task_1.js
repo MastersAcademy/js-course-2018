5a76e1ebd00433f0502718b9d2f172fac067088b
brilux <aavv22oo8ntfku>
2018-12-15 19:15:07 +0200

    add homework_7_task_1 fixes

--- homeworks/artem.bilenko_brilux/homework_7/homework_7_task_1.js
+++ homeworks/artem.bilenko_brilux/homework_7/homework_7_task_1.js
@@ -1,12 +1,18 @@
-function fibonacci() {
+function rangeNumbers() {
+    return Math.floor(Math.random() * 50) + 50;
+}
+
+function fibonacci(endOfRange) {
     const arr = [0, 1];
-    const rangeNumbers = Math.floor(Math.random() * 50) + 50;
-    for (let i = 2; i <= rangeNumbers; i++) {
+    for (let i = 2; i <= endOfRange; i++) {
         const arrElement = arr[i - 1] + arr[i - 2];
-        if (arrElement <= rangeNumbers) {
+        if (arrElement <= endOfRange) {
             arr.push(arrElement);
+        } else {
+            break;
         }
     }
     return arr;
 }
+console.log(fibonacci(rangeNumbers()));
 module.exports = { fibonacci };
--- homeworks/artem.bilenko_brilux/homework_7/homework_7_task_1.test.js
+++ homeworks/artem.bilenko_brilux/homework_7/homework_7_task_1.test.js
@@ -1,32 +1,25 @@
 const { fibonacci } = require('./homework_7_task_1');
 
-const less55 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
-const less89 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
-const less100 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+test('array type check', () => {
+    expect(Array.isArray(fibonacci())).toBe(true);
+});
 
-describe('fibonacci test', () => {
-    test('array check test ', () => {
-        expect(Array.isArray(fibonacci())).toBe(true);
-    });
-    test('first element in array test', () => {
-        expect(fibonacci()[0]).toBe(0);
-    });
-    test('second element in array test', () => {
-        expect(fibonacci()[1]).toBe(1);
-    });
-    test('fibonacci array test < 55', () => {
-        if (fibonacci()[fibonacci().length] === 10) {
-            expect(fibonacci()).toEqual(less55);
-        }
-    });
-    test('fibonacci array test 55 to 89', () => {
-        if (fibonacci()[fibonacci().length] === 11) {
-            expect(fibonacci()).toEqual(less89);
-        }
-    });
-    test('fibonacci array test 89 to 100', () => {
-        if (fibonacci()[fibonacci().length] === 12) {
-            expect(fibonacci()).toEqual(less100);
-        }
-    });
+test('check first and second elements in array', () => {
+    const arr = [0, 1];
+    expect(fibonacci(0)).toEqual(arr);
+});
+
+test('fibonacci array test then diapason less 55', () => {
+    const less55 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+    expect(fibonacci(54)).toEqual(less55);
+});
+
+test('fibonacci array test then diapason from 55 to 89', () => {
+    const less89 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    expect(fibonacci(88)).toEqual(less89);
+});
+
+test('fibonacci array test then diapason from 89 to 100', () => {
+    const less100 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+    expect(fibonacci(99)).toEqual(less100);
 });
