c8f95c452f385158ad866894aa072330af3419fa
AyzekUorren <ayzek1@gmail.com>
2018-12-16 23:04:03 +0200

    add: Fibonacci to max and to number of element, updated test

--- homeworks/anatoliy.pogoriliy_AyzekUorren/homework_7/task1.js
+++ homeworks/anatoliy.pogoriliy_AyzekUorren/homework_7/task1.js
@@ -13,6 +13,33 @@ class Fibonacci {
 
         return this.generate(n - 1) + this.generate(n - 2);
     }
+
+    /**
+     * @param {number} lastElement
+     * @returns Array Fibonacci sequence.
+     */
+    generateArray(lastElement) {
+        const arrayResult = [];
+        for (let i = 0; i < lastElement; i++) {
+            arrayResult.push(this.generate(i));
+        }
+        return arrayResult;
+    }
+
+    /**
+     * @param {number} maxValue
+     * @returns Array Fibonacci sequence.
+     */
+    gereateArrayToMaxValue(maxValue) {
+        const arrayResult = [];
+        for (let i = 0; i < maxValue; i++) {
+            const value = this.generate(i);
+            if (value <= maxValue) {
+                arrayResult.push(value);
+            } else break;
+        }
+        return arrayResult;
+    }
 }
 
 module.exports = Fibonacci;
--- homeworks/anatoliy.pogoriliy_AyzekUorren/homework_7/test/task1.test.js
+++ homeworks/anatoliy.pogoriliy_AyzekUorren/homework_7/test/task1.test.js
@@ -23,4 +23,34 @@ describe('Fibonacci', () => {
             expect(generated).toBe(6765);
         });
     });
+    describe('Fibonacci array', () => {
+        test('array 0 -> 10', () => {
+            const generator = new Fibonacci();
+            const generatedArray = generator.generateArray(10);
+            const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+            expect(generatedArray).toEqual(expectedResult);
+        });
+        test('array 0 -> 20', () => {
+            const generator = new Fibonacci();
+            const generatedArray = generator.generateArray(20);
+            const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+                89, 144, 233, 377, 610, 987, 1597, 2584, 4181];
+            expect(generatedArray).toEqual(expectedResult);
+        });
+    });
+    describe('Fibonacci array to maxNumber', () => {
+        test('array 0 -> 55', () => {
+            const generator = new Fibonacci();
+            const generatedArray = generator.gereateArrayToMaxValue(55);
+            const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+            expect(generatedArray).toEqual(expectedResult);
+        });
+        test('array 0 -> 4181', () => {
+            const generator = new Fibonacci();
+            const generatedArray = generator.gereateArrayToMaxValue(4181);
+            const expectedResult = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55,
+                89, 144, 233, 377, 610, 987, 1597, 2584, 4181];
+            expect(generatedArray).toEqual(expectedResult);
+        });
+    });
 });
