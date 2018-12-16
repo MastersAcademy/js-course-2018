ebd196791ff704e30917c6f8338645528eeee06e
turuta <turuta51@gmail.com>
2018-12-16 13:58:17 +0200

    fixed

--- homeworks/Aleksandr.Turuta_turuta7/homework_7/homework.test.js
+++ homeworks/Aleksandr.Turuta_turuta7/homework_7/homework.test.js
@@ -1,19 +1,19 @@
-const { cycleAndTest } = require('./homework_7');
+const { makeFiboArr } = require('./homework_7');
 
-describe('test fibonacci', () => {
-    test('test fibonacci', () => {
-        expect(cycleAndTest(0)).toEqual([]);
+describe('Test funcltion "makeFiboArr", function supposed to return an array', () => {
+    test('returned empty array', () => {
+        expect(makeFiboArr(0)).toEqual([]);
     });
-});
 
-describe('test fibonacci', () => {
-    test('test fibonacci', () => {
-        expect(cycleAndTest(1)).toEqual([0]);
+    test('1 index element of returned array should be 0', () => {
+        expect(makeFiboArr(1)).toEqual([0]);
     });
-});
 
-describe('test fibonacci', () => {
-    test('test fibonacci', () => {
-        expect(cycleAndTest(2)).toEqual([0, 1, 1]);
+    test('for index 2 and higher, elements of returned array should be the sum of previous two elements', () => {
+        expect(makeFiboArr(2)).toEqual([0, 1, 1]);
+    });
+
+    test('if there is no function value, the array will return [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]', () => {
+        expect(makeFiboArr()).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
     });
 });
--- homeworks/Aleksandr.Turuta_turuta7/homework_7/homework_7.js
+++ homeworks/Aleksandr.Turuta_turuta7/homework_7/homework_7.js
@@ -1,13 +1,13 @@
-function cycleAndTest(rand) {
-    console.log(rand);
-
+function makeFiboArr(rand) {
     const array = [];
+    const randNum = rand === undefined ? 50 : rand;
+    console.log(randNum);
     for (let i = 0; i < 30; i++) {
         const fib = array[i - 1] + array[i - 2];
-        if (i === 0 && rand > 0) array.push(0);
-        if (i === 1 && rand > 1) array.push(1);
-        if (i === 2 && rand > 1) array.push(1);
-        if (i > 2 && fib < rand) array.push(fib);
+        if (i === 0 && randNum > 0) array.push(0);
+        if (i === 1 && randNum > 1) array.push(1);
+        if (i === 2 && randNum > 1) array.push(1);
+        if (i > 2 && fib < randNum) array.push(fib);
     }
     return array;
 }
@@ -16,6 +16,6 @@ function randomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-console.log(cycleAndTest(randomNumber(50, 100)));
+console.log(makeFiboArr(randomNumber(50, 100)));
 
-module.exports = { cycleAndTest };
+module.exports = { makeFiboArr };
