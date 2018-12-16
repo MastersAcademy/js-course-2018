13f06df9d7c0cbde2b1525f5679fa17708467552
VadimMir <Miroshnik_vy@ukr.net>
2018-12-16 17:51:25 +0200

    hw7.2

--- homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7_2.test.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7_2.test.js
@@ -1,20 +1,20 @@
 const fib = require('./homework_7');
 
-describe("fibonacci", function() {
-    test("check array", function() {
+describe('fibonacci',() => {
+    test('check array', function() {
         expect(Array.isArray(fib().toBe(true)));
     });
 
-    test("check 0", function() {
+    test('check 0', () => {
         expect(fib(0).toBe([0]));
     });
 
-    test("check 50", function() {
+    test('check 50', () => {
         const array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
         expect(fin(50).toEqual(array));
     });
 
-    test("check 100", function() {
+    test('check 100', () => {
         const array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
         expect(fin(100).toEqual(array));
     });
