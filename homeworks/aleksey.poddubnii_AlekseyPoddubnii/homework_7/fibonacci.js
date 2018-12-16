f3a2104bfa826263c83d43049839b8c3e2ad7fce
Aleksey <kymotocycle@gmail.com>
2018-12-16 13:28:36 +0200

    Fixed test, function for rand

--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.js
@@ -1,11 +1,17 @@
-const rand = Math.floor(Math.random() * (11 - 10 + 1)) + 10;
-const result = [0, 1];
+function rand(max, min) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function fibonacci(num) {
+    const result = [0, 1];
     for (let i = 2; i <= num; i++) {
         result.push(result[i - 1] + result[i - 2]);
     }
     return result;
 }
-console.log(fibonacci(rand));
-module.exports = { fibonacci, rand };
+console.log(fibonacci(rand(11, 10)));
+
+module.exports = {
+    fibonacci,
+    rand,
+};
--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.test.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.test.js
@@ -1,26 +1,27 @@
-const {
-    fibonacci,
-} = require('./fibonacci.js');
+const { fibonacci, rand } = require('./fibonacci.js');
 
-const { rand } = require('./fibonacci.js');
+test('comming data is array', () => {
+    expect(Array.isArray(fibonacci())).toBe(true);
+});
 
-describe('test fibonacci', () => {
-    test('is fibonacci object?', () => {
-        expect(typeof fibonacci()).toBe('object');
-    });
-    test('comming data is array', () => {
-        expect(Array.isArray(fibonacci())).toBe(true);
-    });
-    test('random num not biggest than 11 (only from 11 to 0)', () => {
-        expect(rand).toBeLessThanOrEqual(11);
-    });
-    test('start from zero', () => {
-        expect(fibonacci()[0]).toBe(0);
-    });
-    test('second num', () => {
-        expect(fibonacci()[1]).toBe(1);
-    });
-    test('third come like first of number of fibanacci', () => {
-        expect(fibonacci()[2]).toBe(1);
-    });
+test('random num not biggest than 11 (only from 11 to 0)', () => {
+    expect(rand(11, 10)).toBeLessThanOrEqual(11);
+});
+
+test('start from zero', () => {
+    expect(fibonacci()[0]).toBe(0);
+});
+
+test('second num', () => {
+    expect(fibonacci()[1]).toBe(1);
+});
+
+test('array less o equal 55', () => {
+    const than55 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+    expect(fibonacci(10)).toEqual(than55);
+});
+
+test('if array biggest less 55', () => {
+    const than89 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+    expect(fibonacci(11)).toEqual(than89);
 });
