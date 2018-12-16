fb538770aac739bd8686a07266cd90aa13dd5ec3
Aleksey <kymotocycle@gmail.com>
2018-12-16 18:25:38 +0200

    Mock

--- homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.test.js
+++ homeworks/aleksey.poddubnii_AlekseyPoddubnii/homework_7/fibonacci.test.js
@@ -1,27 +1,42 @@
-const { fibonacci, rand } = require('./fibonacci.js');
+const { fibonacci } = require('./fibonacci.js');
+let { rand } = require('./fibonacci.js');
 
-test('comming data is array', () => {
-    expect(Array.isArray(fibonacci())).toBe(true);
+describe('Testing changes of random values beetween 10 - 11', () => {
+    rand = jest.fn();
+
+    test('Last is 55', () => {
+        const data = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+        rand.mockReturnValue(10);
+        expect(fibonacci(rand())).toEqual(data);
+    });
+
+    test('Last is 89', () => {
+        const data = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        rand.mockReturnValue(11);
+        expect(fibonacci(rand())).toEqual(data);
+    });
+
+    test('random num not biggest than 11 (only from 11 to 0)', () => {
+        expect(rand(11, 10)).toBeLessThanOrEqual(11);
+    });
 });
 
-test('random num not biggest than 11 (only from 11 to 0)', () => {
-    expect(rand(11, 10)).toBeLessThanOrEqual(11);
-});
+describe('Testing array', () => {
+    test('comming data is array', () => {
+        expect(Array.isArray(fibonacci())).toBe(true);
+    });
 
-test('start from zero', () => {
-    expect(fibonacci()[0]).toBe(0);
-});
+    test('second num', () => {
+        expect(fibonacci()[1]).toBe(1);
+    });
 
-test('second num', () => {
-    expect(fibonacci()[1]).toBe(1);
-});
+    test('array less or equal 55', () => {
+        const than55 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
+        expect(fibonacci(10)).toEqual(than55);
+    });
 
-test('array less o equal 55', () => {
-    const than55 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55];
-    expect(fibonacci(10)).toEqual(than55);
-});
-
-test('if array biggest less 55', () => {
-    const than89 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-    expect(fibonacci(11)).toEqual(than89);
+    test('if array less or equal 89', () => {
+        const than89 = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        expect(fibonacci(11)).toEqual(than89);
+    });
 });
