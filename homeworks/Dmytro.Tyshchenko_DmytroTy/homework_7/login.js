d4de802b3cab1b7158eb82cf69936a82f088bcce
Dmytro Tyshchenko <kwazar2008@ukr.net>
2018-12-17 08:53:05 +0200

    Update-fix

--- homeworks/Dmytro.Tyshchenko_DmytroTy/homework_7/login.js
+++ homeworks/Dmytro.Tyshchenko_DmytroTy/homework_7/login.js
@@ -1,3 +1,5 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+
 const USERS = require('./users');
 
 class Login {
@@ -8,28 +10,42 @@ class Login {
     login(login, password) {
         const FAIL_USER = new Error('The system does not have a user registered with this login!');
         const FAIL_PASSWORD = new Error('Incorrect password entered!');
-        const FAIL_USER_BLOCKED = new Error('Wait 10 seconds before the next login attempt!');
 
+        const user = USERS.filter(obj => obj.login === login)[0];
+        if (user === undefined) {
+            this._addFail(login);
+            throw FAIL_USER;
+        }
+
+        const fail = this.failed.filter(obj => obj.login === login)[0];
+        if (user.password === password && (fail === undefined || fail.count < 3)) {
+            this._clearFail(login);
+            return 'success';
+        }
+
+        this._addFail(login);
+        throw FAIL_PASSWORD;
+    }
+
+    _addFail(login) {
+        const FAIL_USER_BLOCKED = new Error('Wait 10 seconds before the next login attempt!');
         const fail = this.failed.filter(obj => obj.login === login)[0];
         if (fail === undefined) this.failed.push({ login, count: 1 });
         else {
             fail.count++;
-            if (fail.count > 2) this.timerAllowLogin(login);
+            if (fail.count > 2) this._timerAllowLogin(login);
             if (fail.count > 3) throw FAIL_USER_BLOCKED;
         }
-
-        const user = USERS.filter(obj => obj.login === login)[0];
-        if (user === undefined) throw FAIL_USER;
-
-        if (user.password === password) return 'success';
-
-        throw FAIL_PASSWORD;
     }
 
-    timerAllowLogin(login) {
+    _clearFail(login) {
+        const user = this.failed.filter(obj => obj.login === login)[0];
+        this.failed.splice(this.failed.indexOf(user), 1);
+    }
+
+    _timerAllowLogin(login) {
         setTimeout(() => {
-            const user = this.failed.filter(obj => obj.login === login)[0];
-            this.failed.splice(this.failed.indexOf(user), 1);
+            this._clearFail(login);
         }, 10000);
     }
 }
