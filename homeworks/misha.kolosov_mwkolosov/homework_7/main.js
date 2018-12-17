a0927e1deca150c6b9d2b4e47b6614aaf1509581
Misha <mwkolosov@gmail.com>
2018-12-17 22:42:53 +0200

    optimize homework 7

--- homeworks/misha.kolosov_mwkolosov/homework_7/main.js
+++ homeworks/misha.kolosov_mwkolosov/homework_7/main.js
@@ -1,16 +1,17 @@
 const random = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
 
-const fib = (iter) => {
+const fib = (maxNum) => {
     const arr = [0, 1];
-    for (let i = 0; i < iter; i++) {
+    for (let i = 0; i < 20; i++) {
         const a = arr[i] + arr[i + 1];
-        if (a <= random) {
+        if (a <= maxNum) {
             arr.push(a);
         }
     }
     return arr;
 };
 
-console.log(fib(10));
+console.log(fib(random));
+console.log(random);
 
 module.exports = { fib };
