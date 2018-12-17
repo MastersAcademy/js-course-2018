3f48ae6ac57384e419f75e374ccc6c69f22daf76
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2018-12-17 13:02:05 +0200

    save before switch branches

--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_markup_4/js/main.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_markup_4/js/main.js
@@ -2,9 +2,9 @@ const myBurger = document.getElementById('myBurger');
 const myBurgerNav = document.getElementById('myBurgerNav');
 
 function myNavBurgerToggle() {
-    myBurger.children[0].classList.toggle('burger__line--active');
-    myBurgerNav.classList.toggle('dreams-navigation');
-    myBurgerNav.classList.toggle('dreams-navigation--active');
+    myBurger.children[0].classList.toggle('js-burger__line-active');
+    myBurgerNav.classList.toggle('js-dreams-navigation');
+    myBurgerNav.classList.toggle('js-dreams-navigation-active');
 }
 
 myBurger.addEventListener('click', myNavBurgerToggle);
