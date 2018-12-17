5c2b1d0e4293f99e36f6557248f34fedde7e5f9d
Dima Zapisochny <dima.zapisochny@gmail.com>
2018-12-17 20:28:03 +0200

    Fixed

--- homeworks/dima.zapisochny_dima-88/homework_7/task1.js
+++ homeworks/dima.zapisochny_dima-88/homework_7/task1.js
@@ -1,13 +1,16 @@
-const fibNumbers = () => {
-    const lastNumb = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function fibNumbers(maxNumber, length) {
     const arr = [0, 1];
-    for (let i = 0; i < 10; i++) {
-        const a = arr[i] + arr[i + 1];
-        if (a <= lastNumb) {
+    for (let elem = 0; elem < length; elem++) {
+        const a = arr[elem] + arr[elem + 1];
+        if (a <= maxNumber) {
             arr.push(a);
         }
     }
     return arr;
-};
-
+}
+console.log(fibNumbers(getRandom(50, 100), 10));
 module.exports = { fibNumbers };
