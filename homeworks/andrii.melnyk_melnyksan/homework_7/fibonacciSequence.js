bc360f4a97b75af790db6445086614a71c320e4a
melnyksan <andriimelnyk986@gmail.com>
2018-12-17 17:45:59 +0200

    js-homework-7__practice-with-ttd--is-edited.v1_201812171745

--- homeworks/andrii.melnyk_melnyksan/homework_7/fibonacciSequence.js
+++ homeworks/andrii.melnyk_melnyksan/homework_7/fibonacciSequence.js
@@ -1,13 +1,17 @@
+function randomLimitValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-function fibonacciSequence() {
-    const limitRandomValue = Math.floor(Math.random() * 51) + 50;
+function createFibonacciSequence(limit) {
     const array = [0, 1];
 
-    while ((array[array.length - 1] + array[array.length - 2]) < limitRandomValue) {
+    while ((array[array.length - 1] + array[array.length - 2]) < limit) {
         array.push(array[array.length - 1] + array[array.length - 2]);
     }
 
     return array;
 }
 
-module.exports = fibonacciSequence;
+module.exports.randomValue = randomLimitValue;
+
+module.exports.fibonacciSequence = createFibonacciSequence;
--- homeworks/andrii.melnyk_melnyksan/homework_7/fibonacciSequence.test.js
+++ homeworks/andrii.melnyk_melnyksan/homework_7/fibonacciSequence.test.js
@@ -1,15 +1,26 @@
-const fibonacciSequence = require('./fibonacciSequence');
+const fromFibonacciSequence = require('./fibonacciSequence');
+
+const randomLimitValue = fromFibonacciSequence.randomValue;
+const createFibonacciSequence = fromFibonacciSequence.fibonacciSequence;
+
+test('The random value is greater than or equal to min', () => {
+    expect(randomLimitValue(50, 100)).toBeGreaterThanOrEqual(50);
+});
+
+test('The random value is less than or equal to max', () => {
+    expect(randomLimitValue(50, 100)).toBeLessThanOrEqual(100);
+});
 
 test('The first number is 0', () => {
-    expect(fibonacciSequence()[0]).toBe(0);
+    expect(createFibonacciSequence(randomLimitValue(50, 100))[0]).toBe(0);
 });
 
 test('The second number is 1', () => {
-    expect(fibonacciSequence()[1]).toBe(1);
+    expect(createFibonacciSequence(randomLimitValue(50, 100))[1]).toBe(1);
 });
 
 test('Every number is the sum of the two preceding ones', () => {
-    const serviceArray = fibonacciSequence();
+    const serviceArray = createFibonacciSequence(randomLimitValue(50, 100));
     for (let i = 2; i < serviceArray.length; i++) {
         expect(serviceArray[i]).toBe(serviceArray[i - 1] + serviceArray[i - 2]);
     }
