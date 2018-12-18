e1c34b7618832f1ff341f6bb621ca4c44634b860
VadimMir <Miroshnik_vy@ukr.net>
2018-12-18 21:26:35 +0200

    up3

--- homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.js
@@ -2,7 +2,7 @@
 const randomNum = Math.floor(Math.random() * (100 - 50 + 1) + 50);
 
 function fib(random) {
-    const arr = [];
+    const arr = [0];
     let a = 1;
     let b = 0;
     for (let i = 0; i <= random; i += a) {
--- homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.test.js
+++ homeworks/vadym.myroshnyk_VadimMir/homework_7/homework_7.test.js
@@ -1,17 +1,37 @@
 const fib = require('./homework_7');
 
 describe('fibonacci', () => {
-    test('check array', () => {
+    test('check if returned value is array', () => {
         expect(Array.isArray(fib())).toBe(true);
     });
 
-    test('check 50', () => {
-        const array = [1, 1, 2, 3, 5, 8, 13, 21, 34];
-        expect(fib(50)).toEqual(array);
+    test('check Fibonacci sequence until max number of 50', () => {
+        const array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34];
+        const arrayFinonacci = fib(50);
+        for (let i = 0; i < arrayFinonacci.lenght; i++) {
+            expect(arrayFinonacci[i]).toEqual(arrayFinonacci[i] === array[i]);
+        }
     });
 
-    test('check 100', () => {
-        const array = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-        expect(fib(100)).toEqual(array);
+    test('check Fibonacci sequence until max number of 100', () => {
+        const array = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+        const arrayFinonacci = fib(50);
+        for (let i = 0; i < arrayFinonacci.lenght; i++) {
+            expect(arrayFinonacci[i]).toEqual(arrayFinonacci[i] === array[i]);
+        }
+    });
+
+    test('check if iterations count is less then max (50)', () => {
+        const array = fib(50);
+        for (let i = 0; i < array.lenght; i++) {
+            expect(array[i]).toBe(array[i] < 50);
+        }
+    });
+
+    test('check if iterations count is less then max (100)', () => {
+        const array = fib(100);
+        for (let i = 0; i < array.lenght; i++) {
+            expect(array[i]).toBe(array[i] < 100);
+        }
     });
 });
