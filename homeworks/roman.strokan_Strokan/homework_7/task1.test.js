0663a2be6c4900253a13cbdd8ab1e5876e9f0d9c
Strokan <srsche01@gmail.com>
2018-12-18 22:38:56 +0200

    hw7 fix 2

--- homeworks/roman.strokan_Strokan/homework_7/task1.test.js
+++ homeworks/roman.strokan_Strokan/homework_7/task1.test.js
@@ -3,25 +3,23 @@ const { fib } = require('./task1');
 describe('Fibonacci', () => {
     // Сheck start value
 
-    describe('Сheck start value', () => {
+    describe('Сheck start values', () => {
         test('Сheck start value: array[0]', () => {
-            expect(fib()[0]).toBe(0);
+            expect(fib(50)[0]).toBe(0);
+        });
+        test('Сheck start value: array[1]', () => {
+            expect(fib(50)[1]).toBe(1);
         });
     });
 
     // Validation values for correct calculation
 
     describe('Check array', () => {
-        test('Value 1: 51 (50-54)', () => {
-            expect(fib(51)[fib(51).length - 1]).toBe(34);
-        });
-
-        test('Value 2: 70 (55-89)', () => {
-            expect(fib(70)[fib(70).length - 1]).toBe(55);
-        });
-
-        test('Value 3 95 (90-100)', () => {
-            expect(fib(95)[fib(95).length - 1]).toBe(89);
+        test('The sum of the two previous', () => {
+            const fibArr = fib(Math.floor(Math.random() * 50) + 50);
+            for (let i = 2; i < fibArr.length; i++) {
+                expect(fibArr[i - 1] + fibArr[i - 2]).toBe(fibArr[i]);
+            }
         });
     });
 });
