e85625d4aa2c194efac6ea5959dee1edbd2a6a7b
Dmitriy Tyulpa <dimonakainnocent@gmail.com>
2018-12-19 09:24:21 +0200

    Homework 7 v2

--- homeworks/Dmitriy.Tyulpa_innocentDimon/homework_7/task.js
+++ homeworks/Dmitriy.Tyulpa_innocentDimon/homework_7/task.js
@@ -3,7 +3,7 @@ function fibonachi() {
     let b = 1;
     const random = Math.floor(Math.random() * 50 + 50);
     const result = [a, b];
-    for (let i = 3; i <= random; i++) {
+    for (let i = 3; b <= random; i++) {
         const c = a + b;
         a = b; b = c;
         result.push(b);
--- homeworks/Dmitriy.Tyulpa_innocentDimon/homework_7/task.test.js
+++ homeworks/Dmitriy.Tyulpa_innocentDimon/homework_7/task.test.js
@@ -5,19 +5,19 @@ describe('Task test', () => {
         expect(Array.isArray(fibonachi())).toBe(true);
     });
 
-    test('First element', () => {
+    test('First element must be 0', () => {
         expect(fibonachi()[0]).toBe(0);
     });
 
-    test('Five element', () => {
+    test('Five element must be 3', () => {
         expect(fibonachi()[4]).toBe(3);
     });
 
-    test('Ten element', () => {
+    test('Ten element must be 34', () => {
         expect(fibonachi()[9]).toBe(34);
     });
 
-    test('Sum test', () => {
+    test('Sum of the two previous elements test ', () => {
         const num = fibonachi();
         for (let i = 2; i < num.length; i++) {
             expect(num[i]).toBe(num[i - 1] + num[i - 2]);
