b1a435b44105a58a5ed745e6132b487e4cbfc705
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2018-12-20 15:08:24 +0200

    burger menu fixed

--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_markup_4/js/main.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_markup_4/js/main.js
@@ -2,9 +2,8 @@ const myBurger = document.getElementById('myBurger');
 const myBurgerNav = document.getElementById('myBurgerNav');
 
 function myNavBurgerToggle() {
-    myBurger.children[0].classList.toggle('js-burger__line-active');
-    myBurgerNav.classList.toggle('js-dreams-navigation');
-    myBurgerNav.classList.toggle('js-dreams-navigation-active');
+    myBurger.children[0].classList.toggle('active');
+    myBurgerNav.classList.toggle('active');
 }
 
 myBurger.addEventListener('click', myNavBurgerToggle);
