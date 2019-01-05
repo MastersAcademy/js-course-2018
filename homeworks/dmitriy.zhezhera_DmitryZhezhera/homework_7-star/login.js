18e7283a932cc973ede89cf2bf1e4fda9a729e2b
Zhezhera Dmitriy <zhezhera.d@mail.ru>
2019-01-05 11:44:15 +0200

    hw7* fix mentors comments

--- homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_7-star/login.js
+++ homeworks/dmitriy.zhezhera_DmitryZhezhera/homework_7-star/login.js
@@ -1,38 +1,43 @@
 const users = require('./users');
-// console.log(users);
+
+console.log(users);
 
 class Login {
     constructor() {
         this.lastResult = 'failed';
+        this.ourUser = '';
+    }
+
+    wrongPassword() {
+        this.ourUser.nFails = (this.ourUser.nFails === undefined ? 0 : this.ourUser.nFails) + 1;
+        this.ourUser.timeLastFail = Date.now();
+        return 'failed';
+    }
+
+    correctPassword() {
+        let result = '';
+        if (this.ourUser.nFails >= 3
+            && Date.now() <= this.ourUser.timeLastFail + 10000) {
+            result = `authentification is blocked for ${this.ourUser.timeLastFail + 10 - Date.now()} seconds`;
+        } else {
+            this.ourUser.nFails = 0;
+            result = 'success';
+        }
+        return result;
     }
 
     login(login, password) {
-        this.lastResult = 'failed';
-        for (let i = 0; i < users.length; i++) {
-            if (users[i].login === login) {
-                if (users[i].password !== password) {
-                    users[i].nFails = (users[i].nFails === undefined ? 0 : users[i].nFails) + 1;
-                    users[i].timeLastFail = Date.now();
-                    break;
-                } else { // if (users[i].password === password)
-                    if (users[i].nFails >= 3
-                    && Date.now() <= users[i].timeLastFail + 10000) {
-                        this.lastResult = `authentification is blocked for ${users[i].timeLastFail + 10 - Date.now()} seconds`;
-                        break;
-                    }
-                    // ---
-                    this.lastResult = 'success';
-                    users[i].nFails = 0;
-                }
-                break;
-            }
-        }
+        this.ourUser = users.find(user => user.login === login);
+        if (this.ourUser === undefined) return 'failed';
+        if (this.ourUser.password !== password) this.lastResult = this.wrongPassword();
+        else this.lastResult = this.correctPassword();
         return this.lastResult;
     }
 }
 
 const login = new Login();
 // console.log(login.login('admin', 'admin'));
+// console.log(login.login('user', 'admin'));
 // console.log(login.login('admin', 'admin1'));
 // console.log(login.login('admin', 'admin2'));
 // console.log(login.login('admin', 'admin3'));
